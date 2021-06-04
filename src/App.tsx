3141ec8e271f71e540f43618416bb229a9a4909f
steenkamby <steenkamby@live.dk>
2021-06-04 13:58:05 +0200

    try alternate monaco editor

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,6 @@ import ActionsPane from "./organisms/ActionsPane";
 import NavigatorPane from "./organisms/NavigatorPane";
 import FileTreePane from "./organisms/FileTreePane";
 import Header from "./organisms/Header";
-import { loader } from '@monaco-editor/react';
-const path = require('path');
-
-function ensureFirstBackSlash(str:string) {
-  return str.length > 0 && str.charAt(0) !== "/"
-      ? "/" + str
-      : str;
-}
-
-function uriFromPath(_path: any) {
-  const pathName = path.resolve(_path).replace(/\\/g, "/");
-  return encodeURI("file://" + ensureFirstBackSlash(pathName));
-}
-
-let p = path.join(__dirname, "../../../../../../../../node_modules/monaco-editor/min/vs")
-loader.config({
-  paths: {
-    vs: uriFromPath( p )
-  }
-});
 
 function App() {
   return (
--- src/molecules/Monaco/Monaco.tsx
+++ src/molecules/Monaco/Monaco.tsx
@@ -1,10 +1,52 @@
 import React from 'react';
-import Editor from '@monaco-editor/react';
-import * as fs from "fs";
-import path from "path";
+import MonacoEditor from 'react-monaco-editor';
 
+interface IProps {
+}
+
+interface IState {
+  code: any;
+}
+
+class Monaco extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      code: '// type your code...',
+    }
+  }
+  editorDidMount(editor: any, monaco: any) {
+    console.log('editorDidMount', editor);
+    editor.focus();
+  }
+  onChange(newValue: any, e: any) {
+    console.log('onChange', newValue, e);
+  }
+  render() {
+    const code = this.state.code;
+
+    const options = {
+      selectOnLineNumbers: true
+    };
+
+    return (
+      <MonacoEditor
+        width="800"
+        height="600"
+        language="shell"
+        theme="vs-dark"
+        value={code}
+        options={options}
+        onChange={this.onChange}
+        editorDidMount={this.editorDidMount}
+      />
+    );
+  }
+}
+
+/*
 const Monaco = () => {
-  let filename = path.join(__dirname, "../../../../../../../../deployment.yaml")
+  let filename = path.join(__dirname, "../../../deployment.yaml")
   let data = fs.readFileSync(filename, 'utf8')
 
   return (
@@ -18,5 +60,6 @@ const Monaco = () => {
       </div>
   );
 };
+*/
 
 export default Monaco;
