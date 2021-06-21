fd0a2295d1563e0405a2ac18901e84e3ca2fea9d
steenkamby <steenkamby@live.dk>
2021-06-21 14:47:16 +0200

    add styled-components, add preview notification to header

--- src/organisms/Header/Header.tsx
+++ src/organisms/Header/Header.tsx
@@ -1,15 +1,41 @@
 import * as React from 'react';
 import {Container, Row, Col} from 'react-bootstrap';
-import {debugBorder} from "../../styles/DebugStyles";
+import styled from 'styled-components';
 
-const Header = () => (
+import { useAppSelector } from '../../redux/hooks';
+
+
+const StyledRow = styled(Row)`
+  border: 3px solid blue;
+  border-radius: 2px;
+`;
+
+const Title = styled.h4`
+  font-size: 1.5em;
+  text-align: left;
+  color: palevioletred;
+`;
+
+const EditorMode = styled.h4`
+  font-size: 1.0em;
+  text-align: right;
+  color: blue;
+`;
+
+const Header = () => {
+  const isInPreviewMode = useAppSelector(state => state.main.previewResource) ? true : false;
+
+  return (
   <Container fluid>
-    <Row style={debugBorder}>
-      <Col sm={5}>
-        <h4>ManifestUI / Monokle logo</h4>
+    <StyledRow>
+      <Col sm={6}>
+        <Title>ManifestUI / Monokle logo</Title>
       </Col>
-    </Row>
+      <Col sm={6}>
+        <EditorMode>{ isInPreviewMode ? "Preview": "" }</EditorMode>
+      </Col>
+    </StyledRow>
   </Container>
-)
+)};
 
 export default Header;
