f9cab13d91c08eb324471c0e308c695ddc40b137
steenkamby <steenkamby@live.dk>
2021-06-21 15:05:23 +0200

    update background of header to grey, when preview is active

--- src/organisms/Header/Header.tsx
+++ src/organisms/Header/Header.tsx
@@ -8,12 +8,19 @@ import { useAppSelector } from '../../redux/hooks';
 const StyledRow = styled(Row)`
   border: 3px solid blue;
   border-radius: 2px;
+  background: papayawhip;
+`;
+
+const StyledRowPreviewMode = styled(Row)`
+  border: 3px solid tomato;
+  border-radius: 2px;
+  background: grey;
 `;
 
 const Title = styled.h4`
   font-size: 1.5em;
   text-align: left;
-  color: palevioletred;
+  color: tomato;
 `;
 
 const EditorMode = styled.h4`
@@ -27,14 +34,14 @@ const Header = () => {
 
   return (
   <Container fluid>
-    <StyledRow>
+    <Row as={isInPreviewMode? StyledRowPreviewMode : StyledRow}>
       <Col sm={6}>
         <Title>ManifestUI / Monokle logo</Title>
       </Col>
       <Col sm={6}>
         <EditorMode>{ isInPreviewMode ? "Preview": "" }</EditorMode>
       </Col>
-    </StyledRow>
+    </Row>
   </Container>
 )};
 
