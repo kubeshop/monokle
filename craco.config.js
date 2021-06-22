03500d18999aced0c7bbb291498814bccab6bd42
olensmar <ole@lensmar.com>
2021-06-22 21:44:28 +0200

    added webpack config

--- craco.config.js
+++ craco.config.js
@@ -1,15 +1,31 @@
-const CracoAlias = require("craco-alias");
+const CracoAlias = require('craco-alias');
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
 module.exports = {
+
+  webpack: {
+    plugins: {
+      add: [new MonacoWebpackPlugin({
+        languages: ['yaml'],
+      })],
+    },
+    // eslint-disable-next-line no-unused-vars
+    configure: (webpackConfig, { env, paths }) => {
+      webpackConfig.node.__dirname = false;
+      webpackConfig.target = 'electron-renderer';
+      return webpackConfig;
+    },
+  },
+
   plugins: [
     {
       plugin: CracoAlias,
       options: {
-        source: "tsconfig",
-        baseUrl: "./",
-        tsConfigPath: "./paths.json",
+        source: 'tsconfig',
+        baseUrl: './',
+        tsConfigPath: './paths.json',
         unsafeAllowModulesOutsideOfSrc: false,
-        debug: false
+        debug: false,
       }
     }
   ]
