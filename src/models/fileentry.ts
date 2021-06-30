564d83ad49ddee2a1eec5e25e15187e7c45fa042
olensmar <ole@lensmar.com>
2021-06-30 12:39:43 +0200

    added some more comments...

--- src/models/fileentry.ts
+++ src/models/fileentry.ts
@@ -1,3 +1,7 @@
+/**
+ * A file or folder in the file system.
+ */
+
 interface FileEntry {
   name: string; // the name of the file
   filePath: string; // the path of the file relative to the root folder - used as key in the fileMap
