8e1ddc4bb16e808841e1ecb1d26092d83eb255e7
olensmar <ole@lensmar.com>
2021-06-30 17:45:53 +0200

    revert exporting of style

--- src/components/organisms/NavigatorPane/NavigatorPane.tsx
+++ src/components/organisms/NavigatorPane/NavigatorPane.tsx
@@ -34,7 +34,7 @@ const TitleRow = styled(Row)`
   padding: 0;
 `;
 
-export const SectionRow = styled(Row)`
+const SectionRow = styled(Row)`
   border: 1px solid blue;
   border-radius: 2px;
   background: ${colors.appNormalBackgroound};
