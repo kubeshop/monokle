0568433a2a1851ab223885921884a36c443656ea
Catalin Hoha <catalin.hoha@gmail.com>
2021-07-08 12:46:17 +0300

    fix fileEntry tests for Windows machines

--- src/redux/utils/fileEntry.test.ts
+++ src/redux/utils/fileEntry.test.ts
@@ -1,17 +1,22 @@
+import path from "path";
 import {AppConfig} from '@models/appconfig';
 import {initialState} from '@redux/initialState';
 import {FileMapType, ResourceMapType} from '@models/appstate';
 import {getK8sResources} from '@redux/utils/resource';
 import {createFileEntry, getResourcesForPath, readFiles} from './fileEntry';
 
+function createSafePath(originalPath: string) {
+  return originalPath.replaceAll('/', path.sep);
+}
+
 test('create-file-entry', () => {
-  let e = createFileEntry('/a/very/long/path');
+  let e = createFileEntry(createSafePath('/a/very/long/path'));
   expect(e.highlight).toBeFalsy();
   expect(e.selected).toBeFalsy();
   expect(e.expanded).toBeFalsy();
   expect(e.excluded).toBeFalsy();
   expect(e.name).toBe('path');
-  expect(e.filePath).toBe('/a/very/long/path');
+  expect(e.filePath).toBe(createSafePath('/a/very/long/path'));
   expect(e.children).toBeUndefined();
 });
 
@@ -25,16 +30,16 @@ function readManifests(rootFolder: string) {
 }
 
 test('read-files', () => {
-  const {resourceMap, fileMap, files} = readManifests('src/redux/utils/__test__/manifests/argo-rollouts');
+  const {resourceMap, fileMap, files} = readManifests(createSafePath('src/redux/utils/__test__/manifests/argo-rollouts'));
 
   expect(files.length).toBe(7);
   expect(Object.values(fileMap).length).toBe(27);
   expect(getK8sResources(resourceMap, 'Kustomization').length).toBe(5);
-  expect(getResourcesForPath('/base/argo-rollouts-aggregate-roles.yaml', resourceMap).length).toBe(3);
+  expect(getResourcesForPath(createSafePath('/base/argo-rollouts-aggregate-roles.yaml'), resourceMap).length).toBe(3);
 });
 
 test('read-folder-with-one-file', () => {
-  const {resourceMap, fileMap, files} = readManifests('src/redux/utils/__test__/manifests/single');
+  const {resourceMap, fileMap, files} = readManifests(createSafePath('src/redux/utils/__test__/manifests/single'));
 
   expect(files.length).toBe(1);
   expect(Object.values(fileMap).length).toBe(2);
