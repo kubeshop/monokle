c3c159da588703f67375876d90fa2ef040e93aed
Catalin Hoha <catalin.hoha@gmail.com>
2021-07-12 14:38:48 +0300

    add incoming, outgoing and unsatisfied/warning icons as MonoIcon

---
+++ src/components/atoms/MonoIcon/MonoIcon.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+import MonokleIncomingRefs from '@assets/MonokleIncomingRefs.svg';
+import MonokleOutgoingRefs from '@assets/MonokleOutgoingRefs.svg';
+import MonokleWarning from '@assets/MonokleWarning.svg';
+
+export enum MonoIconTypes {
+  IncomingRefs,
+  OutoingRefs,
+  Warning,
+}
+
+export type MonoIconProps = {
+  type: MonoIconTypes;
+  marginRight?: number;
+  marginLeft?: number;
+};
+
+const StyledImage = styled.img`
+  height: 12px;
+`;
+
+const getIconSvg = (type: MonoIconTypes) => {
+  switch (type) {
+    case MonoIconTypes.IncomingRefs:
+      return MonokleIncomingRefs;
+
+    case MonoIconTypes.OutoingRefs:
+      return MonokleOutgoingRefs;
+
+    case MonoIconTypes.Warning:
+      return MonokleWarning;
+
+    default:
+      return null;
+  }
+};
+
+const MonoIcon = (props: MonoIconProps) => {
+  const {type, marginRight, marginLeft} = props;
+
+  const iconSvg = getIconSvg(type);
+
+  if (iconSvg) {
+    return <StyledImage src={iconSvg} style={{marginRight, marginLeft}} alt={MonoIconTypes[type]} />;
+  }
+
+  return null;
+};
+
+export default MonoIcon;
