cce9bfa69b4f4c768d1a42dcc165ba532b35accd
Catalin Hoha <catalin.hoha@gmail.com>
2021-07-15 16:12:05 +0300

    set up and configure electron-store

---
+++ src/utils/electronStore.ts
@@ -0,0 +1,60 @@
+import {PROCESS_ENV} from '@utils/env';
+
+const ElectronStore = require('electron-store');
+
+const schema = {
+  appConfig: {
+    type: 'object',
+    properties: {
+      kubeconfig: {
+        type: 'string',
+      },
+      scanExcludes: {
+        type: 'array',
+        items: {
+          type: 'string',
+        },
+      },
+      fileIncludes: {
+        type: 'array',
+        items: {
+          type: 'string',
+        },
+      },
+      settings: {
+        type: 'object',
+        properties: {
+          theme: {
+            type: 'string',
+          },
+          textSize: {
+            type: 'string',
+          },
+          language: {
+            type: 'string',
+          },
+        },
+      },
+    },
+  },
+};
+
+const defaults = {
+  appConfig: {
+    kubeconfig: PROCESS_ENV.KUBECONFIG,
+    scanExcludes: ['node_modules', '**/.git', '**/pkg/mod/**', '**/.kube'],
+    fileIncludes: ['*.yaml', '*.yml'],
+    settings: {
+      theme: 'dark',
+      textSize: 'medium',
+      language: 'en',
+    },
+  },
+};
+
+const electronStore = new ElectronStore({
+  schema,
+  defaults,
+});
+
+export default electronStore;
