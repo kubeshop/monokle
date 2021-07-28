35567c053dbc3c49841bf283c3dc8f81ab8cbb19
Andrei Sorescu <and.sorescu@gmail.com>
2021-07-28 18:26:33 +0300

    * expand node when it has a highlighted child * scroll to node when it is highlighted

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,3 +4,7 @@ export const generateId = (prefix: string = 'id'): string => {
   lastId += 1;
   return `${prefix}${lastId}`;
 };
+
+export const uniqueArr = <T>(arr: Array<T>): Array<T> => {
+  return Array.from(new Set(arr));
+};
