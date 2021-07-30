eb195e79b19ae9cc17d417382ad4342f8414cacc
olensmar <ole@lensmar.com>
2021-07-30 11:01:06 +0200

    improved error handling for cluster preview

--- src/redux/reducers/thunks.ts
+++ src/redux/reducers/thunks.ts
@@ -75,116 +75,120 @@ export const previewCluster = createAsyncThunk<SetPreviewDataPayload,
   }>('main/previewCluster', async (configPath, thunkAPI) => {
   const state: AppState = thunkAPI.getState().main;
   if (state.previewResource !== configPath) {
-    const kc = new k8s.KubeConfig();
-    kc.loadFromFile(configPath);
-    const k8sAppV1Api = kc.makeApiClient(k8s.AppsV1Api);
-    const k8sCoreV1Api = kc.makeApiClient(k8s.CoreV1Api);
-    const k8sBatchV1Api = kc.makeApiClient(k8s.BatchV1Api);
-    const k8sRbacV1Api = kc.makeApiClient(k8s.RbacAuthorizationV1Api);
-    const k8sNetworkingV1Api = kc.makeApiClient(k8s.NetworkingV1Api);
-    const k8sExtensionsV1Api = kc.makeApiClient(k8s.ApiextensionsV1Api);
 
-    return Promise.allSettled([
-      k8sAppV1Api.listDaemonSetForAllNamespaces().then(res => {
-        return getK8sObjectsAsYaml(res.body.items, 'DaemonSet', 'apps/v1');
-      }),
-      k8sAppV1Api.listDeploymentForAllNamespaces().then(res => {
-        return getK8sObjectsAsYaml(res.body.items, 'Deployment', 'apps/v1');
-      }),
-      k8sAppV1Api.listStatefulSetForAllNamespaces().then(res => {
-        return getK8sObjectsAsYaml(res.body.items, 'StatefuleSet', 'apps/v1');
-      }),
-      k8sAppV1Api.listReplicaSetForAllNamespaces().then(res => {
-        return getK8sObjectsAsYaml(res.body.items, 'ReplicaSet', 'apps/v1');
-      }),
-      k8sCoreV1Api.listConfigMapForAllNamespaces().then(res => {
-        return getK8sObjectsAsYaml(res.body.items, 'ConfigMap', 'v1');
-      }),
-      k8sCoreV1Api.listServiceForAllNamespaces().then(res => {
-        return getK8sObjectsAsYaml(res.body.items, 'Service', 'v1');
-      }),
-      k8sCoreV1Api.listServiceAccountForAllNamespaces().then(res => {
-        return getK8sObjectsAsYaml(res.body.items, 'ServiceAccount', 'v1');
-      }),
-      k8sCoreV1Api.listPersistentVolume().then(res => {
-        return getK8sObjectsAsYaml(res.body.items, 'PersistentVolume', 'v1');
-      }),
-      k8sCoreV1Api.listPersistentVolumeClaimForAllNamespaces().then(res => {
-        return getK8sObjectsAsYaml(res.body.items, 'PersistentVolumeClaim', 'v1');
-      }),
-      k8sCoreV1Api.listPodForAllNamespaces().then(res => {
-        return getK8sObjectsAsYaml(res.body.items, 'Pod', 'v1');
-      }),
-      k8sCoreV1Api.listEndpointsForAllNamespaces().then(res => {
-        return getK8sObjectsAsYaml(res.body.items, 'Endpoints', 'v1');
-      }),
-      k8sCoreV1Api.listSecretForAllNamespaces().then(res => {
-        return getK8sObjectsAsYaml(res.body.items, 'Secret', 'v1');
-      }),
-      k8sCoreV1Api.listReplicationControllerForAllNamespaces().then(res => {
-        return getK8sObjectsAsYaml(res.body.items, 'ReplicationController', 'v1');
-      }),
-      k8sBatchV1Api.listJobForAllNamespaces().then(res => {
-        return getK8sObjectsAsYaml(res.body.items, 'Job', 'batch/v1');
-      }),
-      k8sBatchV1Api.listCronJobForAllNamespaces().then(res => {
-        return getK8sObjectsAsYaml(res.body.items, 'CronJob', 'batch/v1');
-      }),
-      k8sRbacV1Api.listClusterRole().then(res => {
-        return getK8sObjectsAsYaml(res.body.items, 'ClusterRole', 'rbac.authorization.k8s.io/v1');
-      }),
-      k8sRbacV1Api.listClusterRoleBinding().then(res => {
-        return getK8sObjectsAsYaml(res.body.items, 'ClusterRoleBinding', 'rbac.authorization.k8s.io/v1');
-      }),
-      k8sRbacV1Api.listRoleForAllNamespaces().then(res => {
-        return getK8sObjectsAsYaml(res.body.items, 'Role', 'rbac.authorization.k8s.io/v1');
-      }),
-      k8sRbacV1Api.listRoleBindingForAllNamespaces().then(res => {
-        return getK8sObjectsAsYaml(res.body.items, 'RoleBinding', 'rbac.authorization.k8s.io/v1');
-      }),
-      k8sNetworkingV1Api.listIngressForAllNamespaces().then(res => {
-        return getK8sObjectsAsYaml(res.body.items, 'Ingress', 'networking.k8s.io/v1');
-      }),
-      k8sNetworkingV1Api.listNetworkPolicyForAllNamespaces().then(res => {
-        return getK8sObjectsAsYaml(res.body.items, 'NetworkPolicy', 'networking.k8s.io/v1');
-      }),
-      k8sExtensionsV1Api.listCustomResourceDefinition().then(res => {
-        return getK8sObjectsAsYaml(res.body.items, 'CustomResourceDefinition', 'apiextensions.k8s.io/v1');
-      }),
-    ]).then(results => {
-      const fulfilledResults = results.filter(r => r.status === 'fulfilled' && r.value);
+    try {
+      const kc = new k8s.KubeConfig();
+      kc.loadFromFile(configPath);
+      const k8sAppV1Api = kc.makeApiClient(k8s.AppsV1Api);
+      const k8sCoreV1Api = kc.makeApiClient(k8s.CoreV1Api);
+      const k8sBatchV1Api = kc.makeApiClient(k8s.BatchV1Api);
+      const k8sRbacV1Api = kc.makeApiClient(k8s.RbacAuthorizationV1Api);
+      const k8sNetworkingV1Api = kc.makeApiClient(k8s.NetworkingV1Api);
+      const k8sExtensionsV1Api = kc.makeApiClient(k8s.ApiextensionsV1Api);
 
-      if (fulfilledResults.length === 0) {
-        // @ts-ignore
-        return createPreviewRejection(thunkAPI, 'Cluster Resources Failed', results[0].reason ? results[0].reason.toString() : JSON.stringify(results[0]));
-      }
+      return Promise.allSettled([
+        k8sAppV1Api.listDaemonSetForAllNamespaces().then(res => {
+          return getK8sObjectsAsYaml(res.body.items, 'DaemonSet', 'apps/v1');
+        }),
+        k8sAppV1Api.listDeploymentForAllNamespaces().then(res => {
+          return getK8sObjectsAsYaml(res.body.items, 'Deployment', 'apps/v1');
+        }),
+        k8sAppV1Api.listStatefulSetForAllNamespaces().then(res => {
+          return getK8sObjectsAsYaml(res.body.items, 'StatefuleSet', 'apps/v1');
+        }),
+        k8sAppV1Api.listReplicaSetForAllNamespaces().then(res => {
+          return getK8sObjectsAsYaml(res.body.items, 'ReplicaSet', 'apps/v1');
+        }),
+        k8sCoreV1Api.listConfigMapForAllNamespaces().then(res => {
+          return getK8sObjectsAsYaml(res.body.items, 'ConfigMap', 'v1');
+        }),
+        k8sCoreV1Api.listServiceForAllNamespaces().then(res => {
+          return getK8sObjectsAsYaml(res.body.items, 'Service', 'v1');
+        }),
+        k8sCoreV1Api.listServiceAccountForAllNamespaces().then(res => {
+          return getK8sObjectsAsYaml(res.body.items, 'ServiceAccount', 'v1');
+        }),
+        k8sCoreV1Api.listPersistentVolume().then(res => {
+          return getK8sObjectsAsYaml(res.body.items, 'PersistentVolume', 'v1');
+        }),
+        k8sCoreV1Api.listPersistentVolumeClaimForAllNamespaces().then(res => {
+          return getK8sObjectsAsYaml(res.body.items, 'PersistentVolumeClaim', 'v1');
+        }),
+        k8sCoreV1Api.listPodForAllNamespaces().then(res => {
+          return getK8sObjectsAsYaml(res.body.items, 'Pod', 'v1');
+        }),
+        k8sCoreV1Api.listEndpointsForAllNamespaces().then(res => {
+          return getK8sObjectsAsYaml(res.body.items, 'Endpoints', 'v1');
+        }),
+        k8sCoreV1Api.listSecretForAllNamespaces().then(res => {
+          return getK8sObjectsAsYaml(res.body.items, 'Secret', 'v1');
+        }),
+        k8sCoreV1Api.listReplicationControllerForAllNamespaces().then(res => {
+          return getK8sObjectsAsYaml(res.body.items, 'ReplicationController', 'v1');
+        }),
+        k8sBatchV1Api.listJobForAllNamespaces().then(res => {
+          return getK8sObjectsAsYaml(res.body.items, 'Job', 'batch/v1');
+        }),
+        k8sBatchV1Api.listCronJobForAllNamespaces().then(res => {
+          return getK8sObjectsAsYaml(res.body.items, 'CronJob', 'batch/v1');
+        }),
+        k8sRbacV1Api.listClusterRole().then(res => {
+          return getK8sObjectsAsYaml(res.body.items, 'ClusterRole', 'rbac.authorization.k8s.io/v1');
+        }),
+        k8sRbacV1Api.listClusterRoleBinding().then(res => {
+          return getK8sObjectsAsYaml(res.body.items, 'ClusterRoleBinding', 'rbac.authorization.k8s.io/v1');
+        }),
+        k8sRbacV1Api.listRoleForAllNamespaces().then(res => {
+          return getK8sObjectsAsYaml(res.body.items, 'Role', 'rbac.authorization.k8s.io/v1');
+        }),
+        k8sRbacV1Api.listRoleBindingForAllNamespaces().then(res => {
+          return getK8sObjectsAsYaml(res.body.items, 'RoleBinding', 'rbac.authorization.k8s.io/v1');
+        }),
+        k8sNetworkingV1Api.listIngressForAllNamespaces().then(res => {
+          return getK8sObjectsAsYaml(res.body.items, 'Ingress', 'networking.k8s.io/v1');
+        }),
+        k8sNetworkingV1Api.listNetworkPolicyForAllNamespaces().then(res => {
+          return getK8sObjectsAsYaml(res.body.items, 'NetworkPolicy', 'networking.k8s.io/v1');
+        }),
+        k8sExtensionsV1Api.listCustomResourceDefinition().then(res => {
+          return getK8sObjectsAsYaml(res.body.items, 'CustomResourceDefinition', 'apiextensions.k8s.io/v1');
+        }),
+      ]).then(results => {
+        const fulfilledResults = results.filter(r => r.status === 'fulfilled' && r.value);
 
-      // @ts-ignore
-      const allYaml = fulfilledResults.map(r => r.value).join(YAML_DOCUMENT_DELIMITER);
-      const previewResult = createPreviewResult(allYaml, configPath);
-
-      if (fulfilledResults.length < results.length) {
-        const rejectedResult = results.find(r => r.status === 'rejected');
-        if (rejectedResult) {
+        if (fulfilledResults.length === 0) {
           // @ts-ignore
-          const reason = rejectedResult.reason ? rejectedResult.reason.toString() : JSON.stringify(rejectedResult);
-
-          return {
-            alert: {
-              title: 'Get Cluster Resources',
-              message: `Failed to get all cluster resources: ${reason}`,
-              type: AlertEnum.Warning,
-            },
-            ...previewResult,
-          };
+          return createPreviewRejection(thunkAPI, 'Cluster Resources Failed', results[0].reason ? results[0].reason.toString() : JSON.stringify(results[0]));
         }
-      }
 
-      return previewResult;
+        // @ts-ignore
+        const allYaml = fulfilledResults.map(r => r.value).join(YAML_DOCUMENT_DELIMITER);
+        const previewResult = createPreviewResult(allYaml, configPath);
 
-    }, (reason) => {
-      return createPreviewRejection(thunkAPI, 'Cluster Resources Failed', reason.message);
-    });
+        if (fulfilledResults.length < results.length) {
+          const rejectedResult = results.find(r => r.status === 'rejected');
+          if (rejectedResult) {
+            // @ts-ignore
+            const reason = rejectedResult.reason ? rejectedResult.reason.toString() : JSON.stringify(rejectedResult);
+
+            return {
+              alert: {
+                title: 'Get Cluster Resources',
+                message: `Failed to get all cluster resources: ${reason}`,
+                type: AlertEnum.Warning,
+              },
+              ...previewResult,
+            };
+          }
+        }
+
+        return previewResult;
+      }, (reason) => {
+        return createPreviewRejection(thunkAPI, 'Cluster Resources Failed', reason.message);
+      });
+    } catch (e) {
+      return createPreviewRejection(thunkAPI, 'Cluster Resources Failed', e.message);
+    }
   }
 
   return {};
