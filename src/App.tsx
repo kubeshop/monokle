0430c5af922cbb3137651b80554cdd9d6d630798
steenkamby <steenkamby@live.dk>
2021-07-30 09:43:03 +0200

    replace radio with buttons, add real panes for logger and graph

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,66 @@
 import React, {useEffect, useState} from 'react';
 import styled from 'styled-components';
 import 'antd/dist/antd.less';
-import {Space, Radio, RadioChangeEvent} from 'antd';
-import {ClusterOutlined, FolderOpenOutlined, NodeIndexOutlined, CodeOutlined} from '@ant-design/icons';
+import {Button, Space} from 'antd';
+import {ClusterOutlined, FolderOpenOutlined, ApartmentOutlined, CodeOutlined} from '@ant-design/icons';
 import 'react-reflex/styles.css';
 import {ReflexContainer, ReflexSplitter, ReflexElement} from 'react-reflex';
 
 import Colors, {BackgroundColors} from '@styles/Colors';
 import {AppBorders} from '@styles/Borders';
-import Layout from '@atoms/Layout';
-import PageHeader from '@organisms/PageHeader';
-import Content from '@atoms/Content';
-import Row from '@atoms/Row';
-import PageFooter from '@organisms/PageFooter';
-import ActionsPane from '@organisms/ActionsPane';
-import NavigatorPane from '@organisms/NavigatorPane';
-import FileTreePane from '@organisms/FileTreePane';
-import MessageBox from '@organisms/MessageBox';
-import SettingsDrawer from '@organisms/SettingsDrawer';
+import {Layout, Row, Content} from '@atoms';
+import {
+  PageHeader,
+  PageFooter,
+  ActionsPane,
+  NavigatorPane,
+  FileTreePane,
+  MessageBox,
+  SettingsDrawer,
+  DiffModal,
+} from '@organisms';
+import {LogViewer, GraphView} from '@molecules';
 import {Size} from '@models/window';
-import DiffModal from '@organisms/DiffModal';
 import {useWindowSize} from '@utils/hooks';
 import {useAppDispatch} from '@redux/hooks';
 import {initKubeconfig} from '@redux/reducers/appConfig';
+import featureJson from '@src/feature-flags.json';
 
 const StyledReflexContainer = styled(ReflexContainer)`
   &.reflex-container {
     margin-top: 0px;
   }
-  .ant-radio-button-wrapper {
+  .ant-btn {
+    line-height: 1.5715;
     position: relative;
     display: inline-block;
-    height: 32px;
-    margin: 0;
-    padding: 8px;
-    color: rgba(255, 255, 255, 0.85);
-    font-size: 14px;
-    line-height: 30px;
-    background: transparent;
-    border: 0px;
+    font-weight: 400;
+    white-space: nowrap;
+    text-align: center;
+    background-image: none;
+    border: 1px solid transparent;
+    box-shadow: 0 2px 0 rgb(0 0 0 / 2%);
     cursor: pointer;
-    transition: color 0.3s, background 0.3s, border-color 0.3s, box-shadow 0.3s;
+    transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    touch-action: manipulation;
+    height: 40px;
+    padding: 8px;
+    font-size: 14px;
+    border-radius: 2px;
+    color: rgba(255, 255, 255, 0.85);
+    background: transparent;
   }
-  .ant-radio-button-wrapper:first-child:last-child {
-    border: 0px;
+  .ant-btn:hover,
+  .ant-btn:focus {
+    color: #165996;
+    background: transparent;
+    border-color: #165996;
   }
 `;
-/*
-const StyledReflexSplitter = styled(ReflexSplitter)`
-  border-left: ${AppBorders.pageDivider};
-  border-right: ${AppBorders.pageDivider};
-  background-color: ${Colors.grey3};
-`;
-*/
 
 const StyledReflexElement = styled(ReflexElement)`
   height: 100%;
@@ -112,6 +120,10 @@ const App = () => {
 
   const [leftMenuSelection, setLeftMenuSelection] = useState('');
   const [rightMenuSelection, setRightMenuSelection] = useState('');
+  const [rightPaneWidth, setRightPaneWidth] = useState('0%');
+  const [navPaneWidth, setNavPaneWidth] = useState('50%');
+  const [editPaneWidth, setEditPaneWidth] = useState('50%');
+  const [leftPaneWidth, setLeftPaneWidth] = useState('0%');
 
   const mainHeight = `${size.height ? size.height : 100}px`;
   const contentHeight = `${size.height ? size.height - 75 : 75}px`;
@@ -120,16 +132,14 @@ const App = () => {
     dispatch(initKubeconfig());
   }, []);
 
-  const onChangeLeftMenuSelection = (e: RadioChangeEvent) => {
-    const newSelection = e.target.value;
-    console.log('left menu checked', newSelection);
-    setLeftMenuSelection(newSelection === leftMenuSelection ? '' : newSelection);
-  };
-
-  const onChangeRightMenuSelection = (e: RadioChangeEvent) => {
-    const newSelection = e.target.value;
-    console.log('right menu checked', newSelection);
-    setRightMenuSelection(newSelection === rightMenuSelection ? '' : newSelection);
+  const setAspectRatios = () => {
+    /*
+      Possible configurations (left, right) -> left: 25%, nav: 25%, edit:25%, right:25%
+      closed, closed -> left: 0%, nav: 50%, edit:50%, right:0% (default)
+      open, closed -> left: 33%, nav: 33%, edit:33%, right:0%
+      closed, open -> left: 0%, nav: 33%, edit:33%, right:33%
+      open, open -> left: 25%, nav: 25%, edit:25%, right:25%
+    */
   };
 
   return (
@@ -143,35 +153,46 @@ const App = () => {
           <StyledRow style={{height: contentHeight + 4}}>
             <StyledReflexContainer orientation="vertical" windowResizeAware>
               <StyledMenuLeftReflexElement size={43}>
-                <Radio.Group onChange={onChangeLeftMenuSelection} value={leftMenuSelection}>
-                  <Space direction="vertical">
-                    <Radio.Button
-                      value="file-explorer"
-                      style={{
-                        ...iconStyle,
-                        color: leftMenuSelection === 'file-explorer' ? Colors.whitePure : Colors.grey7,
-                      }}
-                    >
-                      <FolderOpenOutlined />
-                    </Radio.Button>
-                    <Radio.Button
-                      value="cluster-explorer"
-                      style={{
-                        ...iconStyle,
-                        color: leftMenuSelection === 'cluster-explorer' ? Colors.whitePure : Colors.grey7,
-                      }}
-                    >
-                      <ClusterOutlined />
-                    </Radio.Button>
-                  </Space>
-                </Radio.Group>
+                <Space direction="vertical">
+                  <Button
+                    size="large"
+                    onClick={() => setLeftMenuSelection(leftMenuSelection === 'file-explorer' ? '' : 'file-explorer')}
+                    icon={
+                      <FolderOpenOutlined
+                        style={{
+                          ...iconStyle,
+                          color: leftMenuSelection === 'file-explorer' ? Colors.whitePure : Colors.grey7,
+                        }}
+                      />
+                    }
+                  />
+                  {featureJson.ShowGraphView && (
+                    <Button
+                      size="large"
+                      onClick={() =>
+                        setLeftMenuSelection(leftMenuSelection === 'cluster-explorer' ? '' : 'cluster-explorer')
+                      }
+                      icon={
+                        <ClusterOutlined
+                          style={{
+                            ...iconStyle,
+                            color: leftMenuSelection === 'cluster-explorer' ? Colors.whitePure : Colors.grey7,
+                          }}
+                        />
+                      }
+                    />
+                  )}
+                </Space>
               </StyledMenuLeftReflexElement>
               <StyledReflexElement
-                minSize={leftMenuSelection === 'file-explorer' ? 200 : 0}
-                maxSize={leftMenuSelection === 'file-explorer' ? 1000 : 0}
-                size={leftMenuSelection !== 'file-explorer' ? 0 : 600}
+                minSize={leftMenuSelection !== '' ? 400 : 0}
+                maxSize={leftMenuSelection !== '' ? 1000 : 0}
+                size={leftMenuSelection === '' ? 0 : 600}
               >
-                <FileTreePane windowHeight={size.height} />
+                {leftMenuSelection === 'file-explorer' ? <FileTreePane windowHeight={size.height} /> : undefined}
+                {featureJson.ShowClusterView && leftMenuSelection === 'cluster-explorer' ? (
+                  <div>cluster buster</div>
+                ) : undefined}
               </StyledReflexElement>
 
               <ReflexSplitter />
@@ -189,28 +210,43 @@ const App = () => {
               <ReflexSplitter />
 
               <StyledReflexElement
-                minSize={rightMenuSelection === 'graph' ? 200 : 0}
-                maxSize={rightMenuSelection === 'graph' ? 1000 : 0}
-                size={rightMenuSelection !== 'graph' ? 0 : 400}
+                minSize={rightMenuSelection !== '' ? 200 : 0}
+                maxSize={rightMenuSelection !== '' ? 1000 : 0}
+                size={rightMenuSelection === '' ? 0 : 600}
               >
-                <div style={{width: rightMenuSelection !== 'graph' ? 0 : 400}}>no pane no gane</div>
+                {featureJson.ShowGraphView && rightMenuSelection === 'graph' ? (
+                  <GraphView editorHeight={contentHeight} />
+                ) : undefined}
+                {rightMenuSelection === 'logs' ? <LogViewer editorHeight={contentHeight} /> : undefined}
               </StyledReflexElement>
 
               <StyledMenuRightReflexElement size={43}>
-                <Radio.Group onChange={onChangeRightMenuSelection} value={rightMenuSelection}>
-                  <Space direction="vertical">
-                    <Radio.Button value="graph">
-                      <NodeIndexOutlined
-                        style={{...iconStyle, color: rightMenuSelection === 'graph' ? Colors.whitePure : Colors.grey7}}
-                      />
-                    </Radio.Button>
-                    <Radio.Button value="logs">
+                <Space direction="vertical">
+                  {featureJson.ShowGraphView && (
+                    <Button
+                      size="large"
+                      onClick={() => setRightMenuSelection(rightMenuSelection === 'graph' ? '' : 'graph')}
+                      icon={
+                        <ApartmentOutlined
+                          style={{
+                            ...iconStyle,
+                            color: rightMenuSelection === 'graph' ? Colors.whitePure : Colors.grey7,
+                          }}
+                        />
+                      }
+                    />
+                  )}
+
+                  <Button
+                    size="large"
+                    onClick={() => setRightMenuSelection(rightMenuSelection === 'logs' ? '' : 'logs')}
+                    icon={
                       <CodeOutlined
                         style={{...iconStyle, color: rightMenuSelection === 'logs' ? Colors.whitePure : Colors.grey7}}
                       />
-                    </Radio.Button>
-                  </Space>
-                </Radio.Group>
+                    }
+                  />
+                </Space>
               </StyledMenuRightReflexElement>
             </StyledReflexContainer>
           </StyledRow>
---
+++ src/components/organisms/index.ts
@@ -0,0 +1,8 @@
+export {default as ActionsPane} from './ActionsPane';
+export {default as DiffModal} from './DiffModal';
+export {default as FileTreePane} from './FileTreePane';
+export {default as MessageBox} from './MessageBox';
+export {default as NavigatorPane} from './NavigatorPane';
+export {default as PageFooter} from './PageFooter';
+export {default as PageHeader} from './PageHeader';
+export {default as SettingsDrawer} from './SettingsDrawer';
