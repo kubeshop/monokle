ddad80c5cd8c776dc7695ad9d66c0375c3f8a5bf
Catalin Hoha <20538711+catalinhoha@users.noreply.github.com>
2021-08-23 15:12:08 +0300

    feat: show refs decorations in monaco editor overview ruler

--- src/components/molecules/Monaco/editorConstants.ts
+++ src/components/molecules/Monaco/editorConstants.ts
@@ -24,9 +24,17 @@ const glyphDecorationOptionsByType: Record<GlyphDecorationTypes, monaco.editor.I
 const inlineDecorationOptionsByType = {
   [GlyphDecorationTypes.UnsatisfiedRef]: {
     inlineClassName: 'monokleEditorUnsatisfiedRefInlineClass',
+    overviewRuler: {
+      position: monaco.editor.OverviewRulerLane.Left,
+      color: '#ad8b00',
+    },
   },
   [GlyphDecorationTypes.SatisfiedRef]: {
     inlineClassName: 'monokleEditorSatisfiedRefInlineClass',
+    overviewRuler: {
+      position: monaco.editor.OverviewRulerLane.Right,
+      color: '#3C8618',
+    },
   },
 };
 
