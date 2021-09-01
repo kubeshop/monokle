e21264fb2eafc8a78ad2d4c33828831d8fa434e4
olensmar <ole@lensmar.com>
2021-09-01 20:40:16 +0200

    fix: validation improvements
    Updated validation to use ajv and introduced
    schema/validation caches for improved performance. Updated yaml dependencies.

--- src/redux/services/schema.ts
+++ src/redux/services/schema.ts
@@ -6,6 +6,7 @@ import {getResourceKindHandler} from '@src/kindhandlers';
 
 const k8sSchema = JSON.parse(loadResource('schemas/k8sschemas.json'));
 const kustomizeSchema = JSON.parse(loadResource('schemas/kustomization.json'));
+const schemaCache = new Map<string, any>();
 
 /**
  * Returns a JSON Schema for the specified resource kind
@@ -19,22 +20,29 @@ export function getResourceSchema(resource: K8sResource) {
   const prefix = resourceKindHandler?.validationSchemaPrefix;
 
   if (prefix) {
-    const kindSchema = k8sSchema['definitions'][`${prefix}.${resource.kind}`];
-    if (kindSchema) {
-      Object.keys(k8sSchema).forEach(key => {
-        if (key !== 'definitions') {
-          delete k8sSchema[key];
-        }
-      });
+    const schemaKey = `${prefix}.${resource.kind}`;
+    if (!schemaCache.has(schemaKey)) {
+      const kindSchema = k8sSchema['definitions'][schemaKey];
+      if (kindSchema) {
+        Object.keys(k8sSchema).forEach(key => {
+          if (key !== 'definitions') {
+            delete k8sSchema[key];
+          }
+        });
 
-      Object.keys(kindSchema).forEach(key => {
-        k8sSchema[key] = JSON.parse(JSON.stringify(kindSchema[key]));
-      });
+        Object.keys(kindSchema).forEach(key => {
+          k8sSchema[key] = JSON.parse(JSON.stringify(kindSchema[key]));
+        });
 
-      return k8sSchema;
+        schemaCache.set(schemaKey, JSON.parse(JSON.stringify(k8sSchema)));
+      }
+    }
+
+    if (schemaCache.has(schemaKey)) {
+      return schemaCache.get(schemaKey);
     }
   }
 
-  log.error(`Failed to find schema for resource of kind ${resource.kind}`);
+  log.warn(`Failed to find schema for resource of kind ${resource.kind}`);
   return undefined;
 }
