25ea58ea7a950273df206af3e0048c3dfd29b559
Catalin Hoha <20538711+catalinhoha@users.noreply.github.com>
2021-09-13 10:11:27 +0300

    feat: new NavSection models

---
+++ src/models/navsection.ts
@@ -0,0 +1,16 @@
+export interface NavSectionItemHandlers<ItemType, ScopeType> {
+  getName: <S extends ScopeType>(item: ItemType, scope: S) => string;
+  getIdentifier: <S extends ScopeType>(item: ItemType, scope: S) => string;
+  isSelected?: <S extends ScopeType>(item: ItemType, scope: S) => boolean;
+  isHighlighted?: <S extends ScopeType>(item: ItemType, scope: S) => boolean;
+  onClick?: <S extends ScopeType>(item: ItemType, scope: S) => void;
+}
+
+export interface NavSection<ItemType, ScopeType = any> {
+  name: string;
+  useScope: () => ScopeType;
+  subsections?: NavSection<ItemType, ScopeType>[];
+  getItems?: <S extends ScopeType>(scope: S) => ItemType[];
+  getItemsGrouped?: <S extends ScopeType>(scope: S) => Record<string, ItemType[]>;
+  itemHandlers?: NavSectionItemHandlers<ItemType, ScopeType>;
+}
