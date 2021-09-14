5b68c3189517c5de9bd8714d2ea9a37923a0babb
Catalin Hoha <20538711+catalinhoha@users.noreply.github.com>
2021-09-14 13:48:58 +0300

    refactor: create NavSectionScopedItemMethod type, add prefix/suffix for items

--- src/models/navsection.ts
+++ src/models/navsection.ts
@@ -1,10 +1,19 @@
+import React from 'react';
+
+export type NavSectionScopedItemMethod<ItemType, ScopeType, MethodReturnType> = <S extends ScopeType>(
+  item: ItemType,
+  scope: S
+) => MethodReturnType;
+
 export interface NavSectionItemHandler<ItemType, ScopeType> {
-  getName: <S extends ScopeType>(item: ItemType, scope: S) => string;
-  getIdentifier: <S extends ScopeType>(item: ItemType, scope: S) => string;
-  isSelected?: <S extends ScopeType>(item: ItemType, scope: S) => boolean;
-  isHighlighted?: <S extends ScopeType>(item: ItemType, scope: S) => boolean;
-  isVisible?: <S extends ScopeType>(item: ItemType, scope: S) => boolean;
-  onClick?: <S extends ScopeType>(item: ItemType, scope: S) => void;
+  getName: (item: ItemType) => string;
+  getIdentifier: (item: ItemType) => string;
+  isSelected?: NavSectionScopedItemMethod<ItemType, ScopeType, boolean>;
+  isHighlighted?: NavSectionScopedItemMethod<ItemType, ScopeType, boolean>;
+  isVisible?: NavSectionScopedItemMethod<ItemType, ScopeType, boolean>;
+  onClick?: NavSectionScopedItemMethod<ItemType, ScopeType, void>;
+  prefix?: React.ReactNode;
+  suffix?: React.ReactNode;
 }
 
 export interface NavSection<ItemType, ScopeType = any> {
