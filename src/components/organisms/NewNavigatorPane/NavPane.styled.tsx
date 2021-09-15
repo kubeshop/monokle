ac16a57db4573531aa06513f0c102b9a27137fc5
Catalin Hoha <20538711+catalinhoha@users.noreply.github.com>
2021-09-15 13:06:12 +0300

    feat: add TitleBar to new NavPane

--- src/components/organisms/NewNavigatorPane/NavPane.styled.tsx
+++ src/components/organisms/NewNavigatorPane/NavPane.styled.tsx
@@ -1,4 +1,7 @@
 import styled from 'styled-components';
+import {Button} from 'antd';
+import {AppBorders} from '@styles/Borders';
+import {BackgroundColors} from '@styles/Colors';
 
 export const List = styled.ol<{height: number}>`
   list-style-type: none;
@@ -10,3 +13,25 @@ export const List = styled.ol<{height: number}>`
   }
   ${props => `height: ${props.height}px;`}
 `;
+
+export const TitleBar = styled.div`
+  display: flex;
+  height: 24px;
+  justify-content: space-between;
+  border-bottom: ${AppBorders.sectionDivider};
+  width: 100%;
+  height: 40px;
+  margin: 0;
+  padding: 0;
+  background: ${BackgroundColors.darkThemeBackground};
+`;
+
+export const TitleBarRightButtons = styled.div`
+  float: right;
+  display: flex;
+  align-items: center;
+`;
+
+export const PlusButton = styled(Button)``;
+
+export const FilterButton = styled(Button)``;
--- src/components/organisms/NewNavigatorPane/NavPane.tsx
+++ src/components/organisms/NewNavigatorPane/NavPane.tsx
@@ -1,19 +1,62 @@
-import React, {useContext} from 'react';
+import React, {useCallback, useContext} from 'react';
 import AppContext from '@src/AppContext';
 import {K8sResource} from '@models/k8sresource';
 import {HelmValuesFile} from '@models/helm';
 import {KustomizationNavSection, KustomizationNavSectionScope} from '@src/navsections/Kustomization.nav';
 import {HelmChartNavSection, HelmChartNavSectionScope} from '@src/navsections/HelmChart.nav';
 import {K8sResourceNavSection, K8sResourceNavSectionScope} from '@src/navsections/K8sResource.nav';
+import {Popover} from 'antd';
+import {PlusOutlined, FilterOutlined} from '@ant-design/icons';
+import ResourceFilter from '@components/molecules/ResourceFilter';
+import {useAppDispatch, useAppSelector} from '@redux/hooks';
+import {NAVIGATOR_HEIGHT_OFFSET, ROOT_FILE_ENTRY} from '@constants/constants';
+import {useSelector} from 'react-redux';
+import {isInClusterModeSelector, isInPreviewModeSelector} from '@redux/selectors';
+import {openNewResourceWizard} from '@redux/reducers/ui';
+import {MonoPaneTitle} from '@components/atoms';
 import NavSectionRenderer from './NavSectionRenderer';
 import * as S from './NavPane.styled';
 
-const NavSectionsPane = () => {
+const NavPane = () => {
+  const dispatch = useAppDispatch();
   const {windowSize} = useContext(AppContext);
   const windowHeight = windowSize.height;
-  const navigatorHeight = windowHeight - 100;
+  const navigatorHeight = windowHeight - NAVIGATOR_HEIGHT_OFFSET;
+
+  const fileMap = useAppSelector(state => state.main.fileMap);
+  const isInClusterMode = useSelector(isInClusterModeSelector);
+  const isInPreviewMode = useSelector(isInPreviewModeSelector);
+
+  const doesRootFileEntryExist = useCallback(() => {
+    return Boolean(fileMap[ROOT_FILE_ENTRY]);
+  }, [fileMap]);
+
+  const onClickNewResource = () => {
+    dispatch(openNewResourceWizard());
+  };
+
   return (
     <nav>
+      <S.TitleBar>
+        <MonoPaneTitle>Navigator</MonoPaneTitle>
+        <S.TitleBarRightButtons>
+          <S.PlusButton
+            disabled={!doesRootFileEntryExist() || isInClusterMode || isInPreviewMode}
+            onClick={onClickNewResource}
+            type="link"
+            size="small"
+            icon={<PlusOutlined />}
+          />
+          <Popover content={<ResourceFilter onChange={() => {}} />} trigger="click">
+            <S.FilterButton
+              disabled={!doesRootFileEntryExist() && !isInClusterMode && !isInPreviewMode}
+              type="link"
+              size="small"
+              icon={<FilterOutlined />}
+            />
+          </Popover>
+        </S.TitleBarRightButtons>
+      </S.TitleBar>
       <S.List height={navigatorHeight}>
         <NavSectionRenderer<HelmValuesFile, HelmChartNavSectionScope> navSection={HelmChartNavSection} level={0} />
         <NavSectionRenderer<K8sResource, KustomizationNavSectionScope> navSection={KustomizationNavSection} level={0} />
@@ -23,4 +66,4 @@ const NavSectionsPane = () => {
   );
 };
 
-export default NavSectionsPane;
+export default NavPane;
