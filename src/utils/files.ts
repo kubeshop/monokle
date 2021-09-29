74b6d5ef7e0271ac7e3512465fad7c955ae0bce3
Erdi Köse <me@erdkse.com>
2021-09-29 15:02:41 +0300

    fix: Check env file existance

--- src/utils/files.ts
+++ src/utils/files.ts
@@ -1,6 +1,18 @@
 import path from 'path';
+import fs from 'fs';
 
 export function isSubDirectory(parentDir: string, dir: string) {
   const relative = path.relative(parentDir, dir);
   return relative && !relative.startsWith('..') && !path.isAbsolute(relative);
+}
+
+export function doesFileExist(filePath: string) {
+  try {
+    if (fs.existsSync(filePath)) {
+      return true;
+    }
+    return false;
+  } catch (err) {
+    return false;
+  }
 }
