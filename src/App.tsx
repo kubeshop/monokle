681f932f6d0e942e9af997b874b4cd7c12c33e84
Erdi Köse <me@erdkse.com>
2021-10-13 16:52:00 +0300

    fix: Move mixxing dependency logic to main process

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect} from 'react';
+import React from 'react';
 import 'antd/dist/antd.less';
 import {Layout} from '@atoms';
 import {
@@ -15,38 +15,14 @@ import {
 } from '@organisms';
 import {Size} from '@models/window';
 import {useWindowSize} from '@utils/hooks';
-import {useAppDispatch} from '@redux/hooks';
-import {ipcRenderer} from 'electron';
-import {setAlert} from '@redux/reducers/alert';
-import {AlertEnum, AlertType} from '@models/alert';
 import ValidationErrorsModal from '@components/molecules/ValidationErrorsModal';
 import UpdateModal from '@components/organisms/UpdateModal';
 import AppContext from './AppContext';
 
 const App = () => {
-  const dispatch = useAppDispatch();
   const size: Size = useWindowSize();
 
   const mainHeight = `${size.height}px`;
-
-  const onMissingDependencyResult = useCallback(
-    (_, {dependencies}) => {
-      const alert: AlertType = {
-        type: AlertEnum.Warning,
-        title: 'Missing dependency',
-        message: `${dependencies.toString()} must be installed for all Monokle functionality to be available`,
-      };
-      dispatch(setAlert(alert));
-    },
-    [dispatch]
-  );
-
-  useEffect(() => {
-    ipcRenderer.on('missing-dependency-result', onMissingDependencyResult);
-    return () => {
-      ipcRenderer.removeListener('missing-dependency-result', onMissingDependencyResult);
-    };
-  }, [onMissingDependencyResult]);
 
   return (
     <AppContext.Provider value={{windowSize: size}}>
