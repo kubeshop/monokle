1e98179c87ba884149cb070320ecfac0e10beb01
Erdi Köse <me@erdkse.com>
2021-10-19 22:58:44 +0300

    chore: Add ignoreInitial option to watcher

--- src/redux/services/kubeConfigMonitor.ts
+++ src/redux/services/kubeConfigMonitor.ts
@@ -3,30 +3,24 @@ import {AppDispatch} from '@redux/store';
 import {loadContexts} from '@redux/thunks/loadKubeConfig';
 
 let watcher: FSWatcher;
-let initializing = false;
 
 export function monitorKubeConfig(filePath: string, dispatch: AppDispatch) {
   if (watcher) {
     watcher.close();
   }
 
-  initializing = true;
   watcher = watch(filePath, {
     persistent: true,
     usePolling: true,
     interval: 1000,
+    ignoreInitial: true,
   });
 
   watcher.on('all', () => {
-    if (!initializing) {
-      dispatch(loadContexts(filePath));
-    }
+    dispatch(loadContexts(filePath));
   });
 
   watcher
     /* eslint-disable no-console */
-    .on('error', error => console.log(`Watcher error: ${error}`))
-    .on('ready', () => {
-      initializing = false;
-    });
+    .on('error', error => console.log(`Watcher error: ${error}`));
 }
