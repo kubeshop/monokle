b3190d3f45697c7ee48d15884189ef8c6a895595
Catalin Hoha <20538711+catalinhoha@users.noreply.github.com>
2021-11-05 18:36:18 +0200

    chore: add new ignored path for cluster resources

--- src/constants/clusterResource.ts
+++ src/constants/clusterResource.ts
@@ -1,6 +1,7 @@
 export const CLUSTER_RESOURCE_IGNORED_PATHS = [
   '...creationTimestamp',
   'metadata#annotations#kubectl.kubernetes.io/last-applied-configuration',
+  'metadata#annotations#deployment.kubernetes.io/revision',
   'metadata#resourceVersion',
   'metadata#uid',
   'metadata#generation',
