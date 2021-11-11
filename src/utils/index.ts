61e7f320281e3301abd3ee6d3a45e605205b49cf
olensmar <ole@lensmar.com>
2021-11-11 08:29:46 +0100

    fix: changed dependency-check to use execSync with node env

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,7 @@
+import {execSync} from 'child_process';
+
+import {PROCESS_ENV} from '@utils/env';
+
 const {which} = require('shelljs');
 
 let lastId: number = 0;
@@ -12,4 +16,16 @@ export const uniqueArr = <T>(arr: Array<T>): Array<T> => {
 };
 
 export const checkMissingDependencies = (dependencies: Array<string>): Array<string> =>
-  dependencies.filter(d => !which(d));
+  dependencies.filter(d => {
+    try {
+      execSync(d, {
+        env: {
+          NODE_ENV: PROCESS_ENV.NODE_ENV,
+          PUBLIC_URL: PROCESS_ENV.PUBLIC_URL,
+        },
+      });
+      return false;
+    } catch (e) {
+      return true;
+    }
+  });
