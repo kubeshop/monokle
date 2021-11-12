fc970bcf6ce412bc01bcccc1cf3601f4d083cd1e
Catalin Hoha <20538711+catalinhoha@users.noreply.github.com>
2021-11-12 14:01:50 +0200

    feat: show message when no resources match filters

--- src/navsections/K8sResourceSectionBlueprint/K8sResourceSectionEmptyDisplay.tsx
+++ src/navsections/K8sResourceSectionBlueprint/K8sResourceSectionEmptyDisplay.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import {useAppSelector} from '@redux/hooks';
+import {activeResourcesSelector} from '@redux/selectors';
+
 function K8sResourceSectionEmptyDisplay() {
+  const activeResources = useAppSelector(activeResourcesSelector);
+
   return (
     <>
       <h1>K8s Resources</h1>
-      <p>No resources found in the current folder.</p>
+      {activeResources.length === 0 ? (
+        <p>No resources found in the current folder.</p>
+      ) : (
+        <p>No resources match the active filters.</p>
+      )}
     </>
   );
 }
