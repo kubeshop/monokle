9ddfc1090bce3862e316ae8287f58c143e592805
Catalin Hoha <20538711+catalinhoha@users.noreply.github.com>
2021-11-12 13:22:41 +0200

    chore: remove commented code

--- src/components/molecules/ClusterDiff/ClusterDiff.tsx
+++ src/components/molecules/ClusterDiff/ClusterDiff.tsx
@@ -5,19 +5,12 @@ import styled from 'styled-components';
 import {useAppDispatch} from '@redux/hooks';
 import {loadClusterDiff} from '@redux/thunks/loadClusterDiff';
 
-// import {HelmValuesFile} from '@models/helm';
-// import {K8sResource} from '@models/k8sresource';
 import {ClusterToLocalResourcesMatch} from '@models/appstate';
 
-// import {MonoPaneTitle} from '@components/atoms';
-// import HelmChartSectionBlueprint, {HelmChartScopeType} from '@src/navsections/HelmChartSectionBlueprint';
-// import KustomizationSectionBlueprint, {KustomizationScopeType} from '@src/navsections/KustomizationSectionBlueprint';
 import {ResourceFilterIconWithPopover, SectionRenderer} from '@components/molecules';
 
 import {ReloadOutlined} from '@ant-design/icons';
 
-// import {NAVIGATOR_HEIGHT_OFFSET} from '@constants/constants';
-// import AppContext from '@src/AppContext';
 import ClusterDiffSectionBlueprint, {ClusterDiffScopeType} from '@src/navsections/ClusterDiffSectionBlueprint';
 
 import * as S from './ClusterDiff.styled';
@@ -27,10 +20,6 @@ const Container = styled.div<{height?: number}>`
   display: flex;
   ${props => props.height && `height: ${props.height};`}
 `;
-
-// const RightPane = styled.div`
-//   width: 300px;
-// `;
 
 const LeftPane = styled.div`
   flex-grow: 1;
@@ -56,10 +45,6 @@ const ListContainer = styled.div`
 
 function ClusterDiff() {
   const dispatch = useAppDispatch();
-  // const {windowSize} = useContext(AppContext);
-  // const windowHeight = windowSize.height;
-  // const navigatorHeight = windowHeight - NAVIGATOR_HEIGHT_OFFSET;
-
   const onClickRefresh = () => {
     dispatch(loadClusterDiff());
   };
@@ -90,32 +75,6 @@ function ClusterDiff() {
           </S.List>
         </ListContainer>
       </LeftPane>
-      {/* <Divider type="vertical" style={{height: '100vh', margin: 0}} />
-      <RightPane>
-        <S.TitleBar>
-          <MonoPaneTitle>Navigator</MonoPaneTitle>
-        </S.TitleBar>
-        <S.List height={navigatorHeight}>
-          <SectionRenderer<HelmValuesFile, HelmChartScopeType>
-            sectionBlueprint={HelmChartSectionBlueprint}
-            level={0}
-            isLastSection={false}
-            itemRendererOptions={{
-              disablePrefix: true,
-              disableSuffix: true,
-            }}
-          />
-          <SectionRenderer<K8sResource, KustomizationScopeType>
-            sectionBlueprint={KustomizationSectionBlueprint}
-            level={0}
-            isLastSection={false}
-            itemRendererOptions={{
-              disablePrefix: true,
-              disableSuffix: true,
-            }}
-          />
-        </S.List>
-      </RightPane> */}
     </Container>
   );
 }
