7bec6c47dc1fd5a14acd06c97de8530bccde6a18
Catalin <20538711+devcatalin@users.noreply.github.com>
2021-11-15 12:41:53 +0200

    refactor: show title tags in cluster compare

--- src/navsections/ClusterDiffSectionBlueprint/ClusterDiffSectionNameDisplay.tsx
+++ src/navsections/ClusterDiffSectionBlueprint/ClusterDiffSectionNameDisplay.tsx
@@ -1,53 +1,50 @@
-// import React from 'react';
-// import styled from 'styled-components';
+import {Tag} from 'antd';
+import React from 'react';
+import styled from 'styled-components';
 
-// import {Tag} from 'antd';
+const NameDisplayContainer = styled.div`
+  margin-left: 16px;
+  padding-top: 8px;
+  padding-bottom: 8px;
+`;
 
-// const NameDisplayContainer = styled.div`
-//   margin-left: 16px;
-//   padding-top: 8px;
-//   padding-bottom: 8px;
-// `;
+const TagsContainer = styled.div`
+  width: 800px;
+  display: flex;
+  justify-content: space-between;
+  margin-left: 24px;
+  font-size: 16px;
+  margin-top: 16px;
+`;
 
-// const TagsContainer = styled.div`
-//   width: 800px;
-//   display: flex;
-//   justify-content: space-between;
-//   margin-left: 24px;
-//   font-size: 16px;
-//   margin-top: 16px;
-// `;
+const TagWrapper = styled.div`
+  width: 250px;
+`;
 
-// const TagWrapper = styled.div`
-//   width: 300px;
-// `;
+const Spacing = styled.div`
+  width: 60px;
+`;
 
-// const Spacing = styled.div`
-//   width: 60px;
-// `;
-
-// const StyledTag = styled(Tag)`
-//   padding: 5px 10px;
-//   font-size: 14px;
-//   font-weight: 600;
-// `;
+const StyledTag = styled(Tag)`
+  padding: 5px 10px;
+  font-size: 14px;
+  font-weight: 600;
+`;
 
 function ResourceDiffSectionNameDisplay() {
-  return null;
-  // return (
-  //   <NameDisplayContainer>
-  //     <h1>K8s Resources</h1>
-  //     {/* <TagsContainer>
-  //       <TagWrapper>
-  //         <StyledTag color="cyan">Local Resources</StyledTag>
-  //       </TagWrapper>
-  //       <Spacing />
-  //       <TagWrapper>
-  //         <StyledTag color="cyan">Cluster Resources</StyledTag>
-  //       </TagWrapper>
-  //     </TagsContainer> */}
-  //   </NameDisplayContainer>
-  // );
+  return (
+    <NameDisplayContainer>
+      <TagsContainer>
+        <TagWrapper>
+          <StyledTag>Local Resources</StyledTag>
+        </TagWrapper>
+        <Spacing />
+        <TagWrapper>
+          <StyledTag>Cluster Resources</StyledTag>
+        </TagWrapper>
+      </TagsContainer>
+    </NameDisplayContainer>
+  );
 }
 
 export default ResourceDiffSectionNameDisplay;
