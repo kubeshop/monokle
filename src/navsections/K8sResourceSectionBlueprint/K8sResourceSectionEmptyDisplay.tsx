65a71de49b9c39a6a15f86ed909c488c9a206e1c
olensmar <ole@lensmar.com>
2021-12-11 15:53:51 +0100

    feat: added action to reset filters when no resources match

--- src/navsections/K8sResourceSectionBlueprint/K8sResourceSectionEmptyDisplay.tsx
+++ src/navsections/K8sResourceSectionBlueprint/K8sResourceSectionEmptyDisplay.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
-import {useAppSelector} from '@redux/hooks';
+import {ResourceFilterType} from '@models/appstate';
+
+import {useAppDispatch, useAppSelector} from '@redux/hooks';
+import {updateResourceFilter} from '@redux/reducers/main';
 import {activeResourcesSelector} from '@redux/selectors';
 
 function K8sResourceSectionEmptyDisplay() {
   const activeResources = useAppSelector(activeResourcesSelector);
+  const dispatch = useAppDispatch();
+
+  function resetFilters() {
+    const emptyFilter: ResourceFilterType = {annotations: {}, labels: {}};
+    dispatch(updateResourceFilter(emptyFilter));
+  }
 
   return (
     <>
@@ -12,7 +21,9 @@ function K8sResourceSectionEmptyDisplay() {
       {activeResources.length === 0 ? (
         <p>No resources found in the current folder.</p>
       ) : (
-        <p>No resources match the active filters.</p>
+        <p>
+          No resources match the active filters. <a onClick={resetFilters}>[Reset Filters]</a>{' '}
+        </p>
       )}
     </>
   );
