b54eaf517ccea02ea13dbed2f0afbcb92f9926db
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2021-12-15 13:07:07 +0200

    feat: update actions menu ux

--- src/components/molecules/CheckedResourcesActionsMenu/CheckedResourcesActionsMenu.tsx
+++ src/components/molecules/CheckedResourcesActionsMenu/CheckedResourcesActionsMenu.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 import {Menu} from 'antd';
 
+import {CloseOutlined} from '@ant-design/icons';
+
 import styled from 'styled-components';
 
-import {useAppDispatch} from '@redux/hooks';
+import {useAppDispatch, useAppSelector} from '@redux/hooks';
 import {uncheckAllResourceIds} from '@redux/reducers/main';
 
 import Colors from '@styles/Colors';
@@ -16,19 +18,43 @@ const StyledMenu = styled(Menu)`
   line-height: 1.57;
   display: flex;
   align-items: center;
+
+  & .ant-menu-item {
+    padding: 0 12px !important;
+  }
+
+  & .ant-menu-item::after {
+    left: 12px;
+    right: 12px;
+  }
+
+  & li:first-child {
+    color: ${Colors.grey7};
+    cursor: default;
+  }
 `;
 
 const CheckedResourcesActionsMenu: React.FC = () => {
   const dispatch = useAppDispatch();
 
+  const checkedResourceIds = useAppSelector(state => state.main.checkedResourceIds);
+
+  const deselectHandler = () => {
+    dispatch(uncheckAllResourceIds());
+  };
+
   return (
     <StyledMenu mode="horizontal">
+      <Menu.Item disabled key="resources-selected">
+        {checkedResourceIds.length} Selected
+      </Menu.Item>
       <Menu.Item style={{color: Colors.red7}} key="delete">
         Delete
       </Menu.Item>
+      <Menu.Item key="deploy">Deploy</Menu.Item>
 
-      <Menu.Item style={{marginLeft: 'auto'}} key="deselect" onClick={() => dispatch(uncheckAllResourceIds())}>
-        Deselect
+      <Menu.Item style={{marginLeft: 'auto'}} key="deselect" onClick={deselectHandler}>
+        <CloseOutlined />
       </Menu.Item>
     </StyledMenu>
   );
