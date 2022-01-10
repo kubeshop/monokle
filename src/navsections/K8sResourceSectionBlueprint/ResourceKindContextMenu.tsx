fa9cd26f585b60e496190215aa9b414e7de5b3d4
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2022-01-10 14:03:44 +0200

    feat: save to file/folder cluster/preview context menu action

--- src/navsections/K8sResourceSectionBlueprint/ResourceKindContextMenu.tsx
+++ src/navsections/K8sResourceSectionBlueprint/ResourceKindContextMenu.tsx
@@ -1,5 +1,4 @@
-import {useMemo} from 'react';
-import {useSelector} from 'react-redux';
+import {useMemo, useState} from 'react';
 
 import {Menu, Modal} from 'antd';
 
@@ -14,7 +13,7 @@ import {ItemCustomComponentProps} from '@models/navigator';
 import {useAppDispatch, useAppSelector} from '@redux/hooks';
 import {removeResource} from '@redux/reducers/main';
 import {openNewResourceWizard, openRenameResourceModal} from '@redux/reducers/ui';
-import {isInPreviewModeSelector} from '@redux/selectors';
+import {isInClusterModeSelector, isInPreviewModeSelector} from '@redux/selectors';
 import {getResourcesForPath} from '@redux/services/fileEntry';
 import {isFileResource, isUnsavedResource} from '@redux/services/resource';
 import {AppDispatch} from '@redux/store';
@@ -22,8 +21,13 @@ import {AppDispatch} from '@redux/store';
 import {Dots} from '@atoms';
 
 import ContextMenu from '@components/molecules/ContextMenu';
+import SaveResourceToFileFolderModal from '@components/molecules/SaveResourcesToFileFolderModal';
 
 import Colors from '@styles/Colors';
+
+const ContextMenuDivider = styled.div`
+  border-bottom: 1px solid rgba(255, 255, 255, 0.25);
+`;
 
 const StyledActionsMenuIconContainer = styled.span<{isSelected: boolean}>`
   cursor: pointer;
@@ -62,11 +66,14 @@ const ResourceKindContextMenu = (props: ItemCustomComponentProps) => {
 
   const dispatch = useAppDispatch();
 
-  const resourceMap = useAppSelector(state => state.main.resourceMap);
+  const isInClusterMode = useAppSelector(isInClusterModeSelector);
+  const isInPreviewMode = useAppSelector(isInPreviewModeSelector);
   const previewType = useAppSelector(state => state.main.previewType);
-  const isInPreviewMode = useSelector(isInPreviewModeSelector);
   const resource = useAppSelector(state => state.main.resourceMap[itemInstance.id]);
+  const resourceMap = useAppSelector(state => state.main.resourceMap);
   const selectedResourceId = useAppSelector(state => state.main.selectedResourceId);
+
+  const [isSaveToFileFolderModalVisible, setIsSaveToFileFolderModalVisible] = useState(false);
 
   const isResourceSelected = useMemo(() => {
     return itemInstance.id === selectedResourceId;
@@ -100,6 +107,15 @@ const ResourceKindContextMenu = (props: ItemCustomComponentProps) => {
 
   const menu = (
     <Menu>
+      {(isInClusterMode || isInPreviewMode) && (
+        <>
+          <Menu.Item onClick={() => setIsSaveToFileFolderModalVisible(true)} key="save_to_file_folder">
+            Save to file/folder
+          </Menu.Item>
+          <ContextMenuDivider />
+        </>
+      )}
+
       <Menu.Item disabled={isInPreviewMode} onClick={onClickRename} key="rename">
         Rename
       </Menu.Item>
@@ -113,11 +129,22 @@ const ResourceKindContextMenu = (props: ItemCustomComponentProps) => {
   );
 
   return (
-    <ContextMenu overlay={menu}>
-      <StyledActionsMenuIconContainer isSelected={itemInstance.isSelected}>
-        <Dots color={isResourceSelected ? Colors.blackPure : undefined} />
-      </StyledActionsMenuIconContainer>
-    </ContextMenu>
+    <>
+      <ContextMenu overlay={menu}>
+        <StyledActionsMenuIconContainer isSelected={itemInstance.isSelected}>
+          <Dots color={isResourceSelected ? Colors.blackPure : undefined} />
+        </StyledActionsMenuIconContainer>
+      </ContextMenu>
+
+      {isSaveToFileFolderModalVisible && (
+        <SaveResourceToFileFolderModal
+          resourcesIds={[itemInstance.id]}
+          title="Save resource"
+          isVisible={isSaveToFileFolderModalVisible}
+          onCancel={() => setIsSaveToFileFolderModalVisible(false)}
+        />
+      )}
+    </>
   );
 };
 
