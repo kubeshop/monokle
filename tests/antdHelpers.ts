cf26fb9bcab4121034dee79c8199d7aac7355195
olensmar <ole@lensmar.com>
2022-01-16 14:41:56 +0100

    fix: added waits for drawer transitions

--- tests/antdHelpers.ts
+++ tests/antdHelpers.ts
@@ -11,6 +11,32 @@ export async function isDrawerVisible(drawer: Locator) {
   return value && value.includes('ant-drawer-open');
 }
 
+export async function waitForDrawerToShow(page: Page, title: string, timeout?: number) {
+  console.log(`waiting for drawer ${title} to show`);
+  const drawer = page.locator(
+    `//div[contains(@class,'ant-drawer') and contains(@class,'ant-drawer-open')]//div[contains(@class,'ant-drawer-title') and text() ='${title}']`
+  );
+  try {
+    await drawer.waitFor({state: 'attached', timeout});
+    return drawer;
+  } catch (e: any) {
+    console.log(`drawer ${title} did not show within ${timeout}ms`, e);
+  }
+}
+
+export async function waitForDrawerToHide(page: Page, title: string, timeout?: number) {
+  console.log(`waiting for drawer ${title} to hide`);
+  const drawer = page.locator(
+    `//div[contains(@class,'ant-drawer') and not(contains(@class,'ant-drawer-open'))]//div[contains(@class,'ant-drawer-title') and contains(text(),'${title}')]`
+  );
+  try {
+    await drawer.waitFor({state: 'attached', timeout});
+    return drawer;
+  } catch (e: any) {
+    console.log(`drawer ${title} did not hide within ${timeout}ms`, e);
+  }
+}
+
 export async function findDrawer(page: Page, title: string) {
   const drawers = page.locator(`//div[contains(@class,'ant-drawer')]`);
   const count = await drawers.count();
@@ -18,7 +44,7 @@ export async function findDrawer(page: Page, title: string) {
   for (let c = 0; c < count; c += 1) {
     const drawer = drawers.nth(c);
     // eslint-disable-next-line no-await-in-loop
-    const locator = await drawer.locator(`//div[contains(@class,'ant-drawer-title')][text()='${title}']`);
+    const locator = await drawer.locator(`//div[contains(@class,'ant-drawer-title') and text()='${title}']`);
     // eslint-disable-next-line no-await-in-loop
     const cnt = await locator.count();
     if (cnt === 1) {
@@ -45,15 +71,27 @@ export async function findModal(page: Page, title: string) {
 
 export async function waitForModal(page: Page, title: string, timeout?: number) {
   console.log(`waiting for modal ${title}`);
-  const modals = page.locator(
-    `//div[contains(@class,'ant-modal-root')][descendant::div[@class="ant-modal-mask"]][descendant::span[contains(@id, '${title}')]]`
+  const modal = page.locator(
+    `//div[contains(@class,'ant-modal-root') and descendant::span[contains(@id, '${title}')]]`
   );
   try {
-    const elm = await modals.elementHandle({timeout});
-    return true;
+    const elm = await modal.elementHandle({timeout});
+    return modal;
   } catch (e: any) {
     console.log(`modal ${title} did not show within ${timeout}ms`, e.name);
-    return false;
+  }
+}
+
+export async function waitForModalToHide(page: Page, title: string, timeout?: number) {
+  console.log(`waiting for modal ${title} to hide`);
+  const modal = page.locator(
+    `//div[contains(@class,'ant-modal-wrap') and contains(@style,'display: none;')] and descendant::div[@class="ant-modal-mask"] and descendant::span[contains(@id, '${title}')]]`
+  );
+  try {
+    const elm = await modal.elementHandle({timeout});
+    return modal;
+  } catch (e: any) {
+    console.log(`modal ${title} did not hide within ${timeout}ms`, e.name);
   }
 }
 
