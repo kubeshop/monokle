188c1f12230fb87a68bee8f2e5f3e14c56a2a23f
olensmar <ole@lensmar.com>
2022-01-18 11:23:36 +0100

    feat: extended global timeout and merged main into branch

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import {PlaywrightTestConfig} from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
   testDir: './tests',
+  timeout: 60000,
   expect: {
     toMatchSnapshot: {threshold: 0.2},
   },
