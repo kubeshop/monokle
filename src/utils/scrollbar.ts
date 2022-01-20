82d0af034e9525304cab5fc1544b108b7a7a81c3
Erdi Köse <me@erdkse.com>
2022-01-20 14:39:57 +0300

    chore: fix styling

--- src/utils/scrollbar.ts
+++ src/utils/scrollbar.ts
@@ -2,14 +2,14 @@ import Colors from '@styles/Colors';
 
 export const GlobalScrollbarStyle = `
   ::-webkit-scrollbar {
-    width: 8px !important;
+    width: 8px;
   }
 
   ::-webkit-scrollbar-track {
-    background: ${Colors.grey1000} !important;
+    background: ${Colors.grey1000};
   }
 
   ::-webkit-scrollbar-thumb {
-    background: ${Colors.grey4} !important;
+    background: ${Colors.grey4};
   }
 `;
