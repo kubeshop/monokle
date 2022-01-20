dc551a7f4b93869caab34ba782ada3877a45b6ef
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2022-01-20 10:45:07 +0200

    feat: search bar for templates and plugins

--- src/components/organisms/TemplateManagerPane/TemplateManagerPane.styled.tsx
+++ src/components/organisms/TemplateManagerPane/TemplateManagerPane.styled.tsx
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
+import Colors from '@styles/Colors';
+
 export const Container = styled.div`
   padding: 16px;
+`;
+
+export const NotFoundLabel = styled.span`
+  color: ${Colors.grey7};
 `;
