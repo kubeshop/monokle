febcd68446ab64b77c4f6c41efc6d7b3c4197748
Erdi Köse <me@erdkse.com>
2022-01-21 09:44:57 +0300

    chore: refactor mergeConfigs function

--- src/redux/services/projectConfig.ts
+++ src/redux/services/projectConfig.ts
@@ -1,5 +1,6 @@
 import {readFileSync, writeFileSync} from 'fs';
 import _ from 'lodash';
+import objectPath from 'object-path';
 import {sep} from 'path';
 
 import {AppConfig, ProjectConfig} from '@models/appconfig';
@@ -99,9 +100,7 @@ export const updateProjectSettings = (dispatch: AppDispatch, projectRootPath?: s
   dispatch(updateProjectConfig(null));
 };
 
-// I am not proud of this code. It can be surely do it better.
-// After 1.5.0 I will refactor this one
-export const mergeConfigs = (baseConfig: ProjectConfig, config?: ProjectConfig | null) => {
+export const mergeConfigs = (baseConfig: ProjectConfig, config?: ProjectConfig | null): ProjectConfig => {
   if (!(baseConfig && baseConfig.settings && baseConfig.kubeConfig)) {
     throw Error('Base config must be set');
   }
@@ -110,85 +109,40 @@ export const mergeConfigs = (baseConfig: ProjectConfig, config?: ProjectConfig |
     return baseConfig;
   }
 
-  if (
-    _.isString(config.settings?.helmPreviewMode) &&
-    !_.isEqual(config.settings?.helmPreviewMode, baseConfig.settings?.helmPreviewMode)
-  ) {
-    baseConfig.settings.helmPreviewMode = config.settings?.helmPreviewMode;
-  }
+  const serializedBaseConfig: any = serializeObject(baseConfig);
+  const serializedConfig: any = serializeObject(baseConfig);
 
-  if (
-    _.isString(config.settings?.kustomizeCommand) &&
-    !_.isEqual(config.settings?.kustomizeCommand, baseConfig.settings?.kustomizeCommand)
-  ) {
-    baseConfig.settings.kustomizeCommand = config.settings?.kustomizeCommand;
-  }
-  if (
-    _.isBoolean(config.settings?.loadLastProjectOnStartup) &&
-    !_.isEqual(config.settings?.loadLastProjectOnStartup, baseConfig.settings?.loadLastProjectOnStartup)
-  ) {
-    baseConfig.settings.loadLastProjectOnStartup = config.settings?.loadLastProjectOnStartup;
-  }
-  if (
-    _.isBoolean(config.settings?.enableHelmWithKustomize) &&
-    !_.isEqual(config.settings?.enableHelmWithKustomize, baseConfig.settings?.enableHelmWithKustomize)
-  ) {
-    baseConfig.settings.enableHelmWithKustomize = config.settings?.enableHelmWithKustomize;
-  }
-  if (
-    _.isBoolean(config.settings?.hideExcludedFilesInFileExplorer) &&
-    !_.isEqual(config.settings?.hideExcludedFilesInFileExplorer, baseConfig.settings?.hideExcludedFilesInFileExplorer)
-  ) {
-    baseConfig.settings.hideExcludedFilesInFileExplorer = config.settings?.hideExcludedFilesInFileExplorer;
-  }
-  if (
-    _.isBoolean(config.settings?.isClusterSelectorVisible) &&
-    !_.isEqual(config.settings?.isClusterSelectorVisible, baseConfig.settings?.isClusterSelectorVisible)
-  ) {
-    baseConfig.settings.isClusterSelectorVisible = config.settings?.isClusterSelectorVisible;
-  }
-  if (_.isEmpty(baseConfig.settings)) {
-    baseConfig.settings = undefined;
-  }
-  if (_.isString(config.kubeConfig?.path) && !_.isEqual(config.kubeConfig?.path, baseConfig.kubeConfig?.path)) {
-    baseConfig.kubeConfig.path = config.kubeConfig?.path;
-  }
-  if (
-    _.isBoolean(config.kubeConfig?.isPathValid) &&
-    !_.isEqual(config.kubeConfig?.isPathValid, baseConfig.kubeConfig?.isPathValid)
-  ) {
-    baseConfig.kubeConfig.isPathValid = config.kubeConfig?.isPathValid;
-  }
-  if (
-    _.isString(config.kubeConfig?.currentContext) &&
-    !_.isEqual(config.kubeConfig?.currentContext, baseConfig.kubeConfig?.currentContext)
-  ) {
-    baseConfig.kubeConfig.currentContext = config.kubeConfig?.currentContext;
-  }
-  if (
-    _.isArray(config.kubeConfig?.contexts) &&
-    !_.isEqual(_.sortBy(config.kubeConfig?.contexts), _.sortBy(baseConfig.kubeConfig?.contexts))
-  ) {
-    baseConfig.kubeConfig.contexts = config.kubeConfig?.contexts;
-  }
-  if (_.isEmpty(baseConfig.kubeConfig)) {
-    baseConfig.kubeConfig = undefined;
-  }
+  Object.keys(serializedBaseConfig).forEach((key: string) => {
+    if (!_.isUndefined(serializedConfig[key])) {
+      serializedBaseConfig[key] = serializedConfig[key];
+    }
+  });
 
-  if (_.isArray(config.scanExcludes) && !_.isEqual(_.sortBy(config.scanExcludes), _.sortBy(baseConfig.scanExcludes))) {
-    baseConfig.scanExcludes = config.scanExcludes;
-  }
+  return deserializeObject(baseConfig);
+};
 
-  if (_.isArray(config.fileIncludes) && !_.isEqual(_.sortBy(config.fileIncludes), _.sortBy(baseConfig.fileIncludes))) {
-    baseConfig.fileIncludes = config.fileIncludes;
-  }
+const serializeObject = (objectToSerialize: any, prefix?: string): any => {
+  let serialized: any = {};
+  Object.keys(objectToSerialize).forEach(key => {
+    if (_.isObject(objectToSerialize[key]) && !_.isArray(objectToSerialize[key])) {
+      const result: any = serializeObject(objectToSerialize[key], key);
+      serialized = {
+        ...serialized,
+        ...result,
+      };
+    } else {
+      const objectKey = prefix ? `${prefix}.${key}` : key;
+      serialized[objectKey] = objectToSerialize[key];
+    }
+  });
+  return serialized;
+};
 
-  if (
-    _.isNumber(config.folderReadsMaxDepth) &&
-    !_.isEqual(config.folderReadsMaxDepth, baseConfig.folderReadsMaxDepth)
-  ) {
-    baseConfig.folderReadsMaxDepth = config.folderReadsMaxDepth;
-  }
+const deserializeObject = (objectToDeserialize: any) => {
+  const deserialized = {};
 
-  return baseConfig;
+  Object.keys(objectToDeserialize).forEach(key => {
+    objectPath.set(deserialized, key, objectToDeserialize[key]);
+  });
+  return deserialized;
 };
