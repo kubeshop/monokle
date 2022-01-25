71965004fd4ba2176cd6a5a1d0cfb19a0570630d
dmitry.yeremenko <dmitry.yeremenko>
2022-01-25 09:20:31 +0200

    feat: fixed alignment for icons and text

--- src/components/organisms/PageHeader/HelpMenu.tsx
+++ src/components/organisms/PageHeader/HelpMenu.tsx
@@ -1,4 +1,4 @@
-import {Dropdown, Menu} from 'antd';
+import {Col, Dropdown, Menu, Row} from 'antd';
 
 import {FileSearchOutlined, GithubOutlined, QuestionCircleOutlined} from '@ant-design/icons';
 
@@ -14,30 +14,49 @@ const IconContainerSpan = styled.span`
   color: ${FontColors.elementSelectTitle};
   padding-top: 10px;
   padding-right: 10px;
-  font-size: 24px;
+  font-size: 1.5em;
   cursor: pointer;
+`;
+
+const LabelContainer = styled(Col)`
+  padding: 0 25px 0 0;
+  display: flex;
+  display: flex;
+  align-content: center;
 `;
 
 const HelpMenu = () => {
   const menu = (
-    <Menu>
+    <Menu inlineIndent={25}>
       <Menu.Item onClick={openDocumentation}>
-        <IconContainerSpan>
-          <FileSearchOutlined size={24} />
-        </IconContainerSpan>
-        Documentation
+        <Row justify="center">
+          <Col span={8}>
+            <IconContainerSpan>
+              <FileSearchOutlined size={24} />
+            </IconContainerSpan>
+          </Col>
+          <LabelContainer span={16}>Documentation</LabelContainer>
+        </Row>
       </Menu.Item>
       <Menu.Item onClick={openGitHub}>
-        <IconContainerSpan>
-          <GithubOutlined size={24} />
-        </IconContainerSpan>
-        GitHub
+        <Row justify="center">
+          <Col span={8}>
+            <IconContainerSpan>
+              <GithubOutlined size={24} />
+            </IconContainerSpan>
+          </Col>
+          <LabelContainer span={16}>GitHub</LabelContainer>
+        </Row>
       </Menu.Item>
       <Menu.Item onClick={openDiscord}>
-        <IconContainerSpan>
-          <img src={DiscordLogo} style={{height: '24px', cursor: 'pointer', marginBottom: '4px'}} />
-        </IconContainerSpan>
-        Discord
+        <Row justify="center">
+          <Col span={8}>
+            <IconContainerSpan>
+              <img src={DiscordLogo} style={{height: '24px', cursor: 'pointer', marginBottom: '4px'}} />
+            </IconContainerSpan>
+          </Col>
+          <LabelContainer span={16}>Discord</LabelContainer>
+        </Row>
       </Menu.Item>
     </Menu>
   );
