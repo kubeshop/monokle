b09221583f8f5aba8f1ec93305e973ccd1af1485
dmitry.yeremenko <dmitry.yeremenko>
2022-02-03 16:45:00 +0200

    feat: restore deleted - for mac os zoom out

--- src/constants/hotkeys.ts
+++ src/constants/hotkeys.ts
@@ -6,7 +6,7 @@ const hotkeys = {
   REFRESH_FOLDER: 'ctrl+f5,command+f5',
   TOGGLE_LEFT_PANE: 'ctrl+b,command+b',
   ZOOM_IN: 'ctrl+=,command+=',
-  ZOOM_OUT: 'ctrl+-,command+',
+  ZOOM_OUT: 'ctrl+-,command+-',
   TOGGLE_RIGHT_PANE: 'ctrl+alt+b,command+alt+b',
   SAVE: 'ctrl+s,command+s',
   SELECT_FROM_HISTORY_BACK: 'alt+left',
