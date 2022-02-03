9a56818b64b7d0a86de244dbba7e8c6c2af5d209
Andrei Vinaga <andrei.vinaga@kubeshop.io>
2022-02-03 15:55:19 +0200

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -241,7 +241,6 @@ const App = () => {
           {isCreateProjectModalVisible && <CreateProjectModal />}
           {isLocalResourceDiffModalVisible && <LocalResourceDiffModal />}
           {isNewResourceWizardVisible && <NewResourceWizard />}
-          {isNewResourceWizardVisible && <NewResourceWizard />}
           {isQuickSearchActionsVisible && <QuickSearchActions />}
           {isRenameEntityModalVisible && <RenameEntityModal />}
           {isRenameResourceModalVisible && <RenameResourceModal />}
