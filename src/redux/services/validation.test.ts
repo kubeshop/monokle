a34a01cf4336b7d46eb25c40b715870dd43b83d6
Erdi Köse <me@erdkse.com>
2022-02-08 20:40:37 +0300

    chore: attempt to solve test fail

--- src/redux/services/validation.test.ts
+++ src/redux/services/validation.test.ts
@@ -1,3 +1,5 @@
+import {PREDEFINED_K8S_VERSION} from '@constants/constants';
+
 import {getTestResourcePath} from '@redux/services/__test__/utils';
 import {readManifests} from '@redux/services/fileEntry.test';
 import {validateResource} from '@redux/services/validation';
@@ -10,7 +12,7 @@ test('validate-resource', () => {
   const deployment = resources.find(r => r.kind === 'Deployment');
   expect(deployment).toBeDefined();
   // @ts-ignore
-  validateResource(deployment);
+  validateResource(deployment, PREDEFINED_K8S_VERSION);
 
   expect(deployment?.validation).toBeDefined();
   expect(deployment?.validation?.errors).toBeDefined();
