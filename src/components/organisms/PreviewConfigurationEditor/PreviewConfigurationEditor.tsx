e640804c04337d2f4ce49c875f005407ce14de3c
Catalin <20538711+devcatalin@users.noreply.github.com>
2022-02-17 22:05:18 +0200

    feat: create base prev conf editor layout

--- src/components/organisms/PreviewConfigurationEditor/PreviewConfigurationEditor.tsx
+++ src/components/organisms/PreviewConfigurationEditor/PreviewConfigurationEditor.tsx
@@ -1,5 +1,23 @@
+import {Input} from 'antd';
+
+import * as S from './styled';
+
 const PreviewConfigurationEditor = () => {
-  return <div />;
+  return (
+    <div>
+      <S.Field>
+        <S.Label>Name your configuration:</S.Label>
+        <Input placeholder="Enter the configuration name" />
+      </S.Field>
+      <S.Field>
+        <S.Label style={{marginBottom: 0}}>Select which values files to use:</S.Label>
+        <S.Description>Drag and drop to specify order</S.Description>
+      </S.Field>
+      <S.Field>
+        <S.Label>Specify options:</S.Label>
+      </S.Field>
+    </div>
+  );
 };
 
 export default PreviewConfigurationEditor;
---
+++ src/components/organisms/PreviewConfigurationEditor/styled.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+import Colors from '@styles/Colors';
+
+export const Label = styled.p`
+  margin: 0;
+  font-size: 14px;
+  font-weight: 600;
+  color: ${Colors.grey9};
+  margin-bottom: 8px;
+`;
+
+export const Field = styled.div`
+  margin-top: 6px;
+  margin-bottom: 30px;
+`;
+
+export const Description = styled.p`
+  margin: 0;
+  font-size: 14px;
+  color: ${Colors.grey7};
+`;
