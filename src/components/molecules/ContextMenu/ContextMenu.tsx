259b4ed8e04ad10188e896efb87e187eac0820e0
Ege Eren <32365178+egeren@users.noreply.github.com>
2022-02-25 02:26:43 +0300

    now we can trigger contextmenu by right-clicking on treeItems on file explorer

--- src/components/molecules/ContextMenu/ContextMenu.tsx
+++ src/components/molecules/ContextMenu/ContextMenu.tsx
@@ -2,13 +2,14 @@ import {Dropdown} from 'antd';
 
 interface ContextMenuProps {
   overlay: React.ReactElement;
+  triggerOnRightClick?: boolean;
 }
 
 const ContextMenu: React.FC<ContextMenuProps> = props => {
-  const {overlay, children} = props;
+  const {overlay, children, triggerOnRightClick = false} = props;
 
   return (
-    <Dropdown overlay={overlay} trigger={['click']} placement="bottomCenter">
+    <Dropdown overlay={overlay} trigger={triggerOnRightClick ? ['contextMenu'] : ['click']} placement="bottomCenter">
       {children}
     </Dropdown>
   );
