197f71b60a794cfda0894f32e5d51c2fa923ceb0
Ege Eren <32365178+egeren@users.noreply.github.com>
2022-02-27 03:44:30 +0300

    feature: pinning template to top

--- src/components/organisms/TemplateManagerPane/TemplateManagerPane.tsx
+++ src/components/organisms/TemplateManagerPane/TemplateManagerPane.tsx
@@ -129,29 +129,19 @@ const TemplatesManagerPane: React.FC<IProps> = props => {
               <S.NotFoundLabel>No templates found.</S.NotFoundLabel>
             ) : (
               <S.TemplatesContainer>
-                {visibleTemplateEntries.map(([path, template]) =>
-                  favoriteTemplates.includes(template.id) ? (
+                {visibleTemplateEntries.sort((a,b) =>{
+                  if(favoriteTemplates.includes(a[1].id))
+                    return -1;
+                  if(favoriteTemplates.includes(b[1].id))
+                    return 1;
+                  return 0; 
+                }).map(([path, template]) =>
                     <TemplateInformation
                       key={path}
                       template={template}
                       disabled={isInPreviewMode}
                       onClickOpenTemplate={() => onClickOpenTemplate(template)}
                     />
-                  ) : (
-                    ''
-                  )
-                )}
-                {visibleTemplateEntries.map(([path, template]) =>
-                  !favoriteTemplates.includes(template.id) ? (
-                    <TemplateInformation
-                      key={path}
-                      template={template}
-                      disabled={isInPreviewMode}
-                      onClickOpenTemplate={() => onClickOpenTemplate(template)}
-                    />
-                  ) : (
-                    ''
-                  )
                 )}
               </S.TemplatesContainer>
             )}
