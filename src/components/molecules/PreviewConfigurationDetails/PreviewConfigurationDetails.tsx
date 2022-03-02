670e45f3b435018e93f72550e5777b633117f13c
Dev Catalin <20538711+devcatalin@users.noreply.github.com>
2022-03-02 19:42:48 +0200

    feat: func for building helm preview command

--- src/components/molecules/PreviewConfigurationDetails/PreviewConfigurationDetails.tsx
+++ src/components/molecules/PreviewConfigurationDetails/PreviewConfigurationDetails.tsx
@@ -4,7 +4,12 @@ import {Typography} from 'antd';
 
 import styled from 'styled-components';
 
+import {ROOT_FILE_ENTRY} from '@constants/constants';
+
 import {useAppSelector} from '@redux/hooks';
+import {kubeConfigContextSelector} from '@redux/selectors';
+
+import {buildHelmCommand} from '@utils/helm';
 
 const {Text} = Typography;
 
@@ -12,10 +17,12 @@ const Container = styled.div`
   padding: 12px;
 `;
 
-const code = 'helm install --yes --no --copy --done';
 const PreviwConfigurationDetails: React.FC = () => {
   const previewConfigurationMap = useAppSelector(state => state.config.projectConfig?.helm?.previewConfigurationMap);
   const selectedPreviewConfigurationId = useAppSelector(state => state.main.selectedPreviewConfigurationId);
+
+  const currentContext = useAppSelector(kubeConfigContextSelector);
+  const rootFolderPath = useAppSelector(state => state.main.fileMap[ROOT_FILE_ENTRY].filePath);
 
   const previewConfiguration = useMemo(
     () =>
@@ -25,14 +32,41 @@ const PreviwConfigurationDetails: React.FC = () => {
     [selectedPreviewConfigurationId, previewConfigurationMap]
   );
 
-  if (!previewConfiguration) {
-    return null;
+  const helmChart = useAppSelector(state => {
+    if (!previewConfiguration) {
+      return undefined;
+    }
+    return Object.values(state.main.helmChartMap).find(
+      chart => chart.filePath === previewConfiguration.helmChartFilePath
+    );
+  });
+
+  const builtCommand = useMemo(() => {
+    if (!previewConfiguration || !helmChart) {
+      return '';
+    }
+    return buildHelmCommand(
+      helmChart,
+      previewConfiguration.orderedValuesFilePaths,
+      previewConfiguration.command,
+      previewConfiguration.options,
+      currentContext,
+      rootFolderPath
+    );
+  }, [previewConfiguration, helmChart, currentContext, rootFolderPath]);
+
+  if (!previewConfiguration || !helmChart) {
+    return (
+      <Container>
+        <p>Something went wrong...</p>
+      </Container>
+    );
   }
 
   return (
     <Container>
       <Text code copyable>
-        {code}
+        {builtCommand}
       </Text>
     </Container>
   );
--- src/utils/helm.ts
+++ src/utils/helm.ts
@@ -1,5 +1,9 @@
 import {ipcRenderer} from 'electron';
 
+import path from 'path';
+
+import {HelmChart} from '@models/helm';
+
 /**
  * Invokes Helm in main thread
  */
@@ -10,4 +14,32 @@ export function runHelm(cmd: any): any {
     });
     ipcRenderer.send('run-helm', cmd);
   });
+}
+
+export function buildHelmCommand(
+  helmChart: HelmChart,
+  valuesFilePaths: string[],
+  command: 'template' | 'install',
+  options: Record<string, string | null>,
+  clusterContext: string,
+  rootFolderPath: string
+): string {
+  const chartFolderPath = path.join(rootFolderPath, path.dirname(helmChart.filePath));
+  const args = [
+    'helm',
+    command,
+    ...valuesFilePaths.map(filePath => ['-f', `"${filePath}"`]).flat(),
+    helmChart.name,
+    `${chartFolderPath}`,
+    Object.entries(options)
+      .map((key, value) => (!value ? [key] : [key, value]))
+      .flat(),
+  ];
+
+  if (command === 'install') {
+    args.splice(1, 0, ...['--kube-context', clusterContext]);
+    args.push('--dry-run');
+  }
+
+  return args.join(' ');
 }
