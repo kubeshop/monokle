ca42da58150f67fdc6863ab9da643c9ffb634773
Erdi Köse <me@erdkse.com>
2022-03-04 06:00:52 +0300

    chore: add error names to constant

--- src/redux/thunks/previewKustomization.ts
+++ src/redux/thunks/previewKustomization.ts
@@ -16,7 +16,7 @@ import {SetPreviewDataPayload} from '@redux/reducers/main';
 import {currentConfigSelector} from '@redux/selectors';
 import {createPreviewResult, createRejectionWithAlert} from '@redux/thunks/utils';
 
-import {trackEvent} from '@utils/telemetry';
+import {DO_KUSTOMIZE_PREVIEW, trackEvent} from '@utils/telemetry';
 
 export type KustomizeCommandOptions = {
   folder: string;
@@ -48,7 +48,7 @@ export const previewKustomization = createAsyncThunk<
     log.info(`previewing ${resource.id} in folder ${folder}`);
     const result = await runKustomize(folder, projectConfig);
 
-    trackEvent('DO_KUSTOMIZE_PREVIEW');
+    trackEvent(DO_KUSTOMIZE_PREVIEW);
 
     if (result.error) {
       return createRejectionWithAlert(thunkAPI, 'Kustomize Error', result.error);
