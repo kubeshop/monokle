bf964a2ebf10c8042ac770181758ee949edbc7cd
Andrei Vinaga <andrei.vinaga@kubeshop.io>
2022-03-17 14:37:13 +0200

    fix: revert logo

--- src/components/organisms/PageHeader/styled.tsx
+++ src/components/organisms/PageHeader/styled.tsx
@@ -78,8 +78,7 @@ export const Header = styled(RawHeader.default)`
 export const Logo = styled.img`
   height: 45px;
   cursor: pointer;
-  margin-left: -20px;
-  margin-top: -10px;
+  margin-left: -15px;
 `;
 
 export const ModeSpan = styled.span`
