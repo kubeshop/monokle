a6672d1e1db51d3422b06546f71eb278fefc277d
Julia M <meyu@opinov8.com>
2022-04-05 13:25:02 +0100

    fix: logical operator

--- src/utils/defineHotkey.ts
+++ src/utils/defineHotkey.ts
@@ -2,6 +2,6 @@ export const defineHotkey = (hotkey: string) => {
   const shortcuts = hotkey.split(' ');
   if (shortcuts.length < 2) return hotkey;
   const darwinShortcuts = shortcuts.filter(shortcut => shortcut.includes('command') || shortcut.includes('cmd'));
-  const windowsShortcuts = shortcuts.filter(shortcut => !shortcut.includes('command') || !shortcut.includes('cmd'));
+  const windowsShortcuts = shortcuts.filter(shortcut => !shortcut.includes('command') && !shortcut.includes('cmd'));
   return process.platform === 'darwin' ? darwinShortcuts.join() : windowsShortcuts.join();
 };
