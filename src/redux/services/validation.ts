ea4366f33baf8ba18b7fac837bccb8d3831349f8
Wito Delnat <wito.delnat@gmail.com>
2022-04-06 11:52:31 +0200

    fix: fix resource validation

--- src/redux/services/validation.ts
+++ src/redux/services/validation.ts
@@ -1,6 +1,4 @@
 import Ajv, {ValidateFunction} from 'ajv';
-// @ts-ignore
-import log from 'loglevel';
 import {Document, LineCounter, ParsedNode, isCollection, parseDocument} from 'yaml';
 
 import {K8sResource, RefPosition, ResourceValidationError} from '@models/k8sresource';
@@ -49,149 +47,151 @@ export function validateResource(resource: K8sResource, schemaVersion: string, u
     return;
   }
 
+  const errors = [];
+  const validate = lazyGetResourceValidator(resource, schemaVersion, userDataDir);
+
+  if (!validate) {
+    resource.validation = {
+      isValid: false,
+      errors: [{property: '', message: 'Native K8s schema not found!'}],
+    };
+    return;
+  }
+
+  // parse for YAML errors that were allowed by non-strict parsing
+  const doc = parseDocument(resource.text);
+  if (doc.errors.length > 0) {
+    const lines = resource.text.split('\n');
+    if (lines[0] === '---') {
+      lines.shift();
+    }
+
+    errors.push(
+      ...doc.errors.map(err => {
+        const line = err.linePos ? lines[err.linePos[0].line].trim() : '';
+        const error: ResourceValidationError = {
+          property: err.name,
+          message: err.code,
+          description: err.message,
+          errorPos: {
+            line: err.linePos && err.linePos.length > 0 && err.linePos[0].line ? err.linePos[0].line : 0,
+            column: err.linePos && err.linePos.length > 0 && err.linePos[0].col ? err.linePos[0].col : 0,
+            length: line.length,
+          },
+        };
+
+        return error;
+      })
+    );
+  }
+
+  // parse for schema errors
+  validate(resource.content);
+  if (validate.errors) {
+    errors.push(
+      ...validate.errors
+        .filter(
+          err =>
+            !ignoredProperties.some(
+              ignored =>
+                (ignored.startsWith('*') && err.dataPath.endsWith(ignored.substring(1))) ||
+                (ignored.endsWith('*') && err.dataPath.startsWith(ignored.substring(0, ignored.length - 1))) ||
+                err.dataPath === ignored
+            )
+        )
+        .map(err => {
+          const parsedDoc = getParsedDoc(resource);
+
+          // @ts-ignore
+          const valueNode = findJsonPointerNode(parsedDoc, err.dataPath.substring(1).split('/'));
+
+          const error: ResourceValidationError = {
+            property: err.dataPath,
+            message: err.message || 'message',
+            errorPos: valueNode ? getErrorPosition(valueNode, getLineCounter(resource)) : undefined,
+            // @ts-ignore
+            description: err.parentSchema?.description || undefined,
+          };
+
+          // @ts-ignore
+          if (err.keyword === 'additionalProperties' && err.params?.additionalProperty) {
+            // @ts-ignore
+            error.message += `: ${err.params.additionalProperty}`;
+          }
+          return error;
+        })
+    );
+  }
+
+  errors.sort(compareErrorsByLineNumber);
+
+  resource.validation = {
+    isValid: errors.length === 0,
+    errors,
+  };
+}
+
+function lazyGetResourceValidator(
+  resource: K8sResource,
+  schemaVersion: string,
+  userDataDir: string
+): Ajv.ValidateFunction | undefined {
   const resourceSchema = getResourceSchema(resource, schemaVersion, userDataDir);
+
+  if (!resourceSchema) {
+    return undefined;
+  }
 
   const validatorCacheKey = `${schemaVersion}-${resource.kind}-${resource.version}`;
 
-  if (resourceSchema && !validatorCache.has(validatorCacheKey)) {
-    const errors = [];
-
-    // parse for YAML errors that were allowed by non-strict parsing
-    const doc = parseDocument(resource.text);
-    if (doc.errors.length > 0) {
-      const lines = resource.text.split('\n');
-      if (lines[0] === '---') {
-        lines.shift();
-      }
-
-      errors.push(
-        ...doc.errors.map(err => {
-          const line = err.linePos ? lines[err.linePos[0].line].trim() : '';
-          const error: ResourceValidationError = {
-            property: err.name,
-            message: err.code,
-            description: err.message,
-            errorPos: {
-              line: err.linePos && err.linePos.length > 0 && err.linePos[0].line ? err.linePos[0].line : 0,
-              column: err.linePos && err.linePos.length > 0 && err.linePos[0].col ? err.linePos[0].col : 0,
-              length: line.length,
-            },
-          };
-
-          return error;
-        })
-      );
-    }
-
-    if (!validatorCache.has(validatorCacheKey)) {
-      const ajv = new Ajv({
-        unknownFormats: 'ignore',
-        validateSchema: false,
-        logger: false,
-        jsonPointers: true,
-        verbose: true,
-        allErrors: true,
-      });
-      validatorCache.set(validatorCacheKey, ajv.compile(resourceSchema));
-    }
-
-    const validate = validatorCache.get(validatorCacheKey);
-
-    if (validate) {
-      try {
-        validate(resource.content);
-
-        if (validate.errors) {
-          errors.push(
-            ...validate.errors
-              .filter(
-                err =>
-                  !ignoredProperties.some(
-                    ignored =>
-                      (ignored.startsWith('*') && err.dataPath.endsWith(ignored.substring(1))) ||
-                      (ignored.endsWith('*') && err.dataPath.startsWith(ignored.substring(0, ignored.length - 1))) ||
-                      err.dataPath === ignored
-                  )
-              )
-              .map(err => {
-                const parsedDoc = getParsedDoc(resource);
-
-                // @ts-ignore
-                const valueNode = findJsonPointerNode(parsedDoc, err.dataPath.substring(1).split('/'));
-
-                const error: ResourceValidationError = {
-                  property: err.dataPath,
-                  message: err.message || 'message',
-                  errorPos: valueNode ? getErrorPosition(valueNode, getLineCounter(resource)) : undefined,
-                  // @ts-ignore
-                  description: err.parentSchema?.description || undefined,
-                };
-
-                // @ts-ignore
-                if (err.keyword === 'additionalProperties' && err.params?.additionalProperty) {
-                  // @ts-ignore
-                  error.message += `: ${err.params.additionalProperty}`;
-                }
-                return error;
-              })
-          );
-        }
-
-        // sort errors by line number
-        errors.sort((e1, e2): number => {
-          if (e1.errorPos && !e2.errorPos) {
-            return 1;
-          }
-          if (!e1.errorPos && e2.errorPos) {
-            return -1;
-          }
-          if (!e1.errorPos && !e2.errorPos) {
-            return 0;
-          }
-
-          // @ts-ignore
-          return e1.errorPos.line - e2.errorPos.line;
-        });
-
-        resource.validation = {
-          isValid: errors.length === 0,
-          errors: errors && errors.length > 0 ? errors : [],
-        };
-      } catch (e) {
-        log.warn('Failed to validate', e);
-      }
-    } else {
-      resource.validation = {
-        isValid: false,
-        errors: [{property: '', message: 'Native K8s schema not found!'}],
-      };
-    }
-
-    resource.validation = {
-      isValid: errors.length === 0,
-      errors,
-    };
+  if (!validatorCache.has(validatorCacheKey)) {
+    const ajv = new Ajv({
+      unknownFormats: 'ignore',
+      validateSchema: false,
+      logger: false,
+      jsonPointers: true,
+      verbose: true,
+      allErrors: true,
+    });
+    validatorCache.set(validatorCacheKey, ajv.compile(resourceSchema));
   }
 
-  function findJsonPointerNode(valuesDoc: Document.Parsed<ParsedNode>, path: string[]) {
-    if (!valuesDoc?.contents) {
-      return undefined;
-    }
+  return validatorCache.get(validatorCacheKey);
+}
 
-    let valueNode: any = valuesDoc.contents;
-
-    for (let c = 0; valueNode && c < path.length; c += 1) {
-      let node = path[c];
-      if (isCollection(valueNode)) {
-        const nextNode = valueNode.get(node, true);
-        if (nextNode) {
-          valueNode = nextNode;
-        } else {
-          return valueNode;
-        }
-      } else break;
-    }
-
-    return valueNode;
+function compareErrorsByLineNumber(e1: ResourceValidationError, e2: ResourceValidationError): number {
+  if (e1.errorPos && !e2.errorPos) {
+    return 1;
   }
+  if (!e1.errorPos && e2.errorPos) {
+    return -1;
+  }
+  if (!e1.errorPos && !e2.errorPos) {
+    return 0;
+  }
+
+  // @ts-ignore
+  return e1.errorPos.line - e2.errorPos.line;
+}
+
+function findJsonPointerNode(valuesDoc: Document.Parsed<ParsedNode>, path: string[]) {
+  if (!valuesDoc?.contents) {
+    return undefined;
+  }
+
+  let valueNode: any = valuesDoc.contents;
+
+  for (let c = 0; valueNode && c < path.length; c += 1) {
+    let node = path[c];
+    if (isCollection(valueNode)) {
+      const nextNode = valueNode.get(node, true);
+      if (nextNode) {
+        valueNode = nextNode;
+      } else {
+        return valueNode;
+      }
+    } else break;
+  }
+
+  return valueNode;
 }
