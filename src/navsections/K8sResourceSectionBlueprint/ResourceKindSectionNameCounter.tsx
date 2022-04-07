6411dfbf959d0900f91dc098d994f91d369cfd25
Wito Delnat <wito.delnat@gmail.com>
2022-04-07 10:04:41 +0200

    refactor: improve styling

--- src/navsections/K8sResourceSectionBlueprint/ResourceKindSectionNameCounter.tsx
+++ src/navsections/K8sResourceSectionBlueprint/ResourceKindSectionNameCounter.tsx
@@ -4,16 +4,18 @@ import {SectionCustomComponentProps} from '@models/navigator';
 
 import {useAppSelector} from '@redux/hooks';
 
+import {Icon} from '@components/atoms';
+
 import {isDefined} from '@utils/filter';
 import {countResourceErrors, countResourceWarnings} from '@utils/resources';
 
-import * as S from '../../components/molecules/SectionRenderer/styled';
-import * as WarningStyle from '../../components/organisms/NavigatorPane/WarningAndErrorsDisplay.styled';
+import * as S from './ResourceKindSectionNameCounter.styled';
 
 function ResourceKindSectionCounter({sectionInstance, onClick}: SectionCustomComponentProps) {
   const {id, isSelected, itemIds} = sectionInstance;
   const isCollapsed = useAppSelector(state => state.navigator.collapsedSectionIds.includes(id));
   const resources = useAppSelector(state => itemIds.map(itemId => state.main.resourceMap[itemId]).filter(isDefined));
+  const selected = isSelected && isCollapsed;
 
   const resourceCount = resources.length;
   const warningCount = useMemo(() => countResourceWarnings(resources), [resources]);
@@ -21,20 +23,20 @@ function ResourceKindSectionCounter({sectionInstance, onClick}: SectionCustomCom
 
   return (
     <>
-      <S.Counter selected={isSelected && isCollapsed} onClick={onClick}>
+      <S.Counter selected={selected} onClick={onClick}>
         {resourceCount}
       </S.Counter>
 
       {isCollapsed && warningCount > 0 ? (
-        <WarningStyle.WarningCountContainer $type="warning" onClick={onClick}>
-          <WarningStyle.Icon $type="warning" name="warning" /> {warningCount}
-        </WarningStyle.WarningCountContainer>
+        <S.WarningCountContainer selected={selected} $type="warning" onClick={onClick}>
+          <Icon name="warning" /> {warningCount}
+        </S.WarningCountContainer>
       ) : undefined}
 
       {isCollapsed && errorCount > 0 ? (
-        <WarningStyle.WarningCountContainer $type="error" onClick={onClick}>
-          <WarningStyle.Icon $type="error" name="error" /> {errorCount}
-        </WarningStyle.WarningCountContainer>
+        <S.WarningCountContainer selected={selected} $type="error" onClick={onClick}>
+          <Icon name="error" /> {errorCount}
+        </S.WarningCountContainer>
       ) : undefined}
     </>
   );
