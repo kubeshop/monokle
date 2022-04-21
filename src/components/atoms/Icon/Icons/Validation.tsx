be675675d1d9071f3d7f7de03197d3c19b5002d8
Wito Delnat <wito.delnat@gmail.com>
2022-04-21 11:35:29 +0200

    fix: fix icon padding

--- src/components/atoms/Icon/Icons/Validation.tsx
+++ src/components/atoms/Icon/Icons/Validation.tsx
@@ -1,5 +1,5 @@
 const Validation = () => (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
       fillRule="evenodd"
       clipRule="evenodd"
