2b5cc1847edeb8bc4eda62a713621317ad8e6b4b
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2022-05-03 17:04:34 +0300

    refactor: Guide styling

--- src/components/organisms/StartProjectPane/Guide.styled.tsx
+++ src/components/organisms/StartProjectPane/Guide.styled.tsx
@@ -1,7 +1,5 @@
 import {Button as ButtonRaw} from 'antd';
 
-import {ExclamationCircleFilled as ExclamationCircleFilledRaw} from '@ant-design/icons';
-
 import styled from 'styled-components';
 
 import Colors from '@styles/Colors';
@@ -10,20 +8,15 @@ export const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  gap: 1.2rem;
   width: 100%;
   margin: 10px 0;
   height: 100%;
 `;
 
-export const ExclamationCircleFilled = styled(ExclamationCircleFilledRaw)`
-  margin-right: 0.6rem;
-  cursor: pointer;
-`;
-
 export const Item = styled(ButtonRaw)`
   display: flex;
   padding: 0;
-  margin: 0.6rem;
   align-items: center;
   font-size: 12px;
   font-weight: 600;
--- src/components/organisms/StartProjectPane/Guide.tsx
+++ src/components/organisms/StartProjectPane/Guide.tsx
@@ -1,3 +1,5 @@
+import {ExclamationCircleFilled} from '@ant-design/icons';
+
 import {openUrlInExternalBrowser} from '@utils/shell';
 
 import * as S from './Guide.styled';
@@ -5,13 +7,16 @@ import * as S from './Guide.styled';
 const Guide = () => {
   return (
     <S.Container>
-      <S.ExclamationCircleFilled />
+      <ExclamationCircleFilled />
+
       <S.Item onClick={() => openUrlInExternalBrowser('https://kubeshop.github.io/monokle/features')}>
         Read a quick start guide
       </S.Item>
+
       <S.Item onClick={() => openUrlInExternalBrowser('https://www.youtube.com/watch?v=ossBDhP_K-4')}>
         Watch a 3-minute video tutorial
       </S.Item>
+
       <S.Item onClick={() => openUrlInExternalBrowser('https://kubeshop.github.io/monokle')}>Documentation</S.Item>
     </S.Container>
   );
