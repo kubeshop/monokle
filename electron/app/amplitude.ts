615692e1c651f0893b3e60b258d55c915e664988
Dev Catalin <20538711+devcatalin@users.noreply.github.com>
2022-05-04 19:03:10 +0300

    refactor: logic to enable/disable amplitude

--- electron/app/amplitude.ts
+++ electron/app/amplitude.ts
@@ -1,9 +1,22 @@
+import electronStore from '@utils/electronStore';
+
 import * as Amplitude from '@amplitude/node';
 
 let amplitudeClient: Amplitude.NodeClient | undefined;
-
-if (process.env.AMPLITUDE_API_KEY) {
-  amplitudeClient = Amplitude.init(process.env.AMPLITUDE_API_KEY);
-}
+const disableTracking = Boolean(electronStore.get('appConfig.disableEventTracking'));
 
 export const getAmplitudeClient = () => amplitudeClient;
+
+export const enableAmplitude = () => {
+  if (process.env.AMPLITUDE_API_KEY) {
+    amplitudeClient = Amplitude.init(process.env.AMPLITUDE_API_KEY);
+  }
+};
+
+export const disableAmplitude = () => {
+  amplitudeClient = undefined;
+};
+
+if (!disableTracking) {
+  enableAmplitude();
+}
