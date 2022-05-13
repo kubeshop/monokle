e724f91b6d7c7cc4058d6ad2ef9115dc958aea85
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2022-05-13 15:23:52 +0300

    feat: image ref icon + name + key

--- src/components/molecules/ResourceRefsIconPopover/RefLink.tsx
+++ src/components/molecules/ResourceRefsIconPopover/RefLink.tsx
@@ -33,15 +33,23 @@ const getRefTargetName = (ref: ResourceRef, resourceMap: ResourceMapType) => {
 
 const RefIcon = React.memo((props: {resourceRef: ResourceRef; style: React.CSSProperties}) => {
   const {resourceRef, style} = props;
+
   if (isOutgoingRef(resourceRef.type)) {
+    if (resourceRef.target?.type === 'image') {
+      return <Icon name="docker-images" style={style} />;
+    }
+
     return <Icon name="outgoingRefs" style={style} />;
   }
+
   if (isIncomingRef(resourceRef.type)) {
     return <Icon name="incomingRefs" style={style} />;
   }
+
   if (isUnsatisfiedRef(resourceRef.type)) {
     return <Icon name="warning" style={style} color={Colors.yellowWarning} />;
   }
+
   return null;
 });
 
@@ -85,6 +93,14 @@ const ResourceRefLink = (props: {
           </>
         );
       }
+    }
+
+    if (resourceRef.target?.type === 'image') {
+      return (
+        <S.TargetName $isDisabled={isDisabled} $isUnsatisfied={isUnsatisfiedRef(resourceRef.type)}>
+          {resourceRef.name}:{resourceRef.target?.tag}
+        </S.TargetName>
+      );
     }
 
     return <span>{targetName}</span>;
