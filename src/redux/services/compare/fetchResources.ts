8f7706cf151641b13d87b7fdc0de990256071930
Wito Delnat <wito.delnat@gmail.com>
2022-05-13 17:00:14 +0200

    refactor: extract fetchResources

--- src/redux/services/compare/fetchResources.ts
+++ src/redux/services/compare/fetchResources.ts
@@ -6,14 +6,38 @@ import {CLUSTER_DIFF_PREFIX, YAML_DOCUMENT_DELIMITER_NEW_LINE} from '@constants/
 import {K8sResource} from '@models/k8sresource';
 import {RootState} from '@models/rootstate';
 
+import {ResourceSet} from '@redux/reducers/compare';
 import {currentKubeContext} from '@redux/selectors';
 
 import {createKubeClient} from '@utils/kubeclient';
 
 import getClusterObjects from '../getClusterObjects';
+import {isKustomizationResource} from '../kustomize';
 import {extractK8sResources} from '../resource';
 
-export async function fetchResourcesFromCluster(state: RootState): Promise<K8sResource[]> {
+export async function fetchResources(state: RootState, options: ResourceSet): Promise<K8sResource[]> {
+  const {type} = options;
+
+  switch (type) {
+    case 'local':
+      return fetchLocalResources(state);
+    case 'cluster':
+      return fetchResourcesFromCluster(state);
+    default:
+      throw new Error('Not yet implemented');
+  }
+}
+
+function fetchLocalResources(state: RootState): K8sResource[] {
+  return Object.values(state.main.resourceMap).filter(
+    resource =>
+      !resource.filePath.startsWith(CLUSTER_DIFF_PREFIX) &&
+      !resource.name.startsWith('Patch:') &&
+      !isKustomizationResource(resource)
+  );
+}
+
+async function fetchResourcesFromCluster(state: RootState): Promise<K8sResource[]> {
   try {
     const currentContext = currentKubeContext(state.config);
     const clusterAccess = state.config.projectConfig?.clusterAccess?.filter(ca => ca.context === currentContext) || [];
