77ff669cc4caccb293c7919ae713d38598d106f7
Wito Delnat <wito.delnat@gmail.com>
2022-05-16 11:39:04 +0200

    refactor: make listeners composable

--- src/redux/listeners/base.ts
+++ src/redux/listeners/base.ts
@@ -6,3 +6,8 @@ import {RootState} from '@models/rootstate';
 export const listenerMiddleware = createListenerMiddleware();
 export type AppStartListening = TypedStartListening<RootState, AppDispatch>;
 export const startAppListening = listenerMiddleware.startListening as AppStartListening;
+export type AppListenerFn = (listen: AppStartListening) => void;
+
+export function combineListeners(listeners: AppListenerFn[]) {
+  listeners.forEach(listenerFn => listenerFn(startAppListening));
+}
