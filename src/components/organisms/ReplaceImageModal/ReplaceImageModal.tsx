65b1d81d4366a040a6fd2afdc7dd97bfe32824ff
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2022-05-18 11:09:14 +0300

    feat: get current image on replace image modal

--- src/components/organisms/ReplaceImageModal/ReplaceImageModal.tsx
+++ src/components/organisms/ReplaceImageModal/ReplaceImageModal.tsx
@@ -1,3 +1,5 @@
+import {useMemo} from 'react';
+
 import {Form, Input, Modal} from 'antd';
 
 import {useAppDispatch, useAppSelector} from '@redux/hooks';
@@ -7,9 +9,18 @@ const ReplaceImageModal: React.FC = () => {
   const [form] = Form.useForm();
 
   const dispatch = useAppDispatch();
+  const imagesMap = useAppSelector(state => state.main.imagesMap);
   const uiState = useAppSelector(state => state.ui.replaceImageModal);
 
-  if (!uiState) {
+  const image = useMemo(() => {
+    if (!uiState) {
+      return null;
+    }
+
+    return imagesMap.find(im => im.id === uiState.imageId);
+  }, [imagesMap, uiState]);
+
+  if (!uiState || !image) {
     return null;
   }
 
@@ -17,8 +28,14 @@ const ReplaceImageModal: React.FC = () => {
     dispatch(closeReplaceImageModal());
   };
 
+  const handleOk = () => {
+    form.validateFields().then(values => {
+      console.log(values);
+    });
+  };
+
   return (
-    <Modal visible={uiState.isOpen} onCancel={handleCancel}>
+    <Modal title={`Replace ${image.name} tag`} visible={uiState.isOpen} onCancel={handleCancel} onOk={handleOk}>
       <Form form={form} layout="vertical">
         <Form.Item name="tag" label="New Image Tag" rules={[{required: true, message: 'This field is required'}]}>
           <Input id="image-tag-input" placeholder="Enter image tag" />
