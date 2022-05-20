736f44b5d00a2a52c7c1eaf390db12be91d8d560
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2022-05-20 15:30:59 +0300

    feat: update images pane title

--- src/components/organisms/DockerImagesPane/DockerImagesPane.tsx
+++ src/components/organisms/DockerImagesPane/DockerImagesPane.tsx
@@ -7,7 +7,7 @@ import * as S from './DockerImagesPane.styled';
 const DockerImagesPane: React.FC = () => {
   return (
     <S.DockerImagesPaneContainer id="DockerImagesPane">
-      <TitleBar title="Docker Images" closable />
+      <TitleBar title="Images" closable />
 
       <S.List id="docker-images-section-container">
         <SectionRenderer sectionBlueprint={DockerImagesSectionBlueprint} level={0} isLastSection={false} />
