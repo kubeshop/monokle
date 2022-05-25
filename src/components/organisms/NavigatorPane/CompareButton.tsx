45602bca7bf8a0e4fe630f861d3d45ceb2f671f9
Wito Delnat <wito.delnat@gmail.com>
2022-05-25 10:33:18 +0200

    chore: remove default compare view

--- src/components/organisms/NavigatorPane/CompareButton.tsx
+++ src/components/organisms/NavigatorPane/CompareButton.tsx
@@ -7,22 +7,13 @@ import {SwapOutlined} from '@ant-design/icons';
 import {TOOLTIP_DELAY} from '@constants/constants';
 
 import {useAppDispatch} from '@redux/hooks';
-import {ComparisonView, compareToggled} from '@redux/reducers/compare';
+import {compareToggled} from '@redux/reducers/compare';
 
 export const CompareButton: React.FC = ({children}) => {
   const dispatch = useAppDispatch();
 
   const onClickClusterComparison = () => {
-    const defaultView: ComparisonView = {
-      leftSet: {
-        type: 'local',
-      },
-      rightSet: {
-        type: 'cluster',
-        context: 'minikube',
-      },
-    };
-    dispatch(compareToggled({value: true, initialView: defaultView}));
+    dispatch(compareToggled({value: true}));
   };
 
   return (
