36e0a7c07a45bb5740f74ec62c47c78aa36ee30d
Erdi Köse <me@erdkse.com>
2022-05-29 09:14:08 +0300

    chore: add project creation actions

--- src/components/organisms/PageHeader/CreateProject.tsx
+++ src/components/organisms/PageHeader/CreateProject.tsx
@@ -1,43 +1,71 @@
 import {Dropdown} from 'antd';
 
+import {useAppDispatch} from '@redux/hooks';
+import {setCreateProject} from '@redux/reducers/appConfig';
+import {openCreateProjectModal} from '@redux/reducers/ui';
+
+import FileExplorer from '@components/atoms/FileExplorer';
+
+import {useFileExplorer} from '@hooks/useFileExplorer';
+
 import * as S from './CreateProject.styled';
 
-const menuItems = [
-  {
-    icon: <S.FolderSmallWhite />,
-    label: 'New from local folder',
-  },
-  {
-    icon: <S.FolderSmallPlusWhite />,
-    label: 'New from scratch',
-  },
-  {
-    icon: <S.TemplateSmallWhite />,
-    label: 'New From template',
-  },
-];
-
-const itemsMenu = () => {
-  return (
-    <S.MenuContainer>
-      {menuItems.map(item => (
-        <S.MenuItem key={item.label}>
-          <S.MenuItemIcon>{item.icon}</S.MenuItemIcon>
-          <S.MenuItemLabel>{item.label}</S.MenuItemLabel>
-        </S.MenuItem>
-      ))}
-    </S.MenuContainer>
-  );
-};
-
 const CreateProject = () => {
+  const dispatch = useAppDispatch();
+
+  const {openFileExplorer, fileExplorerProps} = useFileExplorer(
+    ({folderPath}) => {
+      if (folderPath) {
+        dispatch(setCreateProject({rootFolder: folderPath}));
+      }
+    },
+    {isDirectoryExplorer: true}
+  );
+
+  const handleCreateProject = (fromTemplate: boolean) => {
+    dispatch(openCreateProjectModal({fromTemplate}));
+  };
   return (
     <S.DropdownContainer>
-      <Dropdown overlay={itemsMenu} placement="bottomLeft" trigger={['click']}>
+      <Dropdown
+        overlay={
+          <S.MenuContainer>
+            <S.MenuItem onClick={openFileExplorer}>
+              <S.MenuItemIcon>
+                <S.FolderSmallWhite />
+              </S.MenuItemIcon>
+              <S.MenuItemLabel>New from local folder</S.MenuItemLabel>
+            </S.MenuItem>
+            <S.MenuItem
+              onClick={() => {
+                handleCreateProject(false);
+              }}
+            >
+              <S.MenuItemIcon>
+                <S.FolderSmallPlusWhite />
+              </S.MenuItemIcon>
+              <S.MenuItemLabel>New from scratch</S.MenuItemLabel>
+            </S.MenuItem>
+            <S.MenuItem
+              onClick={() => {
+                handleCreateProject(true);
+              }}
+            >
+              <S.MenuItemIcon>
+                <S.TemplateSmallWhite />
+              </S.MenuItemIcon>
+              <S.MenuItemLabel>New From template</S.MenuItemLabel>
+            </S.MenuItem>
+          </S.MenuContainer>
+        }
+        placement="bottomLeft"
+        trigger={['click']}
+      >
         <S.Button type="link" size="small">
           <S.PlusIcon />
         </S.Button>
       </Dropdown>
+      <FileExplorer {...fileExplorerProps} />
     </S.DropdownContainer>
   );
 };
