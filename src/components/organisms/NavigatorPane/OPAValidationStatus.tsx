52c60b6d1071a5a06dae4b2b1091be9c1e38004c
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2022-06-15 10:51:57 +0300

    feat: number of active rules

--- src/components/organisms/NavigatorPane/OPAValidationStatus.tsx
+++ src/components/organisms/NavigatorPane/OPAValidationStatus.tsx
@@ -1,12 +1,25 @@
+import {useAppSelector} from '@redux/hooks';
+
 import {Icon} from '@components/atoms';
 
 import * as S from './OPAValidationStatus.styled';
 
 const OPAValidationStatus: React.FC = () => {
+  const activeRules = useAppSelector(state => {
+    const plugins = state.main.policies.plugins;
+    let numberOfActiveRules = 0;
+
+    plugins.forEach(plugin => {
+      numberOfActiveRules += plugin.config.enabledRules.length;
+    });
+
+    return numberOfActiveRules;
+  });
+
   return (
-    <S.Container $status="active">
+    <S.Container $status={activeRules ? 'active' : 'inactive'}>
       <Icon name="opa-status" />
-      {5}
+      {activeRules || '-'}
     </S.Container>
   );
 };
