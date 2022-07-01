52cdbf4dc65e91a58c93baa0410c94c6ecf1e45b
olensmar <ole@lensmar.com>
2022-07-01 10:28:23 +0200

    feat: added EndpointSlice handler

---
+++ src/kindhandlers/EndpointSlice.handler.ts
@@ -0,0 +1,67 @@
+import * as k8s from '@kubernetes/client-node';
+
+import navSectionNames from '@constants/navSectionNames';
+
+import {K8sResource} from '@models/k8sresource';
+import {ResourceKindHandler} from '@models/resourcekindhandler';
+
+import {
+  implicitNamespaceMatcher,
+  optionalExplicitNamespaceMatcher,
+  targetKindMatcher,
+} from '@src/kindhandlers/common/customMatchers';
+
+const EndpointSliceHandler: ResourceKindHandler = {
+  kind: 'EndpointSlice',
+  apiVersionMatcher: '**',
+  isNamespaced: true,
+  navigatorPath: [navSectionNames.K8S_RESOURCES, navSectionNames.NETWORK, 'EndpointSlice'],
+  clusterApiVersion: 'v1',
+  validationSchemaPrefix: 'io.k8s.api.discovery.v1',
+  isCustom: false,
+  getResourceFromCluster(kubeconfig: k8s.KubeConfig, resource: K8sResource): Promise<any> {
+    const discoveryV1Api = kubeconfig.makeApiClient(k8s.DiscoveryV1Api);
+    return discoveryV1Api.readNamespacedEndpointSlice(resource.name, resource.namespace || 'default', 'true');
+  },
+  async listResourcesInCluster(kubeconfig: k8s.KubeConfig, {namespace}) {
+    const discoveryV1Api = kubeconfig.makeApiClient(k8s.DiscoveryV1Api);
+    const response = namespace
+      ? await discoveryV1Api.listNamespacedEndpointSlice(namespace)
+      : await discoveryV1Api.listEndpointSliceForAllNamespaces();
+    return response.body.items;
+  },
+  async deleteResourceInCluster(kubeconfig: k8s.KubeConfig, resource: K8sResource) {
+    const discoveryV1Api = kubeconfig.makeApiClient(k8s.DiscoveryV1Api);
+    await discoveryV1Api.deleteNamespacedEndpointSlice(resource.name, resource.namespace || 'default');
+  },
+  outgoingRefMappers: [
+    {
+      source: {
+        pathParts: ['metadata', 'labels', 'kubernetes.io/service-name'],
+        siblingMatchers: {
+          namespace: implicitNamespaceMatcher,
+        },
+      },
+      target: {
+        kind: 'Service',
+      },
+      type: 'name',
+    },
+    {
+      source: {
+        pathParts: ['targetRef', 'name'],
+        siblingMatchers: {
+          namespace: optionalExplicitNamespaceMatcher,
+          kind: targetKindMatcher,
+        },
+      },
+      target: {
+        kind: '$.*',
+      },
+      type: 'name',
+    },
+  ],
+  helpLink: 'https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.22/#endpointslice-v1-discovery-k8s-io',
+};
+
+export default EndpointSliceHandler;
