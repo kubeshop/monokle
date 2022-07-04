106bb5b887b343a50f700861dd8189b50125623f
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2022-07-04 13:13:50 +0300

    refactor: cluster diff + conext menu

---
+++ src/components/molecules/ClusterDiff/ClusterDiffNamespaceFilter.styled.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const NamespaceFilterContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 300px;
+`;
+
+export const NamespaceFilterLabel = styled.span`
+  margin-right: 10px;
+`;
