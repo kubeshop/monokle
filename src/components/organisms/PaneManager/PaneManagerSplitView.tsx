40c4af8b61cd5a9b217378b2b8f777631ad0da23
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2022-07-25 17:23:06 +0300

    fix: top pane height

--- src/components/organisms/PaneManager/PaneManagerSplitView.tsx
+++ src/components/organisms/PaneManager/PaneManagerSplitView.tsx
@@ -1,4 +1,4 @@
-import React, {Suspense, useCallback} from 'react';
+import React, {Suspense, useCallback, useMemo} from 'react';
 
 import {GUTTER_SPLIT_VIEW_PANE_WIDTH, MIN_SPLIT_VIEW_PANE_WIDTH} from '@constants/constants';
 
@@ -23,11 +23,18 @@ const SearchPane = React.lazy(() => import('@organisms/SearchPane'));
 
 const PaneManagerSplitView: React.FC = () => {
   const dispatch = useAppDispatch();
+  const bottomPaneHeight = useAppSelector(state => state.ui.paneConfiguration.bottomPaneHeight);
+  const bottomSelection = useAppSelector(state => state.ui.leftMenu.bottomSelection);
   const layout = useAppSelector(state => state.ui.paneConfiguration);
   const leftActiveMenu = useAppSelector(state =>
     state.ui.leftMenu.isActive ? state.ui.leftMenu.selection : undefined
   );
   const {height, width} = useMainPaneDimensions();
+
+  const paneHeight = useMemo(
+    () => (bottomSelection ? height - bottomPaneHeight - 2 : height),
+    [bottomPaneHeight, bottomSelection, height]
+  );
 
   const handleResize = useCallback(
     (elements: any) => {
@@ -49,13 +56,13 @@ const PaneManagerSplitView: React.FC = () => {
         <ReflexElement id="leftPane" minSize={MIN_SPLIT_VIEW_PANE_WIDTH} flex={layout.leftPane}>
           <S.LeftPane>
             <Suspense fallback={<div />}>
-              {leftActiveMenu === 'file-explorer' && <FileTreePane height={height} />}
+              {leftActiveMenu === 'file-explorer' && <FileTreePane height={paneHeight} />}
               {leftActiveMenu === 'helm-pane' && <HelmPane />}
               {leftActiveMenu === 'kustomize-pane' && <KustomizePane />}
               {leftActiveMenu === 'images-pane' && <ImagesPane />}
-              {leftActiveMenu === 'templates-pane' && <TemplateManagerPane height={height} />}
-              {leftActiveMenu === 'validation-pane' && <ValidationPane height={height} />}
-              {leftActiveMenu === 'search' && <SearchPane height={height} />}
+              {leftActiveMenu === 'templates-pane' && <TemplateManagerPane height={paneHeight} />}
+              {leftActiveMenu === 'validation-pane' && <ValidationPane height={paneHeight} />}
+              {leftActiveMenu === 'search' && <SearchPane height={paneHeight} />}
             </Suspense>
           </S.LeftPane>
         </ReflexElement>
@@ -71,7 +78,7 @@ const PaneManagerSplitView: React.FC = () => {
         maxSize={MIN_SPLIT_VIEW_PANE_WIDTH + 200}
         flex={layout.navPane}
       >
-        <NavigatorPane height={height} />
+        <NavigatorPane height={paneHeight} />
       </ReflexElement>
 
       {/* react-reflex does not work as intended when you use propagate 
