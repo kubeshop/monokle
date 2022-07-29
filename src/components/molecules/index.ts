aca9a96a875d42ea28e3f5dbc03abb2d76a035e3
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2022-07-29 11:36:55 +0300

    refactor: small change

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -29,4 +29,4 @@ export {default as SectionRenderer} from './SectionRenderer';
 export {default as TemplateFormRenderer} from './TemplateFormRenderer';
 export {default as TitleBar} from './TitleBar';
 export {default as ValidationErrorsPopover} from './ValidationErrorsPopover';
-export {default as Walkthrough} from './Walkthrough/Walkthrough';
+export {default as Walkthrough} from './Walkthrough';
