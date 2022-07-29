290e011b8d6eb2d7f74314cb75eedad2b475388d
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2022-07-29 14:58:08 +0300

    fix: warning

--- src/navsections/K8sResourceSectionBlueprint/ResourceKindInformation.styled.tsx
+++ src/navsections/K8sResourceSectionBlueprint/ResourceKindInformation.styled.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 
 import Colors from '@styles/Colors';
 
-export const InfoCircleOutlined = styled(RawInfoCircleOutlined)<{isSelected: boolean}>`
-  color: ${props => (props.isSelected ? Colors.blackPure : Colors.blue9)};
+export const InfoCircleOutlined = styled(RawInfoCircleOutlined)<{$isSelected: boolean}>`
+  color: ${props => (props.$isSelected ? Colors.blackPure : Colors.blue9)};
   padding: 0 5px;
 `;
