f1082b9042ceb24df6f21af78376cc05f2101b63
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2022-08-02 12:07:59 +0300

    refactor: redesign terminals tabs

--- src/components/organisms/BottomPaneManager/BottomPaneManager.styled.tsx
+++ src/components/organisms/BottomPaneManager/BottomPaneManager.styled.tsx
@@ -1,22 +1,44 @@
+import {CloseOutlined as RawCloseOutlined, PlusCircleFilled as RawPlusCircleFilled} from '@ant-design/icons';
+
 import styled from 'styled-components';
 
 import {AppBorders} from '@styles/Borders';
-import {PanelColors} from '@styles/Colors';
+import Colors, {PanelColors} from '@styles/Colors';
 
 export const BottomPaneManagerContainer = styled.div`
   height: 100%;
   border-left: 9px solid ${PanelColors.toolBar};
-  padding: 8px 16px;
+`;
+
+export const CloseOutlined = styled(RawCloseOutlined)`
+  color: ${Colors.grey6};
+`;
+
+export const PlusCircleFilled = styled(RawPlusCircleFilled)`
+  color: ${Colors.blue6};
+  font-size: 16px;
+  cursor: pointer;
 `;
 
 export const Tab = styled.div<{$selected: boolean}>`
-  font-weight: ${({$selected}) => ($selected ? '700' : '400')};
-
-  padding: 10px;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  padding: 10px 1px;
   cursor: pointer;
-  border: ${AppBorders.sectionDivider};
+  color: ${({$selected}) => ($selected ? Colors.blue6 : Colors.whitePure)};
+  font-weight: ${({$selected}) => ($selected ? '700' : '400')};
+  ${({$selected}) => ($selected ? `border-bottom: 1px solid ${Colors.blue6}` : '')};
+`;
+
+export const Tabs = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 14px;
 `;
 
 export const TabsContainer = styled.div`
   display: flex;
+  border-bottom: ${AppBorders.sectionDivider};
+  padding: 0px 10px;
 `;
--- src/components/organisms/BottomPaneManager/BottomPaneManager.tsx
+++ src/components/organisms/BottomPaneManager/BottomPaneManager.tsx
@@ -1,9 +1,17 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
+import {useMeasure} from 'react-use';
+
+import {Tooltip} from 'antd';
 
 import {v4 as uuidv4} from 'uuid';
 
+import {TOOLTIP_DELAY} from '@constants/constants';
+import {AddTerminalTooltip, KillTerminalTooltip} from '@constants/tooltips';
+
 import {useAppDispatch, useAppSelector} from '@redux/hooks';
 import {addTerminal, setSelectedTerminal} from '@redux/reducers/terminal';
+
+import {Icon} from '@atoms';
 
 import TerminalPane from '../TerminalPane';
 import * as S from './BottomPaneManager.styled';
@@ -13,6 +21,22 @@ const BottomPaneManager: React.FC = () => {
   const bottomSelection = useAppSelector(state => state.ui.leftMenu.bottomSelection);
   const selectedTerminal = useAppSelector(state => state.terminal.selectedTerminal);
   const terminalsMap = useAppSelector(state => state.terminal.terminalsMap);
+
+  const [terminalToKill, setTerminalToKill] = useState<string>('');
+
+  const [bottomPaneManagerRef, {height}] = useMeasure<HTMLDivElement>();
+  const [tabsContainerRef, {height: tabsContainerHeight}] = useMeasure<HTMLDivElement>();
+
+  const onAddTerminalHandler = () => {
+    const newTerminalId = uuidv4();
+
+    dispatch(setSelectedTerminal(newTerminalId));
+    dispatch(addTerminal({id: newTerminalId, isRunning: false}));
+  };
+
+  const onKillTerminalHandler = (terminalId: string) => {
+    setTerminalToKill(terminalId);
+  };
 
   // treat the case where the bottom selection is first set to terminal or terminal was already opened
   useEffect(() => {
@@ -26,19 +50,33 @@ const BottomPaneManager: React.FC = () => {
   }, [bottomSelection, dispatch, selectedTerminal]);
 
   return (
-    <S.BottomPaneManagerContainer>
-      {Object.keys(terminalsMap).length > 1 ? (
-        <S.TabsContainer>
+    <S.BottomPaneManagerContainer ref={bottomPaneManagerRef}>
+      <S.TabsContainer ref={tabsContainerRef}>
+        <S.Tabs>
           {Object.keys(terminalsMap).map((id, index) => (
             <S.Tab key={id} $selected={selectedTerminal === id} onClick={() => dispatch(setSelectedTerminal(id))}>
-              Terminal {index}
+              <Icon name="terminal" />
+              Terminal {index ? index + 1 : ''}
+              <Tooltip mouseEnterDelay={TOOLTIP_DELAY} title={KillTerminalTooltip}>
+                <S.CloseOutlined onClick={() => onKillTerminalHandler(id)} />
+              </Tooltip>
             </S.Tab>
           ))}
-        </S.TabsContainer>
-      ) : null}
+
+          <Tooltip mouseEnterDelay={TOOLTIP_DELAY} title={AddTerminalTooltip}>
+            <S.PlusCircleFilled onClick={onAddTerminalHandler} />
+          </Tooltip>
+        </S.Tabs>
+      </S.TabsContainer>
 
       {Object.values(terminalsMap).map(terminal => (
-        <TerminalPane key={terminal.id} terminal={terminal} />
+        <TerminalPane
+          key={terminal.id}
+          height={height - tabsContainerHeight}
+          terminal={terminal}
+          terminalToKill={terminalToKill}
+          onTerminalKilled={() => setTerminalToKill('')}
+        />
       ))}
     </S.BottomPaneManagerContainer>
   );
