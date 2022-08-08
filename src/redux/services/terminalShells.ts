1a3b5c2f285e8452ecfc98163b1ee8eccd080c35
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2022-08-08 12:31:04 +0300

    feat: mac/linux shell + default shell

--- src/redux/services/terminalShells.ts
+++ src/redux/services/terminalShells.ts
@@ -1,4 +1,8 @@
+import {detectDefaultShell} from 'default-shell';
 import fs from 'fs';
+import path from 'path';
+
+import {SUPPORTED_SHELLS} from '@constants/terminal';
 
 import {AppDispatch} from '@models/appdispatch';
 import {ShellsMapType, TerminalSettingsType} from '@models/terminal';
@@ -6,11 +10,27 @@ import {ShellsMapType, TerminalSettingsType} from '@models/terminal';
 import {setShells, setTerminalSettings} from '@redux/reducers/terminal';
 
 // get available shells for mac/linux
-const detectAvailableUnixShells = (): ShellsMapType => {
-  const contents = fs.readFileSync('/etc/shells', 'utf8');
-  console.log(contents);
+const detectAvailableUnixShells = (osPlatform: NodeJS.Platform): ShellsMapType => {
+  const getBasename = osPlatform === 'win32' ? path.win32.basename : path.basename;
 
-  return {bash: {name: 'Bash', shell: 'bash'}, zsh: {name: 'Zsh', shell: 'zsh'}};
+  const contents = fs.readFileSync('/etc/shells', 'utf8');
+  const shells = contents.split('\n').filter(e => e.trim().indexOf('#') !== 0 && e.trim().length > 0);
+
+  let shellsMap: ShellsMapType = {};
+
+  if (osPlatform === 'darwin') {
+    shells.forEach(shellPath => {
+      const shell = getBasename(shellPath);
+
+      if (SUPPORTED_SHELLS['darwin'].includes(shell)) {
+        shellsMap[shell] = {name: shell.charAt(0).toUpperCase() + shell.slice(1), shell};
+      }
+    });
+  } else {
+    shellsMap['bash'] = {name: 'Bash', shell: 'bash'};
+  }
+
+  return shellsMap;
 };
 
 export const setTerminalShells = (
@@ -21,14 +41,28 @@ export const setTerminalShells = (
   let shellsMap: ShellsMapType = {};
 
   if (osPlatform === 'win32') {
-    shellsMap['powershell'] = {name: 'Powershell', shell: 'powershell.exe'};
-    shellsMap['cmd'] = {name: 'Command Prompt', shell: 'cmd.exe'};
+    shellsMap['powershell.exe'] = {name: 'Powershell', shell: 'powershell.exe'};
+    shellsMap['cmd.exe'] = {name: 'Command Prompt', shell: 'cmd.exe'};
   } else {
-    shellsMap = detectAvailableUnixShells();
+    shellsMap = detectAvailableUnixShells(osPlatform);
   }
 
   if (!settings.defaultShell) {
-    dispatch(setTerminalSettings({...settings, defaultShell: Object.keys(shellsMap)[0]}));
+    const getBasename = osPlatform === 'win32' ? path.win32.basename : path.basename;
+    const defaultShellPath = detectDefaultShell();
+    const defaultDetectedShell = getBasename(defaultShellPath);
+
+    let defaultShell = '';
+
+    if (osPlatform === 'win32' || osPlatform === 'darwin') {
+      defaultShell = SUPPORTED_SHELLS[osPlatform].includes(defaultDetectedShell)
+        ? defaultDetectedShell
+        : SUPPORTED_SHELLS[osPlatform][0];
+    } else {
+      defaultShell = SUPPORTED_SHELLS['other'][0];
+    }
+
+    dispatch(setTerminalSettings({...settings, defaultShell}));
   }
 
   dispatch(setShells(shellsMap));
