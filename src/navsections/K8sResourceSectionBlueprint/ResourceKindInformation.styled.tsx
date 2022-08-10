22ecf17e28d5f9434a1807a6b7dab1304783e78c
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2022-08-10 13:02:09 +0300

    refactor: age/status styling

--- src/navsections/K8sResourceSectionBlueprint/ResourceKindInformation.styled.tsx
+++ src/navsections/K8sResourceSectionBlueprint/ResourceKindInformation.styled.tsx
@@ -1,3 +1,5 @@
+import {Tag as RawTag} from 'antd';
+
 import {InfoCircleOutlined as RawInfoCircleOutlined} from '@ant-design/icons';
 
 import styled from 'styled-components';
@@ -7,4 +9,18 @@ import Colors from '@styles/Colors';
 export const InfoCircleOutlined = styled(RawInfoCircleOutlined)<{$isSelected: boolean}>`
   color: ${props => (props.$isSelected ? Colors.blackPure : Colors.blue9)};
   padding: 0 5px;
+`;
+
+export const InfoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  color: ${Colors.grey9};
+`;
+
+export const Tag = styled(RawTag)`
+  color: ${Colors.yellow8};
+  background: rgba(252, 255, 130, 0.15);
+  border-radius: 4px;
+  margin: 0;
 `;
