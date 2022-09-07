e87d70671ebf7f6ac7f91077e9f73860dd9a8907
Erdi Köse <me@erdkse.com>
2022-09-07 13:21:26 +0300

    chore: add tag to environment

--- generate-cask.sh
+++ generate-cask.sh
@@ -1,13 +1,14 @@
-output=$(shasum -a 256 dist/Monokle-mac-universal.dmg)
+sha256=$(shasum -a 256 dist/Monokle-mac-universal.dmg | awk '{print $1}')
 version=$(jq -r .version package.json)
+description=$(jq -r .description package.json)
 
 cat  << EOF
 cask "monokle" do  
     version "$version"
-    url "https://github.com/kubeshop/monokle/releases/download/latest-version/Monokle-mac-universal.dmg"
-    sha256 "$output"
+    url "https://github.com/kubeshop/monokle/releases/download/${TAG}/Monokle-mac-universal.dmg"
+    sha256 "$sha256"
     name "Monokle"
-    desc "UI for managing k8s manifests"
+    desc "$description"
     homepage "https://monokle.io/"
     app "Monokle.app"
   end
