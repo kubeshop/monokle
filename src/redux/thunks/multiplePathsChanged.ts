fe6f918bf999a2ba52b41c4c983d53da40850781
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2022-09-14 13:27:29 +0300

    feat: monitor .git folder

--- src/redux/thunks/multiplePathsChanged.ts
+++ src/redux/thunks/multiplePathsChanged.ts
@@ -2,14 +2,10 @@ import {createAsyncThunk, createNextState} from '@reduxjs/toolkit';
 
 import micromatch from 'micromatch';
 
-import {GitRepo} from '@models/git';
 import {RootState} from '@models/rootstate';
 
-import {setChangedFiles, setCurrentBranch, setRepo} from '@redux/git';
 import {currentConfigSelector} from '@redux/selectors';
 import {addPath, getFileEntryForAbsolutePath, reloadFile} from '@redux/services/fileEntry';
-
-import {promiseFromIpcRenderer} from '@utils/promises';
 
 export const multiplePathsChanged = createAsyncThunk(
   'main/multiplePathsChanged',
@@ -17,8 +13,6 @@ export const multiplePathsChanged = createAsyncThunk(
     const state: RootState = thunkAPI.getState();
     const projectConfig = currentConfigSelector(state);
     const userDataDir = String(state.config.userDataDir);
-    const projectRootFolderPath = state.config.selectedProjectRootFolder;
-    const gitRepo = state.git.repo;
 
     const nextMainState = createNextState(state.main, mainState => {
       filePaths.forEach((filePath: string) => {
@@ -30,22 +24,6 @@ export const multiplePathsChanged = createAsyncThunk(
         }
       });
     });
-
-    if (gitRepo) {
-      promiseFromIpcRenderer<GitRepo>('git.fetchGitRepo', 'git.fetchGitRepo.result', projectRootFolderPath).then(
-        result => {
-          thunkAPI.dispatch(setRepo(result));
-          thunkAPI.dispatch(setCurrentBranch(result.currentBranch));
-        }
-      );
-
-      promiseFromIpcRenderer('git.getChangedFiles', 'git.getChangedFiles.result', {
-        localPath: projectRootFolderPath,
-        fileMap: nextMainState.fileMap,
-      }).then(result => {
-        thunkAPI.dispatch(setChangedFiles(result));
-      });
-    }
 
     return nextMainState;
   }
