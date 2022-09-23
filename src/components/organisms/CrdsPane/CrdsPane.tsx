454522ffe7eaada798edfc7803e6befc550a3bbe
Dev Catalin <20538711+devcatalin@users.noreply.github.com>
2022-09-23 13:46:23 +0300

    feat: show list of registered CRDs

--- src/components/organisms/CrdsPane/CrdsPane.tsx
+++ src/components/organisms/CrdsPane/CrdsPane.tsx
@@ -1,9 +1,24 @@
+import {useMemo} from 'react';
+
+import {useAppSelector} from '@redux/hooks';
+import {registeredKindHandlersSelector} from '@redux/selectors';
+
 import {TitleBar} from '@molecules';
 
 const CrdsPane: React.FC = () => {
+  const kindHandlers = useAppSelector(registeredKindHandlersSelector);
+  const crdKindHandlers = useMemo(() => kindHandlers.filter(kh => kh.isCustom), [kindHandlers]);
+
   return (
     <div>
       <TitleBar title="Custom Resource Definitions" closable />
+      <ul>
+        {crdKindHandlers.map(c => (
+          <li>
+            {c.clusterApiVersion} - {c.kind}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
