53fa8fb26263fa62b079af6abaae43a4e7312db7
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2022-10-07 10:59:47 +0300

    feat: browse location

--- src/components/organisms/GitCloneModal/GitCloneModal.tsx
+++ src/components/organisms/GitCloneModal/GitCloneModal.tsx
@@ -1,12 +1,29 @@
 import {Button, Form, Input, Modal} from 'antd';
+import {useForm} from 'antd/lib/form/Form';
 
 import {DEFAULT_GIT_REPO_PLACEHOLDER} from '@constants/constants';
 
 import {closeGitCloneModal} from '@redux/git';
-import {useAppDispatch} from '@redux/hooks';
+import {useAppDispatch, useAppSelector} from '@redux/hooks';
+
+import {FileExplorer} from '@atoms';
+
+import {useFileExplorer} from '@hooks/useFileExplorer';
 
 const GitCloneModal: React.FC = () => {
   const dispatch = useAppDispatch();
+  const projectsRootPath = useAppSelector(state => state.config.projectsRootPath);
+
+  const [form] = useForm();
+
+  const {openFileExplorer, fileExplorerProps} = useFileExplorer(
+    ({folderPath}) => {
+      if (folderPath) {
+        form.setFieldValue('location', folderPath);
+      }
+    },
+    {isDirectoryExplorer: true}
+  );
 
   const onCancel = () => {
     dispatch(closeGitCloneModal());
@@ -14,7 +31,7 @@ const GitCloneModal: React.FC = () => {
 
   return (
     <Modal open onCancel={onCancel}>
-      <Form layout="vertical">
+      <Form form={form} initialValues={{repoURL: '', location: projectsRootPath}} layout="vertical">
         <Form.Item
           name="repoURL"
           label="Repository URL"
@@ -27,15 +44,20 @@ const GitCloneModal: React.FC = () => {
         >
           <Input placeholder={DEFAULT_GIT_REPO_PLACEHOLDER} />
         </Form.Item>
+
         <Form.Item label="Location">
           <Input.Group compact>
-            <Form.Item name="localPath">
+            <Form.Item name="location" noStyle>
               <Input style={{width: 'calc(100% - 100px)'}} />
             </Form.Item>
-            <Button style={{width: '100px'}}>Browse</Button>
+            <Button style={{width: '100px'}} onClick={openFileExplorer}>
+              Browse
+            </Button>
           </Input.Group>
         </Form.Item>
       </Form>
+
+      <FileExplorer {...fileExplorerProps} />
     </Modal>
   );
 };
