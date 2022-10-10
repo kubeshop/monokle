f4722f224b64f2ad7954520465eb10fd6fdd2f6a
Dev Catalin <20538711+devcatalin@users.noreply.github.com>
2022-10-10 16:00:58 +0300

    feat: modal for creating new resource from CRD

---
+++ src/components/organisms/ValidationPane/CRDsSchemaValidation/CRDItem.styled.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const DotsContainer = styled.div`
+  cursor: pointer;
+`;
+
+export const Row = styled.li`
+  display: flex;
+  justify-content: space-between;
+`;
