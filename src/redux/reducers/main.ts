22581fda0f287a289f97bfab5a76056bb866c4b4
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2022-10-13 12:23:27 +0300

    fix: remove unused

--- src/redux/reducers/main.ts
+++ src/redux/reducers/main.ts
@@ -24,7 +24,6 @@ import {
   SelectionHistoryEntry,
 } from '@models/appstate';
 import {CurrentMatch, FileEntry} from '@models/fileentry';
-import {GitChangedFile, GitRepo} from '@models/git';
 import {HelmChart} from '@models/helm';
 import {ImageType} from '@models/image';
 import {ValidationIntegration} from '@models/integrations';
