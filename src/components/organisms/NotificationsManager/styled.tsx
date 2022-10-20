1df81bca5241770e7b2b07f04e53c484b125cc5e
Divv Saxena <111836493+DivvSaxena@users.noreply.github.com>
2022-10-20 15:17:07 +0530

    Update styled.tsx

--- src/components/organisms/NotificationsManager/styled.tsx
+++ src/components/organisms/NotificationsManager/styled.tsx
@@ -8,24 +8,28 @@ import styled from 'styled-components';
 
 import Colors, {FontColors} from '@styles/Colors';
 
+html{
+ font-size : 10px;
+}
+
 export const CheckCircleOutlined = styled(RawCheckCircleOutlined)`
   color: ${Colors.polarGreen};
-  font-size: 16px;
+  font-size: 1.6rem;
 `;
 
 export const ExclamationCircleOutlined = styled(RawExclamationCircleOutlined)`
   color: ${FontColors.error};
-  font-size: 16px;
+  font-size: 1.6rem;
 `;
 
 export const ExclamationCircleOutlinedWarning = styled(RawExclamationCircleOutlined)`
   color: ${Colors.yellowWarning};
-  font-size: 16px;
+  font-size: 1.6rem;
 `;
 
 export const InfoCircleOutlined = styled(RawInfoCircleOutlined)`
   color: ${Colors.cyan};
-  font-size: 16px;
+  font-size: 1.6rem;
 `;
 
 export const NoNotificationsContainer = styled.div`
