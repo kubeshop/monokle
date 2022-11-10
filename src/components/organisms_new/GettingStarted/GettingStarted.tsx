8fbee69b3373ac2f4e98cf9fb4251bcfa5f9a1b9
manoukyanmari <manoukyan.mariam@gmail.com>
2022-11-10 16:58:34 +0400

    feat: 2621: merge with 2.0 resolving conflicts

--- src/components/organisms_new/GettingStarted/GettingStarted.tsx
+++ src/components/organisms_new/GettingStarted/GettingStarted.tsx
@@ -2,14 +2,11 @@ import {useMemo} from 'react';
 
 import {useAppSelector} from '@redux/hooks';
 
-// import ValidationOverView from "@organisms/ValidationPane/ValidationOverview/ValidationOverview";
 import GettingStartedOverview from '@organismsNew/GettingStarted/GettingStartedOverview';
 
 import {usePaneHeight} from '@hooks/usePaneHeight';
 
 import * as S from './GettingStarted.styled';
-
-// import {DEFAULT_PANE_TITLE_HEIGHT} from "@constants/constants";
 
 const GettingStarted = () => {
   const integration = useAppSelector(state => state.main.validationIntegration);
