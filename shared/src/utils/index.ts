ba03b9de49a069d38c396e3f97fc18e4fd6d9297
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2022-11-14 17:50:59 +0200

    refactor: shared logs utils

--- shared/src/utils/index.ts
+++ shared/src/utils/index.ts
@@ -2,5 +2,6 @@ export * from './electronStore';
 export * from './env';
 export * from './fileSystem';
 export * from './hotkey';
+export * from './logs';
 export * from './selectors';
 export * from './shell';
