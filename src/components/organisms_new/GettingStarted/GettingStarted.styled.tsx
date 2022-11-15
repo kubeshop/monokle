aacf7625c1d2dbd8245d007f93dc4f7354867627
manoukyanmari <manoukyan.mariam@gmail.com>
2022-11-15 19:47:51 +0400

    feat: 2621: Remove height dynamic value and add 100%

--- src/components/organisms_new/GettingStarted/GettingStarted.styled.tsx
+++ src/components/organisms_new/GettingStarted/GettingStarted.styled.tsx
@@ -3,7 +3,7 @@ import {Skeleton as RawSkeleton} from 'antd';
 import styled from 'styled-components';
 
 export const GettingStartedContainer = styled.div<{$height: number}>`
-  height: ${({$height}) => $height}px;
+  height: 100%;
 `;
 
 export const Skeleton = styled(RawSkeleton)`
