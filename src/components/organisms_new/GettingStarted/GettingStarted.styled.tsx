994891d9d07a527cd06f4bef0097473b588b3a60
manoukyanmari <manoukyan.mariam@gmail.com>
2022-11-17 09:39:42 +0400

    feat: 2621: restructure the Getting started page

--- src/components/organisms_new/GettingStarted/GettingStarted.styled.tsx
+++ src/components/organisms_new/GettingStarted/GettingStarted.styled.tsx
@@ -1,12 +1,56 @@
 import {Skeleton as RawSkeleton} from 'antd';
 
+import {RightOutlined} from '@ant-design/icons';
+
 import styled from 'styled-components';
 
-export const GettingStartedContainer = styled.div<{$height: number}>`
+export const GettingStartedContainer = styled.div`
   height: 100%;
 `;
 
 export const Skeleton = styled(RawSkeleton)`
   margin: 20px;
   width: 90%;
+`;
+
+export const GettingStartedCards = styled.div`
+  margin-top: 20px;
+  display: flex;
+  gap: 10px;
+`;
+
+export const GettingStartedReferences = styled.div`
+  margin-top: 40px;
+  display: grid;
+  gap: 10px;
+  grid-template-columns: repeat(auto-fit, minmax(180px, 1fr));
+  grid-auto-rows: 180px;
+`;
+
+export const GettingStartedTitle = styled.h1`
+  font-size: 16px;
+  font-weight: 700;
+  //font-family: 'Inter';
+  line-height: 22px;
+  letter-spacing: 0;
+  text-align: left;
+  margin-top: 33px;
+  margin-bottom: 20px;
+`;
+export const GettingStartedSubTitle = styled.h3`
+  letter-spacing: 0;
+  text-align: left;
+  margin-bottom: 36px;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 22px;
+  color: #acacac;
+`;
+
+export const Arrow = styled(RightOutlined)`
+  font-size: 8px;
+  cursor: pointer;
+  width: 8px;
+  align-self: center;
 `;
--- src/components/organisms_new/GettingStarted/GettingStarted.tsx
+++ src/components/organisms_new/GettingStarted/GettingStarted.tsx
@@ -1,17 +1,48 @@
 import React from 'react';
 
-import GettingStartedOverview from '@organismsNew/GettingStarted/GettingStartedOverview';
-
-import {usePaneHeight} from '@hooks/usePaneHeight';
+import {EDIT, EXPLORE, PUBLISH, VALIDATE} from '@monokle-desktop/shared/constants/features';
+import {
+  DISCORD,
+  DOCUMENTATION,
+  FEEDBACK,
+  GUIDE,
+  TUTORIAL,
+  WHATSNEW,
+} from '@monokle-desktop/shared/constants/referenceLinks';
 
 import * as S from './GettingStarted.styled';
+import GettingStartedCard from './GettingStartedCard';
+import GettingStartedReference from './GettingStartedReference';
 
 const GettingStarted = () => {
-  const height = usePaneHeight();
-
   return (
-    <S.GettingStartedContainer $height={height}>
-      <GettingStartedOverview />
+    <S.GettingStartedContainer>
+      <S.GettingStartedTitle>Starting with Monokle</S.GettingStartedTitle>
+      <S.GettingStartedSubTitle>
+        Select in which stage of the K8s manifests management you are in (or from which one you want to learn more
+        about) and let us show you how Monokle can help you.
+      </S.GettingStartedSubTitle>
+
+      <S.GettingStartedCards>
+        <GettingStartedCard feature={EXPLORE} />
+        <S.Arrow />
+        <GettingStartedCard feature={EDIT} />
+        <S.Arrow />
+        <GettingStartedCard feature={VALIDATE} />
+        <S.Arrow />
+        <GettingStartedCard feature={PUBLISH} />
+        <S.Arrow />
+      </S.GettingStartedCards>
+
+      <S.GettingStartedTitle>Helpful Resources</S.GettingStartedTitle>
+      <S.GettingStartedReferences>
+        <GettingStartedReference referenceLink={GUIDE} />
+        <GettingStartedReference referenceLink={TUTORIAL} />
+        <GettingStartedReference referenceLink={DOCUMENTATION} />
+        <GettingStartedReference referenceLink={DISCORD} />
+        <GettingStartedReference referenceLink={WHATSNEW} />
+        <GettingStartedReference referenceLink={FEEDBACK} />
+      </S.GettingStartedReferences>
     </S.GettingStartedContainer>
   );
 };
