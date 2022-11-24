9a944fd0082ae5f7ce5eabfc322e94a30d54625c
Dev Catalin <20538711+devcatalin@users.noreply.github.com>
2022-11-24 19:35:50 +0200

    refactor: kind handler model

--- src/shared/models/resourceKindHandler.ts
+++ src/shared/models/resourceKindHandler.ts
@@ -1,52 +1,6 @@
 import * as k8s from '@kubernetes/client-node';
 
-import {monaco} from 'react-monaco-editor';
-
-import {JsonObject} from 'type-fest';
-
-import {K8sResource} from './k8sResource';
-
-type SymbolMatcher = {
-  isMatch?(symbols: monaco.languages.DocumentSymbol[]): boolean;
-};
-
-type RefMapper = {
-  source: {
-    pathParts: string[];
-
-    // sibling matchers that will be used to validate this ref, maps the name of a sibling property to
-    // a function that validates if the corresponding property value is a valid match
-    siblingMatchers?: Record<
-      string, // name of the property to match, for example 'kind'
-      (
-        sourceResource: K8sResource,
-        targetResource: K8sResource,
-        value: string, // the actual value of the property to be matched
-        siblingValues: Record<string, string>, // a map of all sibling property values
-        properties?: JsonObject // optional configuration properties passed to matcher (see below)
-      ) => boolean // function that controls if the specified value
-    >;
-
-    // optional matcher configuration properties that will be passed to each correspondingly named matcher
-    matcherProperties?: Record<string, JsonObject>;
-
-    // optionally checks for an 'optional' sibling to validate ref
-    isOptional?: boolean;
-  };
-  target: {
-    kind: string;
-    pathParts?: string[];
-  };
-
-  type: 'path' | 'name' | 'pairs' | 'image';
-
-  // called to validate if an unsatisfied ref should be created for this mapper
-  shouldCreateUnsatisfiedRef?: (
-    refMapper: RefMapper,
-    sourceResource: K8sResource,
-    siblingValues: Record<string, string>
-  ) => boolean;
-};
+import {ResourceMeta} from './k8sResource';
 
 type ClusterResourceOptions = {
   namespace?: string;
@@ -93,7 +47,7 @@ type ResourceKindHandler = {
    * Retrieve the specified resource of this type using the provided kubeconfig
    */
 
-  getResourceFromCluster(kubeconfig: k8s.KubeConfig, resource: K8sResource): Promise<any>;
+  getResourceFromCluster(kubeconfig: k8s.KubeConfig, resource: ResourceMeta): Promise<any>;
 
   /**
    * Get all resources of this type using the provided kubeconfig
@@ -102,20 +56,14 @@ type ResourceKindHandler = {
   listResourcesInCluster(
     kubeconfig: k8s.KubeConfig,
     options: ClusterResourceOptions,
-    crd?: K8sResource
+    crd?: ResourceMeta
   ): Promise<any[]>;
 
   /**
    * Delete the specified resource from the cluster
    */
 
-  deleteResourceInCluster: (kubeconfig: k8s.KubeConfig, resource: K8sResource) => Promise<void>;
-
-  /**
-   * optional outgoing RefMappers to use for resolving refs in resources of this type
-   */
-
-  outgoingRefMappers?: RefMapper[];
+  deleteResourceInCluster: (kubeconfig: k8s.KubeConfig, resource: ResourceMeta) => Promise<void>;
 
   /**
    * The desired navigator section and subsection for this resource type,
@@ -124,15 +72,6 @@ type ResourceKindHandler = {
    */
 
   navigatorPath: [navigatorName: string, sectionName: string, subsectionName: string];
-
-  /**
-   * optional JSON Schema for validation and symbol-matchers to pass to the source editor
-   */
-
-  sourceEditorOptions?: {
-    editorSchema: any;
-    symbolMatchers?: SymbolMatcher[];
-  };
 
   /**
    * optional form editor options; if undefined no form editor will be shown
@@ -151,4 +90,4 @@ type ResourceKindHandler = {
   validationSchemaPrefix?: string;
 };
 
-export type {ClusterResourceOptions, ResourceKindHandler, RefMapper, SymbolMatcher};
+export type {ClusterResourceOptions, ResourceKindHandler};
