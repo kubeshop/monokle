45d5828f065b002f5a62b2af4a6c826c93f99a8f
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2022-11-24 17:21:26 +0200

    feat: sort file explorer by settings

--- src/components/organisms/FileTreePane/FileTreePane.tsx
+++ src/components/organisms/FileTreePane/FileTreePane.tsx
@@ -38,6 +38,8 @@ import {
 } from '@hooks/fileTreeHooks';
 import {usePaneHeight} from '@hooks/usePaneHeight';
 
+import {sortFoldersFiles} from '@utils/fileExplorer';
+
 import {createNode} from './CreateNode';
 import TreeItem from './TreeItem';
 import {TreeNode} from './types';
@@ -50,6 +52,7 @@ const FileTreePane: React.FC = () => {
 
   const dispatch = useAppDispatch();
   const expandedFolders = useAppSelector(state => state.ui.leftMenu.expandedFolders);
+  const fileExplorerSortOrder = useAppSelector(state => state.config.fileExplorerSortOrder);
   const fileMap = useAppSelector(state => state.main.fileMap);
   const fileOrFolderContainedInFilter = useAppSelector(state => state.main.resourceFilter.fileOrFolderContainedIn);
   const isFolderLoading = useAppSelector(state => state.ui.isFolderLoading);
@@ -59,6 +62,7 @@ const FileTreePane: React.FC = () => {
   const resourceMap = useAppSelector(state => state.main.resourceMap);
   const selectedPath = useAppSelector(state => state.main.selectedPath);
   const selectedResourceId = useAppSelector(state => state.main.selectedResourceId);
+
   const {onFileSelect} = useFileSelect();
   const {onPreview} = usePreview();
   const {onDelete, processingEntity, setProcessingEntity} = useDelete();
@@ -253,7 +257,7 @@ const FileTreePane: React.FC = () => {
           <S.TreeDirectoryTree
             height={height - 2 * DEFAULT_PANE_TITLE_HEIGHT - 20}
             onSelect={onFileSelect}
-            treeData={[tree]}
+            treeData={[sortFoldersFiles(fileExplorerSortOrder, tree)]}
             ref={treeRef}
             expandedKeys={expandedFolders}
             onExpand={onExpand}
