097764af5cf9221dbe68ff1ed724c3afb40d78b4
Dev Catalin <20538711+devcatalin@users.noreply.github.com>
2022-11-26 11:19:20 +0200

    refactor: improve resources & maps types

--- src/shared/models/k8sResource.ts
+++ src/shared/models/k8sResource.ts
@@ -1,7 +1,7 @@
 import {KubernetesObject} from './appState';
-import {LocalOrigin, ResourceOrigin} from './origin';
+import {ClusterOrigin, HelmOrigin, KustomizeOrigin, LocalOrigin, ResourceOrigin} from './origin';
 
-type ResourceMeta = {
+export type ResourceMeta = {
   /** an internally generated UUID
    * - used for references/lookups in resourceMaps */
   id: string;
@@ -26,14 +26,39 @@ type ResourceMeta = {
   isUnsaved?: boolean;
 };
 
-type ResourceContent = {
+export type LocalResourceMeta = ResourceMeta & {origin: LocalOrigin};
+export type ClusterResourceMeta = ResourceMeta & {origin: ClusterOrigin};
+export type HelmResourceMeta = ResourceMeta & {origin: HelmOrigin};
+export type KustomizeResourceMeta = ResourceMeta & {origin: KustomizeOrigin};
+
+export type ResourceContent = {
   id: string;
+  origin: ResourceOrigin;
   text: string;
   object: KubernetesObject;
 };
 
-type K8sResource = ResourceMeta & ResourceContent;
+export type LocalResourceContent = ResourceContent & {origin: LocalOrigin};
+export type ClusterResourceContent = ResourceContent & {origin: ClusterOrigin};
+export type HelmResourceContent = ResourceContent & {origin: HelmOrigin};
+export type KustomizeResourceContent = ResourceContent & {origin: KustomizeOrigin};
 
-type LocalK8sResource = K8sResource & {origin: LocalOrigin};
+export type K8sResource = ResourceMeta & ResourceContent;
+export type LocalK8sResource = K8sResource & {origin: LocalOrigin};
+export type ClusterK8sResource = K8sResource & {origin: ClusterOrigin};
+export type HelmK8sResource = K8sResource & {origin: HelmOrigin};
+export type KustomizeK8sResource = K8sResource & {origin: KustomizeOrigin};
 
-export type {K8sResource, LocalK8sResource, ResourceMeta, ResourceContent};
+export type ResourceMetaMap = Record<string, ResourceMeta>;
+export type LocalResourceMetaMap = Record<string, LocalResourceMeta>;
+export type ClusterResourceMetaMap = Record<string, ClusterResourceMeta>;
+export type HelmResourceMetaMap = Record<string, HelmResourceMeta>;
+export type KustomizeResourceMetaMap = Record<string, KustomizeResourceMeta>;
+
+export type ResourceContentMap = Record<string, ResourceContent>;
+export type LocalResourceContentMap = Record<string, LocalResourceContent>;
+export type ClusterResourceContentMap = Record<string, ClusterResourceContent>;
+export type HelmResourceContentMap = Record<string, HelmResourceContent>;
+export type KustomizeResourceContentMap = Record<string, KustomizeResourceContent>;
+
+export type ResourceMapType = Record<string, K8sResource>;
