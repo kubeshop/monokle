632523e9b5ad6b471dc047e17384f64b3ddfad54
Dev Catalin <20538711+devcatalin@users.noreply.github.com>
2022-11-26 15:46:20 +0200

    fix: add preview configuration selection types

--- src/shared/models/selection.ts
+++ src/shared/models/selection.ts
@@ -1,6 +1,6 @@
 import * as Rt from 'runtypes';
 
-import {ResourceOriginRuntype} from './origin';
+import {AnyOriginRuntype} from './origin';
 
 const FileSelectionRuntype = Rt.Record({
   type: Rt.Literal('file'),
@@ -9,7 +9,7 @@ const FileSelectionRuntype = Rt.Record({
 
 const ResourceSelectionRuntype = Rt.Record({
   type: Rt.Literal('resource'),
-  resourceOrigin: ResourceOriginRuntype,
+  resourceOrigin: AnyOriginRuntype,
   resourceId: Rt.String,
 });
 
@@ -23,21 +23,29 @@ const CommandSelectionRuntype = Rt.Record({
   commandId: Rt.String,
 });
 
+const PreviewConfigurationSelectionRuntype = Rt.Record({
+  type: Rt.Literal('preview.configuration'),
+  previewConfigurationId: Rt.String,
+});
+
 const AppSelectionRuntype = Rt.Union(
   FileSelectionRuntype,
   ResourceSelectionRuntype,
   ImageSelectionRuntype,
-  CommandSelectionRuntype
+  CommandSelectionRuntype,
+  PreviewConfigurationSelectionRuntype
 );
 
 export type FileSelection = Rt.Static<typeof FileSelectionRuntype>;
 export type ResourceSelection = Rt.Static<typeof ResourceSelectionRuntype>;
 export type ImageSelection = Rt.Static<typeof ImageSelectionRuntype>;
 export type CommandSelection = Rt.Static<typeof CommandSelectionRuntype>;
+export type PreviewConfigurationSelection = Rt.Static<typeof PreviewConfigurationSelectionRuntype>;
 export type AppSelection = Rt.Static<typeof AppSelectionRuntype>;
 
 export const isFileSelection = FileSelectionRuntype.guard;
 export const isResourceSelection = ResourceSelectionRuntype.guard;
 export const isImageSelection = ImageSelectionRuntype.guard;
 export const isCommandSelection = CommandSelectionRuntype.guard;
+export const isPreviewConfigurationSelection = PreviewConfigurationSelectionRuntype.guard;
 export const isAppSelection = AppSelectionRuntype.guard;
