90bd658eacfb206d3b142d0eef2881fa37a7b5e6
Dev Catalin <20538711+devcatalin@users.noreply.github.com>
2022-11-26 12:55:58 +0200

    chore: reorder origin types & objects

--- src/shared/models/origin.ts
+++ src/shared/models/origin.ts
@@ -4,45 +4,38 @@ export type LocalOrigin = {
   type: 'local';
   filePath: string;
 };
-
-export const LocalOriginRuntype: Rt.Runtype<LocalOrigin> = Rt.Record({
-  type: Rt.Literal('local'),
-  filePath: Rt.String,
-});
-
 export type ClusterOrigin = {
   type: 'cluster';
   context: string;
 };
-
-export const ClusterOriginRuntype: Rt.Runtype<ClusterOrigin> = Rt.Record({
-  type: Rt.Literal('cluster'),
-  context: Rt.String,
-});
-
 export type HelmOrigin = {
   type: 'helm';
   chartId: string;
   valuesFilePath: string;
 };
+export type KustomizeOrigin = {
+  type: 'kustomize';
+  kustomizationId: string;
+};
+export type ResourceOrigin = LocalOrigin | ClusterOrigin | HelmOrigin | KustomizeOrigin;
 
+export const LocalOriginRuntype: Rt.Runtype<LocalOrigin> = Rt.Record({
+  type: Rt.Literal('local'),
+  filePath: Rt.String,
+});
+export const ClusterOriginRuntype: Rt.Runtype<ClusterOrigin> = Rt.Record({
+  type: Rt.Literal('cluster'),
+  context: Rt.String,
+});
 export const HelmOriginRuntype: Rt.Runtype<HelmOrigin> = Rt.Record({
   type: Rt.Literal('helm'),
   chartId: Rt.String,
   valuesFilePath: Rt.String,
 });
-
-export type KustomizeOrigin = {
-  type: 'kustomize';
-  kustomizationId: string;
-};
-
 export const KustomizeOriginRuntype: Rt.Runtype<KustomizeOrigin> = Rt.Record({
   type: Rt.Literal('kustomize'),
   kustomizationId: Rt.String,
 });
-
-export type ResourceOrigin = LocalOrigin | ClusterOrigin | HelmOrigin | KustomizeOrigin;
 
 export const ResourceOriginRuntype: Rt.Runtype<ResourceOrigin> = Rt.Union(
   LocalOriginRuntype,
