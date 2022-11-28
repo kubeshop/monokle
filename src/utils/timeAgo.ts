c5a42e409fecbfd2954ca0c4e1fb074381f69b9c
Erdi Köse <me@erdkse.com>
2022-11-28 16:10:54 +0300

    chore: handle timeAgo error

--- src/utils/timeAgo.ts
+++ src/utils/timeAgo.ts
@@ -1,15 +1,21 @@
+import log from 'loglevel';
 import {DateTime} from 'luxon';
 
 const units = ['year', 'month', 'week', 'day', 'hour', 'minute', 'second'];
 
 export const timeAgo = (date: string) => {
-  let dateTime = DateTime.fromISO(date);
-  const diff = dateTime.diffNow().shiftTo(...(units as any));
-  const unit = units.find((u: string) => diff.get(u as any) !== 0) || 'second';
+  try {
+    let dateTime = DateTime.fromISO(date);
+    const diff = dateTime.diffNow().shiftTo(...(units as any));
+    const unit = units.find((u: string) => diff.get(u as any) !== 0) || 'second';
 
-  const relativeFormatter = new Intl.RelativeTimeFormat('en', {
-    numeric: 'auto',
-    style: 'short',
-  });
-  return relativeFormatter.format(Math.trunc(diff.as(unit as any)), unit as any);
+    const relativeFormatter = new Intl.RelativeTimeFormat('en', {
+      numeric: 'auto',
+      style: 'short',
+    });
+    return relativeFormatter.format(Math.trunc(diff.as(unit as any)), unit as any);
+  } catch (error) {
+    log.warn(error.message);
+    return '-';
+  }
 };
