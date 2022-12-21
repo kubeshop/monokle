d996bbd17ba915b96da1aaa3ddf043909c2f222b
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2022-12-21 16:57:12 +0200

    fix: badge correct count

--- src/components/organisms_new/StartPage/StartPageHeader.tsx
+++ src/components/organisms_new/StartPage/StartPageHeader.tsx
@@ -7,6 +7,8 @@ import {BellOutlined, EllipsisOutlined} from '@ant-design/icons';
 import {TOOLTIP_DELAY} from '@constants/constants';
 import {NotificationsTooltip} from '@constants/tooltips';
 
+import {useAppSelector} from '@redux/hooks';
+
 import {HelpMenu} from '@organisms/PageHeader/HelpMenu';
 
 import {IconButton, SearchInput} from '@atoms';
@@ -16,6 +18,8 @@ import MonokleKubeshopLogo from '@assets/NewMonokleLogoDark.svg';
 import * as S from './StatePageHeader.styled';
 
 const StartPageHeader: React.FC = () => {
+  const unseenNotificationsCount = useAppSelector(state => state.main.notifications.filter(n => !n.hasSeen).length);
+
   const [isHelpMenuOpen, setIsHelpMenuOpen] = useState(false);
 
   return (
@@ -30,7 +34,7 @@ const StartPageHeader: React.FC = () => {
         <S.LearnButton type="text">Learn</S.LearnButton>
 
         <Tooltip mouseEnterDelay={TOOLTIP_DELAY} title={NotificationsTooltip}>
-          <Badge count={2} size="small">
+          <Badge count={unseenNotificationsCount} size="small">
             <IconButton $size="large">
               <BellOutlined />
             </IconButton>
