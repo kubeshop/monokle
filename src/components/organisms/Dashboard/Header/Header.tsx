8e37b42a6c5e1204f006798c99cd83e5d4807bf8
Dev Catalin <20538711+devcatalin@users.noreply.github.com>
2023-01-11 15:42:26 +0200

    chore: remove namespace dropdown from dashboard

--- src/components/organisms/Dashboard/Header/Header.tsx
+++ src/components/organisms/Dashboard/Header/Header.tsx
@@ -1,46 +1,11 @@
-import {useCallback} from 'react';
-
-import {K8sResource} from '@models/k8sresource';
-
-import {useAppSelector} from '@redux/hooks';
-
-import NamespaceHandler from '@src/kindhandlers/Namespace.handler';
-
 import {TitleBar} from '@monokle/components';
 
 import * as S from './Header.styled';
 
 export const Header = ({title}: {title: string}) => {
-  const resourceMap = useAppSelector(state => state.main.resourceMap);
-  const selectedNamespace = useAppSelector(state => state.config.clusterPreviewNamespace);
-
-  const getNamespaces = useCallback((): K8sResource[] => {
-    return Object.values(resourceMap)
-      .filter((resource: K8sResource) => resource.filePath.startsWith('preview://'))
-      .filter(resource => resource.kind === NamespaceHandler.kind);
-  }, [resourceMap]);
-
   return (
     <S.Container>
-      <TitleBar
-        type="secondary"
-        title={title}
-        actions={
-          <S.Select
-            allowClear
-            disabled
-            maxTagCount="responsive"
-            showSearch
-            placeholder="Search to Select"
-            value={selectedNamespace}
-            filterOption={(input: any, option: any) => (option?.label ?? '').includes(input)}
-            filterSort={(optionA: any, optionB: any) =>
-              (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
-            }
-            options={[...getNamespaces().map(resource => ({label: resource.name, value: resource.name}))]}
-          />
-        }
-      />
+      <TitleBar type="secondary" title={title} />
     </S.Container>
   );
 };
