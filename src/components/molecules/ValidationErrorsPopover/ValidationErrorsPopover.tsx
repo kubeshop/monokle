ba3f9be9e627d53463763bc14ec54a3f91be55b9
Dev Catalin <20538711+devcatalin@users.noreply.github.com>
2023-01-20 18:05:01 +0200

    chore: comment out error links components

--- src/components/molecules/ValidationErrorsPopover/ValidationErrorsPopover.tsx
+++ src/components/molecules/ValidationErrorsPopover/ValidationErrorsPopover.tsx
@@ -1,36 +1,38 @@
-import {useMemo} from 'react';
+// import {useMemo} from 'react';
 
-import {Popover} from 'antd';
+// import {Popover} from 'antd';
 
-import Icon from '@atoms/Icon';
+// import Icon from '@atoms/Icon';
 
-import {countResourceErrors} from '@utils/resources';
+// import {countResourceErrors} from '@utils/resources';
 
-import {K8sResource} from '@shared/models/k8sResource';
-import {Colors} from '@shared/styles/colors';
+// import {K8sResource} from '@shared/models/k8sResource';
+// import {Colors} from '@shared/styles/colors';
 
-import ErrorsPopoverContent from './ErrorsPopoverContent';
+// import ErrorsPopoverContent from './ErrorsPopoverContent';
 
-interface IProps {
-  isDisabled: boolean;
-  isSelected: boolean;
-  resource: K8sResource;
-}
+// interface IProps {
+//   isDisabled: boolean;
+//   isSelected: boolean;
+//   resource: K8sResource;
+// }
 
-const ValidationErrorsPopover: React.FC<IProps> = props => {
-  const {resource, isDisabled, isSelected} = props;
+// TODO: this component has to be reimplemented after the @monokle/validation library is integrated
+const ValidationErrorsPopover = () => {
+  return <div />;
+  // const {resource, isDisabled, isSelected} = props;
 
-  const errorCount = useMemo(() => countResourceErrors([resource]), [resource]);
+  // const errorCount = useMemo(() => countResourceErrors([resource]), [resource]);
 
-  if (isDisabled || errorCount === 0) {
-    return null;
-  }
+  // if (isDisabled || errorCount === 0) {
+  //   return null;
+  // }
 
-  return (
-    <Popover mouseEnterDelay={0.5} placement="rightTop" content={<ErrorsPopoverContent resource={resource} />}>
-      <Icon name="error" style={{marginLeft: 5}} color={isSelected ? Colors.blackPure : Colors.redError} />
-    </Popover>
-  );
+  // return (
+  //   <Popover mouseEnterDelay={0.5} placement="rightTop" content={<ErrorsPopoverContent resource={resource} />}>
+  //     <Icon name="error" style={{marginLeft: 5}} color={isSelected ? Colors.blackPure : Colors.redError} />
+  //   </Popover>
+  // );
 };
 
 export default ValidationErrorsPopover;
