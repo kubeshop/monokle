2cd9a35598ac9cfb897141bc5ac2308342a187b4
Dev Catalin <20538711+devcatalin@users.noreply.github.com>
2023-01-31 18:41:44 +0200

    refactor: update types for previews

--- src/shared/models/preview.ts
+++ src/shared/models/preview.ts
@@ -1,38 +1,61 @@
 import * as Rt from 'runtypes';
 
-const HelmPreviewRuntype = Rt.Record({
+type HelmPreview = {
+  type: 'helm';
+  valuesFileId: string;
+  chartId: string;
+};
+
+type HelmConfigPreview = {
+  type: 'helm-config';
+  configId: string;
+};
+
+type KustomizePreview = {
+  type: 'kustomize';
+  kustomizationId: string;
+};
+
+type CommandPreview = {
+  type: 'command';
+  commandId: string;
+};
+
+export type AnyPreview = HelmPreview | HelmConfigPreview | KustomizePreview | CommandPreview;
+
+const HelmPreviewRuntype: Rt.Runtype<HelmPreview> = Rt.Record({
   type: Rt.Literal('helm'),
   valuesFileId: Rt.String,
   chartId: Rt.String,
 });
 
-const HelmConfigPreviewRuntype = Rt.Record({
+const HelmConfigPreviewRuntype: Rt.Runtype<HelmConfigPreview> = Rt.Record({
   type: Rt.Literal('helm-config'),
   configId: Rt.String,
 });
 
-const KustomizePreviewRuntype = Rt.Record({
+const KustomizePreviewRuntype: Rt.Runtype<KustomizePreview> = Rt.Record({
   type: Rt.Literal('kustomize'),
   kustomizationId: Rt.String,
 });
 
-const CommandPreviewRuntype = Rt.Record({
+const CommandPreviewRuntype: Rt.Runtype<CommandPreview> = Rt.Record({
   type: Rt.Literal('command'),
   commandId: Rt.String,
 });
 
-export const AnyPreviewRuntype = Rt.Union(
+export const AnyPreviewRuntype: Rt.Runtype<AnyPreview> = Rt.Union(
   HelmPreviewRuntype,
   HelmConfigPreviewRuntype,
   KustomizePreviewRuntype,
   CommandPreviewRuntype
 );
 
-export type HelmPreview = Rt.Static<typeof HelmPreviewRuntype>;
-export type HelmConfigPreview = Rt.Static<typeof HelmConfigPreviewRuntype>;
-export type KustomizePreview = Rt.Static<typeof KustomizePreviewRuntype>;
-export type CommandPreview = Rt.Static<typeof CommandPreviewRuntype>;
-export type AnyPreview = Rt.Static<typeof AnyPreviewRuntype>;
+// export type HelmPreview = Rt.Static<typeof HelmPreviewRuntype>;
+// export type HelmConfigPreview = Rt.Static<typeof HelmConfigPreviewRuntype>;
+// export type KustomizePreview = Rt.Static<typeof KustomizePreviewRuntype>;
+// export type CommandPreview = Rt.Static<typeof CommandPreviewRuntype>;
+// export type AnyPreview = Rt.Static<typeof AnyPreviewRuntype>;
 
 export type PreviewType = AnyPreview['type'];
 
