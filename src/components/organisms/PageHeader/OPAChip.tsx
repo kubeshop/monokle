4f92bd29af333da9fa6dd315f2d269cfa53e52af
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2023-02-06 17:17:15 +0200

    feat: opa rule count

--- src/components/organisms/PageHeader/OPAChip.tsx
+++ src/components/organisms/PageHeader/OPAChip.tsx
@@ -1,11 +1,12 @@
-import {useAppSelector} from '@redux/hooks';
+import {opaRuleCountSelector, useValidationSelector} from '@redux/validation/validation.selectors';
 
 import {Icon} from '@monokle/components';
 
 import * as S from './OPAChip.styled';
 
 export const OPAChip = () => {
-  const opaRulesCount = useAppSelector(state => state.validation.config.rules?.length || 0);
+  const opaRulesCount = useValidationSelector(opaRuleCountSelector);
+
   return (
     <S.Container>
       <Icon name="opa-status" />
