c224636e578c65e0c3e3cd9936beab1f71190ecd
Dev Catalin <20538711+devcatalin@users.noreply.github.com>
2023-02-07 14:55:57 +0200

    fix: preview type guards

--- src/shared/models/preview.ts
+++ src/shared/models/preview.ts
@@ -23,26 +23,38 @@ export type AnyPreview = HelmPreview | HelmConfigPreview | KustomizePreview | Co
 
 export type PreviewType = AnyPreview['type'];
 
-export const isHelmPreview = (preview: AnyPreview): preview is HelmPreview =>
+export const isHelmPreview = (preview: any): preview is HelmPreview =>
+  typeof preview === 'object' &&
+  'type' in preview &&
   preview.type === 'helm' &&
   'valuesFileId' in preview &&
   typeof preview.valuesFileId === 'string' &&
   'chartId' in preview &&
   typeof preview.chartId === 'string';
 
-export const isHelmConfigPreview = (preview: AnyPreview): preview is HelmConfigPreview =>
-  preview.type === 'helm-config' && 'configId' in preview && typeof preview.configId === 'string';
+export const isHelmConfigPreview = (preview: any): preview is HelmConfigPreview =>
+  typeof preview === 'object' &&
+  'type' in preview &&
+  preview.type === 'helm-config' &&
+  'configId' in preview &&
+  typeof preview.configId === 'string';
 
-export const isKustomizePreview = (preview: AnyPreview): preview is KustomizePreview =>
-  preview.type === 'kustomize' && 'kustomizationId' in preview && typeof preview.kustomizationId === 'string';
+export const isKustomizePreview = (preview: any): preview is KustomizePreview =>
+  typeof preview === 'object' &&
+  'type' in preview &&
+  preview.type === 'kustomize' &&
+  'kustomizationId' in preview &&
+  typeof preview.kustomizationId === 'string';
 
-export const isCommandPreview = (preview: AnyPreview): preview is CommandPreview =>
-  preview.type === 'command' && 'commandId' in preview && typeof preview.commandId === 'string';
+export const isCommandPreview = (preview: any): preview is CommandPreview =>
+  typeof preview === 'object' &&
+  'type' in preview &&
+  preview.type === 'command' &&
+  'commandId' in preview &&
+  typeof preview.commandId === 'string';
 
 export const isAnyPreview = (preview: any): preview is AnyPreview => {
   return (
-    typeof preview === 'object' &&
-    'type' in preview &&
-    (isHelmPreview(preview) || isHelmConfigPreview(preview) || isKustomizePreview(preview) || isCommandPreview(preview))
+    isHelmPreview(preview) || isHelmConfigPreview(preview) || isKustomizePreview(preview) || isCommandPreview(preview)
   );
 };
