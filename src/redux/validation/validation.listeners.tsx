5be9c235c6ac678de5a94fb0cefd9cba713caef1
Mahmoud Mortada <mortda.m.11@gmail.com>
2023-02-07 12:38:48 +0200

    fix: checkout file

--- src/redux/validation/validation.listeners.tsx
+++ src/redux/validation/validation.listeners.tsx
@@ -8,14 +8,23 @@ import {isEmpty, isEqual} from 'lodash';
 
 import {AppListenerFn} from '@redux/listeners/base';
 import {updateK8sVersion, updateProjectK8sVersion} from '@redux/reducers/appConfig';
+import {addMultipleResources, addResource, clearPreview, clearPreviewAndSelectionHistory} from '@redux/reducers/main';
+import {setIsInQuickClusterMode} from '@redux/reducers/ui';
 import {currentConfigSelector} from '@redux/selectors';
 import {clusterResourceMapSelector} from '@redux/selectors/resourceMapSelectors';
-import {loadClusterResources, reloadClusterResources} from '@redux/thunks/cluster';
+import {previewSavedCommand} from '@redux/services/previewCommand';
+import {loadClusterResources, reloadClusterResources, stopClusterConnection} from '@redux/thunks/cluster';
 import {downloadK8sSchema} from '@redux/thunks/downloadK8sSchema';
+import {previewHelmValuesFile} from '@redux/thunks/previewHelmValuesFile';
+import {previewKustomization} from '@redux/thunks/previewKustomization';
+import {removeResources} from '@redux/thunks/removeResources';
+import {runPreviewConfiguration} from '@redux/thunks/runPreviewConfiguration';
 import {setRootFolder} from '@redux/thunks/setRootFolder';
+import {updateResource} from '@redux/thunks/updateResource';
 
 import {doesSchemaExist} from '@utils/index';
 
+import {Incremental} from '@monokle/validation';
 import {isDefined} from '@shared/utils/filter';
 import {activeProjectSelector, kubeConfigContextSelector} from '@shared/utils/selectors';
 
@@ -25,14 +34,18 @@ import {loadValidation, validateResources} from './validation.thunks';
 const loadListener: AppListenerFn = listen => {
   listen({
     matcher: isAnyOf(
-      loadClusterResources.fulfilled,
-      reloadClusterResources.fulfilled,
+      setIsInQuickClusterMode,
       setRootFolder.fulfilled,
-      setConfigK8sSchemaVersion,
+      updateProjectK8sVersion,
       toggleOPARules,
       toggleValidation
     ),
-    async effect(_, {dispatch, delay, signal, cancelActiveListeners}) {
+    async effect(_action, {dispatch, delay, signal, cancelActiveListeners}) {
+      if (isAnyOf(setIsInQuickClusterMode)(_action)) {
+        if (!_action.payload) {
+          return;
+        }
+      }
       cancelActiveListeners();
       await delay(1);
       const loading = dispatch(loadValidation());
@@ -44,13 +57,20 @@ const loadListener: AppListenerFn = listen => {
 
 const validateListener: AppListenerFn = listen => {
   listen({
-    matcher: isAnyOf(loadValidation.fulfilled),
+    matcher: isAnyOf(
+      loadValidation.fulfilled,
+      loadClusterResources.fulfilled,
+      stopClusterConnection.fulfilled,
+      reloadClusterResources.fulfilled,
+      previewKustomization.fulfilled,
+      previewHelmValuesFile.fulfilled,
+      runPreviewConfiguration.fulfilled,
+      previewSavedCommand.fulfilled,
+      clearPreviewAndSelectionHistory,
+      clearPreview
+    ),
     async effect(_action, {dispatch, getState, cancelActiveListeners, signal, delay}) {
       cancelActiveListeners();
-
-      // if (getState().resources.previewLoading) {
-      //   return;
-      // }
 
       const validatorsLoading = getState().validation.status === 'loading';
       if (validatorsLoading) return;
@@ -64,6 +84,47 @@ const validateListener: AppListenerFn = listen => {
   });
 };
 
+const incrementalValidationListener: AppListenerFn = listen => {
+  listen({
+    matcher: isAnyOf(addResource, addMultipleResources, updateResource.fulfilled, removeResources.fulfilled),
+    async effect(_action, {dispatch, delay, signal}) {
+      let incremental: Incremental = {resourceIds: []};
+
+      if (isAnyOf(updateResource.fulfilled, removeResources.fulfilled)(_action)) {
+        incremental = {
+          resourceIds: _action.payload.affectedResourceIdentifiers?.map(r => r.id) ?? [],
+        };
+      }
+
+      if (isAnyOf(addResource)(_action)) {
+        incremental = {
+          resourceIds: [_action.payload.id],
+        };
+      }
+
+      if (isAnyOf(addMultipleResources)(_action)) {
+        incremental = {
+          resourceIds: _action.payload.map(r => r.id),
+        };
+      }
+
+      if (incremental.resourceIds.length === 0) return;
+
+      // TODO: should we cancel active listeners or not?
+      // I think it depends on the resource storage?
+      // but maybe validation should actually be cancelled while the processing of refs should not?!
+      // cancelActiveListeners();
+
+      await delay(200);
+      if (signal.aborted) return;
+      const response = dispatch(validateResources({incremental}));
+      signal.addEventListener('abort', () => response.abort());
+      await response;
+    },
+  });
+};
+
+// TODO: this should be moved to a different file / folder related to cluster interactions or related to schemas
 const clusterK8sSchemaVersionListener: AppListenerFn = listen => {
   listen({
     matcher: isAnyOf(loadClusterResources.fulfilled),
@@ -90,7 +151,6 @@ const clusterK8sSchemaVersionListener: AppListenerFn = listen => {
           nodeResources
             .map(resource => {
               const kubeletVersion = resource.object?.status?.nodeInfo?.kubeletVersion;
-
               if (typeof kubeletVersion !== 'string') {
                 return undefined;
               }
@@ -138,4 +198,9 @@ const clusterK8sSchemaVersionListener: AppListenerFn = listen => {
   });
 };
 
-export const validationListeners = [clusterK8sSchemaVersionListener, loadListener, validateListener];
+export const validationListeners = [
+  clusterK8sSchemaVersionListener,
+  loadListener,
+  validateListener,
+  incrementalValidationListener,
+];
