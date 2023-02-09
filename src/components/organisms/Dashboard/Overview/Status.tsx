116a5b34bcd07b7692264d8205b9170e3dca5a32
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2023-02-09 17:32:23 +0200

    fix: dashboard overview status for errors

--- src/components/organisms/Dashboard/Overview/Status.tsx
+++ src/components/organisms/Dashboard/Overview/Status.tsx
@@ -1,43 +1,24 @@
-import {useCallback} from 'react';
-
 import {size} from 'lodash';
 
 import {useAppSelector} from '@redux/hooks';
 import {clusterResourceMapSelector} from '@redux/selectors/resourceMapSelectors';
+import {problemsSelector, useValidationSelector} from '@redux/validation/validation.selectors';
 
 import * as S from './Status.styled';
 
 export const Status = () => {
   const clusterResourceMap = useAppSelector(clusterResourceMapSelector);
-
-  const getResourceCount = useCallback(() => {
-    return size(clusterResourceMap);
-  }, [clusterResourceMap]);
-
-  // TODO: re-implement this after @monokle/validation
-  const getErrorCount = useCallback(() => {
-    // return Object.values(clusterResourceMap)
-    //   .reduce((total: number, resource: K8sResource) => {
-    //     if (resource.issues && resource.issues.errors) {
-    //       total += resource.issues.errors.length;
-    //     }
-    //     if (resource.validation && resource.validation.errors) {
-    //       total += resource.validation.errors.length;
-    //     }
-    //     return total;
-    //   }, 0);
-    return 0;
-  }, [clusterResourceMap]);
+  const problems = useValidationSelector(problemsSelector);
 
   return (
     <S.Container>
       <S.KindRow $type="resource">
-        <S.Count>{getResourceCount()}</S.Count>
+        <S.Count>{size(clusterResourceMap)}</S.Count>
         <span>resources</span>
       </S.KindRow>
       <S.InnerContainer>
         <S.KindRow $type="error">
-          <S.Count>{getErrorCount()}</S.Count>
+          <S.Count>{size(problems)}</S.Count>
           <span>errors</span>
         </S.KindRow>
       </S.InnerContainer>
