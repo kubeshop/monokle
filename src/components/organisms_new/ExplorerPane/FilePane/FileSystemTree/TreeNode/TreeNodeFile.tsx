01a81259907694448f18bfc213eeff50b4ffa5ea
Dev Catalin <20538711+devcatalin@users.noreply.github.com>
2023-02-11 14:03:18 +0200

    feat: useMenuItems hook for file tree nodes

--- src/components/organisms_new/ExplorerPane/FilePane/FileSystemTree/TreeNode/TreeNodeFile.tsx
+++ src/components/organisms_new/ExplorerPane/FilePane/FileSystemTree/TreeNode/TreeNodeFile.tsx
@@ -1,9 +1,14 @@
-import {useAppSelector} from '@redux/hooks';
+import {useRef} from 'react';
+import {useHoverDirty} from 'react-use';
 
+import {useAppSelector} from '@redux/hooks';
+import {isInClusterModeSelector, isInPreviewModeSelectorNew} from '@redux/selectors';
+
+import {Spinner} from '@monokle/components';
 import {FileEntry} from '@shared/models/fileEntry';
 
 import * as S from './TreeNode.styled';
-import {useCanPreview, useIsDisabled} from './hooks';
+import {useCanPreview, useDelete, useIsDisabled} from './hooks';
 
 type Props = {
   filePath: string;
@@ -12,24 +17,46 @@ type Props = {
 const TreeNodeFile: React.FC<Props> = props => {
   const {filePath} = props;
   const fileEntry: FileEntry | undefined = useAppSelector(state => state.main.fileMap[filePath]);
-
+  const isInClusterMode = useAppSelector(isInClusterModeSelector);
+  const isInPreviewMode = useAppSelector(isInPreviewModeSelectorNew);
+  const nodeContainerRef = useRef<HTMLDivElement>(null);
+  const isHovered = useHoverDirty(nodeContainerRef);
   const isSelected = useAppSelector(
     state => state.main.selection?.type === 'file' && state.main.selection.filePath === filePath
   );
   const isDisabled = useIsDisabled(fileEntry);
   const canBePreviewed = useCanPreview(fileEntry, isDisabled);
+  const {deleteEntry, isDeleteLoading} = useDelete();
 
   if (!fileEntry) {
     return null;
   }
 
   return (
-    <S.NodeContainer $isDisabled={isDisabled}>
+    <S.NodeContainer ref={nodeContainerRef} $isDisabled={isDisabled}>
       <S.TitleContainer>
         <S.TitleText>{fileEntry.name}</S.TitleText>
 
         {canBePreviewed && <S.PreviewIcon $isSelected={isSelected} />}
       </S.TitleContainer>
+      <S.SpinnerContainer>
+        <Spinner />
+      </S.SpinnerContainer>
+
+      {isHovered && (
+        <S.ActionButtonsContainer>
+          {canBePreviewed && (
+            <S.PreviewButton
+              type="text"
+              size="small"
+              disabled={isInPreviewMode || isInClusterMode}
+              $isItemSelected={isSelected}
+            >
+              Preview
+            </S.PreviewButton>
+          )}
+        </S.ActionButtonsContainer>
+      )}
     </S.NodeContainer>
   );
 };
--- src/components/organisms_new/ExplorerPane/FilePane/FileSystemTree/TreeNode/hooks.ts
+++ src/components/organisms_new/ExplorerPane/FilePane/FileSystemTree/TreeNode/hooks.ts
@@ -1,14 +1,17 @@
-import {useCallback, useMemo} from 'react';
+import {useCallback, useMemo, useRef, useState} from 'react';
 
-import {useAppDispatch} from '@redux/hooks';
+import {ItemType as AntdMenuItem} from 'antd/lib/menu/hooks/useItems';
+
+import {useAppDispatch, useAppSelector} from '@redux/hooks';
 import {useResourceMetaMapRef} from '@redux/selectors/resourceMapSelectors';
-import {getHelmValuesFile} from '@redux/services/helm';
+import {getHelmValuesFile, isHelmChartFile, isHelmTemplateFile, isHelmValuesFile} from '@redux/services/helm';
 import {isKustomizationFile} from '@redux/services/kustomize';
 
 import {deleteFileEntry, dispatchDeleteAlert, isFileEntryDisabled} from '@utils/files';
 import {useRefSelector} from '@utils/hooks';
 
 import {FileEntry} from '@shared/models/fileEntry';
+import {isDefined} from '@shared/utils/filter';
 
 export const useCanPreview = (fileEntry?: FileEntry, isDisabled?: boolean) => {
   const localResourceMetaMapRef = useResourceMetaMapRef('local');
@@ -27,13 +30,22 @@ export const useCanPreview = (fileEntry?: FileEntry, isDisabled?: boolean) => {
 
 export const useDelete = (fileEntry?: FileEntry) => {
   const dispatch = useAppDispatch();
+  const [isLoading, setIsLoading] = useState(false);
 
-  return useCallback(() => {
+  const deleteEntry = useCallback(async () => {
     if (!fileEntry) {
       return;
     }
-    deleteFileEntry(fileEntry).then(result => dispatchDeleteAlert(dispatch, result));
+    setIsLoading(true);
+    const result = await deleteFileEntry(fileEntry);
+    dispatchDeleteAlert(dispatch, result);
+    setIsLoading(false);
   }, [fileEntry, dispatch]);
+
+  return {
+    deleteEntry,
+    isDeleteLoading: isLoading,
+  };
 };
 
 export const useIsDisabled = (fileEntry?: FileEntry) => {
@@ -41,4 +53,103 @@ export const useIsDisabled = (fileEntry?: FileEntry) => {
     return isFileEntryDisabled(fileEntry);
   }, [fileEntry]);
   return isDisabled;
+};
+
+export const useFileMenuItems = (
+  stateArgs: {canBePreviewed: boolean; isInClusterMode: boolean; isInPreviewMode: boolean},
+  fileEntry?: FileEntry
+) => {
+  const {canBePreviewed, isInClusterMode, isInPreviewMode} = stateArgs;
+
+  const osPlatform = useAppSelector(state => state.config.osPlatform);
+  const platformFileManagerName = useMemo(() => (osPlatform === 'darwin' ? 'Finder' : 'Explorer'), [osPlatform]);
+  const localResourceMetaMapRef = useResourceMetaMapRef('local');
+  const onClickPreviewRef = useRef(() => {});
+
+  const menuItems = useMemo(() => {
+    if (!fileEntry) {
+      return [];
+    }
+
+    const isFolder = isDefined(fileEntry?.children);
+    const newMenuItems: AntdMenuItem[] = [];
+
+    if (canBePreviewed) {
+      newMenuItems.push({
+        key: 'preview',
+        label: 'Preview',
+        onClick: onClickPreviewRef.current,
+      });
+    }
+
+    if (isFolder) {
+      newMenuItems.push({
+        key: 'new-folder',
+        label: 'New Folder',
+        onClick: () => {},
+      });
+
+      newMenuItems.push({
+        key: 'new-resource',
+        label: 'New Resource',
+        disabled: isInClusterMode || isInPreviewMode,
+        onClick: () => {},
+      });
+    } else {
+      newMenuItems.push({
+        key: 'new-file',
+        label: 'New File',
+        onClick: () => {},
+      });
+
+      newMenuItems.push({
+        key: 'add-resource',
+        label: 'Add Resource',
+        disabled:
+          isKustomizationFile(fileEntry, localResourceMetaMapRef.current) ||
+          isHelmChartFile(fileEntry.filePath) ||
+          isHelmValuesFile(fileEntry.filePath) ||
+          isHelmTemplateFile(fileEntry.filePath) ||
+          !fileEntry.isSupported ||
+          !fileEntry.isExcluded,
+      });
+
+      newMenuItems.push({
+        key: 'duplicate',
+        label: 'Duplicate',
+        onClick: () => {},
+      });
+    }
+
+    newMenuItems.push({
+      key: 'copy-full-path',
+      label: 'Copy path',
+      onClick: () => {},
+    });
+
+    newMenuItems.push({
+      key: 'copy-relative-path',
+      label: 'Copy relative path',
+      onClick: () => {},
+    });
+
+    newMenuItems.push({
+      key: 'rename',
+      label: 'Rename',
+      onClick: () => {},
+    });
+
+    newMenuItems.push({
+      key: 'open-in-github',
+      label: 'Open on GitHub',
+      onClick: () => {},
+    });
+
+    newMenuItems.push({
+      key: 'reveal',
+      label: `Reveal in ${platformFileManagerName}`,
+    });
+  }, [fileEntry, canBePreviewed, isInClusterMode, isInPreviewMode, platformFileManagerName, localResourceMetaMapRef]);
+
+  return menuItems;
 };
