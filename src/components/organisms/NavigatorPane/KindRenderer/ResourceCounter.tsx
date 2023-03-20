d0dada66c3737f8c6058462369a70a29b7ef189a
Dev Catalin <20538711+devcatalin@users.noreply.github.com>
2023-03-20 13:01:39 +0200

    fix: resource counter

--- src/components/organisms/NavigatorPane/KindRenderer/ResourceCounter.tsx
+++ src/components/organisms/NavigatorPane/KindRenderer/ResourceCounter.tsx
@@ -2,6 +2,9 @@ import styled from 'styled-components';
 
 import {useAppSelector} from '@redux/hooks';
 import {getActiveResourceMetaMapFromState} from '@redux/selectors/resourceMapGetters';
+import {isKustomizationPatch, isKustomizationResource} from '@redux/services/kustomize';
+
+import {isResourcePassingFilter} from '@utils/resources';
 
 import {Colors, FontColors} from '@shared/styles';
 
@@ -13,9 +16,17 @@ type Props = {
 
 function ResourceCounter({kind, isSelected, onClick}: Props) {
   const isCollapsed = useAppSelector(state => state.ui.navigator.collapsedResourceKinds.includes(kind));
+  const resourceFilter = useAppSelector(state => state.main.resourceFilter);
 
   const resourceCount = useAppSelector(
-    state => Object.values(getActiveResourceMetaMapFromState(state)).filter(r => r.kind === kind).length
+    state =>
+      Object.values(getActiveResourceMetaMapFromState(state)).filter(
+        r =>
+          r.kind === kind &&
+          isResourcePassingFilter(r, resourceFilter) &&
+          !isKustomizationResource(r) &&
+          !isKustomizationPatch(r)
+      ).length
   );
 
   if (resourceCount === undefined) {
