6c1ef3276ddd832c4fcbf38a947e6498b3b858ec
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2023-03-22 11:28:50 +0200

    fix: remove unused

--- src/components/molecules/Monaco/Monaco.tsx
+++ src/components/molecules/Monaco/Monaco.tsx
@@ -49,8 +49,7 @@ import {ResourceRef} from '@monokle/validation';
 import {ROOT_FILE_ENTRY} from '@shared/constants/fileEntry';
 import {ResourceFilterType} from '@shared/models/appState';
 import {ResourceIdentifier} from '@shared/models/k8sResource';
-import {isHelmPreview} from '@shared/models/preview';
-import {ResourceSelection, isHelmValuesFileSelection} from '@shared/models/selection';
+import {ResourceSelection} from '@shared/models/selection';
 import {MonacoRange, NewResourceWizardInput} from '@shared/models/ui';
 
 import * as S from './Monaco.styled';
@@ -117,7 +116,6 @@ const Monaco: React.FC<IProps> = props => {
   const isInPreviewMode = useAppSelector(isInPreviewModeSelectorNew);
   const isInClusterMode = useAppSelector(isInClusterModeSelector);
   const k8sVersion = useAppSelector(state => state.config.projectConfig?.k8sVersion);
-  const preview = useAppSelector(state => state.main.preview);
 
   const localResourceMetaMap = useResourceMetaMap('local');
   const localResourceContentMap = useResourceContentMap('local');
