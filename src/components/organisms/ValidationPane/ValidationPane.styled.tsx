5b90452f72196a77147bcfc82cff824f0e77654c
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2023-03-30 16:38:47 +0300

    fix: treat validation rejected case

--- src/components/organisms/ValidationPane/ValidationPane.styled.tsx
+++ src/components/organisms/ValidationPane/ValidationPane.styled.tsx
@@ -2,6 +2,8 @@ import {Button} from 'antd';
 
 import styled from 'styled-components';
 
+import {Colors} from '@shared/styles/colors';
+
 export const BackToDashboardButton = styled(Button)`
   display: block;
   margin-top: 10px;
@@ -14,6 +16,20 @@ export const DescriptionContainer = styled.div`
   gap: 20px;
   align-items: center;
   padding: 8px 8px 8px 15px;
+`;
+
+export const ErrorMessage = styled.div`
+  padding-top: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  color: ${Colors.grey9};
+  font-weight: 600;
+`;
+
+export const RevalidateButton = styled(Button)`
+  border-radius: 4px;
+  width: max-content;
 `;
 
 export const ValidationPaneContainer = styled.div`
