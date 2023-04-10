d9a4137e30ff23a5ec62623bad4cbc1272d55616
Erdi Köse <me@erdkse.com>
2023-04-10 06:09:01 +0300

    chore: add styles for exluded folders

--- src/components/organisms/ExplorerPane/FilePane/FileSystemTree/TreeNode/TreeNode.styled.tsx
+++ src/components/organisms/ExplorerPane/FilePane/FileSystemTree/TreeNode/TreeNode.styled.tsx
@@ -55,13 +55,20 @@ export const TitleContainer = styled.div<{$actionButtonsWidth: number; $isHovere
   width: ${({$actionButtonsWidth, $isHovered}) => ($isHovered ? `calc(100% - ${$actionButtonsWidth}px)` : '100%')};
 `;
 
-export const TitleText = styled.span<{$isSelected?: boolean; $isHighlighted?: boolean}>(props => ({
-  overflow: 'hidden',
-  position: 'relative',
-  textOverflow: 'ellipsis',
-  whiteSpace: 'nowrap',
-  color: props.$isSelected ? Colors.blackPure : props.$isHighlighted ? Colors.cyan7 : Colors.blue10,
-}));
+export const TitleText = styled.span<{$isSelected?: boolean; $isHighlighted?: boolean; $isExcluded?: boolean}>(
+  props => ({
+    overflow: 'hidden',
+    position: 'relative',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
+    color:
+      (props.$isSelected && Colors.blackPure) ||
+      (props.$isHighlighted && Colors.cyan7) ||
+      (props.$isExcluded && Colors.grey7) ||
+      Colors.blue10,
+    fontStyle: props.$isExcluded ? `italic` : 'inherit',
+  })
+);
 
 export const NodeContainer = styled.div<{$isDisabled: boolean}>`
   position: relative;
--- src/components/organisms/ExplorerPane/FilePane/FileSystemTree/TreeNode/TreeNodeFile.tsx
+++ src/components/organisms/ExplorerPane/FilePane/FileSystemTree/TreeNode/TreeNodeFile.tsx
@@ -64,7 +64,7 @@ const TreeNodeFile: React.FC<Props> = props => {
       $isDisabled={isDisabled}
     >
       <S.TitleContainer $actionButtonsWidth={actionButtonsWidth} $isHovered={isHovered}>
-        <S.TitleText $isSelected={isSelected}>
+        <S.TitleText $isSelected={isSelected} $isExcluded={fileEntry.isExcluded}>
           <Tooltip
             overlayStyle={{fontSize: '12px', wordBreak: 'break-all'}}
             mouseEnterDelay={TOOLTIP_DELAY}
