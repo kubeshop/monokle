a54065a5686f3cfae647a67dfb4122daaa20146f
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2023-04-25 16:24:20 +0300

    fix: title bar description vertically aligned

--- src/components/organisms/ExplorerPane/FilePane/FilePane.tsx
+++ src/components/organisms/ExplorerPane/FilePane/FilePane.tsx
@@ -112,6 +112,7 @@ const FilePane: React.FC<InjectedPanelProps> = props => {
                 <TitleBarCount count={filesOnly.length} isActive={false} />
               )
             }
+            descriptionStyle={{paddingTop: '10px'}}
             description={
               <S.RootFolderText>
                 {isFolderLoading ? (
