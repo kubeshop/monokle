797fe34cbbc4344265e109416ac137dd58047292
Ole Lensmar <ole@lensmar.com>
2023-04-28 10:21:55 +0200

    fix: formatting or removal of timestamp properties retrieved from cluster

--- src/redux/thunks/utils.ts
+++ src/redux/thunks/utils.ts
@@ -1,13 +1,11 @@
 import * as k8s from '@kubernetes/client-node';
 
 import invariant from 'tiny-invariant';
-import {stringify} from 'yaml';
+import {Document, Scalar, visit} from 'yaml';
 
 import {YAML_DOCUMENT_DELIMITER_NEW_LINE} from '@constants/constants';
 
 import {createKubeClientWithSetup} from '@redux/cluster/service/kube-client';
-
-import {stringifyK8sResource} from '@utils/yaml';
 
 import {getResourceKindHandler} from '@src/kindhandlers';
 
@@ -16,21 +14,54 @@ import {K8sObject} from '@shared/models/k8s';
 import {ResourceMeta} from '@shared/models/k8sResource';
 
 /**
+ * Preprocess for proper serialization:
+ * - remove null values in timestamp properties
+ * - change Date objects to properly quoted strings
+ */
+
+function preprocessClusterResource(item: any) {
+  let doc = new Document(item, {schema: 'yaml-1.1'});
+  visit(doc, {
+    Pair(_, pair) {
+      if (
+        pair.key instanceof Scalar &&
+        pair.value instanceof Scalar &&
+        pair.value.value === null &&
+        // should we even check this? or just remove all pairs with null values?
+        pair.key.value.toString().toLowerCase().includes('timestamp')
+      ) {
+        return visit.REMOVE;
+      }
+    },
+    Scalar(key, node, path) {
+      if (node.value instanceof Date) {
+        node.value = node.value.toISOString();
+        node.type = 'QUOTE_SINGLE';
+      }
+    },
+  });
+  return doc;
+}
+
+/**
  * Utility to convert list of objects returned by k8s api to a single YAML document
  */
 
 export function getK8sObjectsAsYaml(items: any[], kind?: string, apiVersion?: string): string {
-  return items
+  const result = items
     .map(item => {
       delete item.metadata?.managedFields;
 
+      let doc = preprocessClusterResource(item);
       if (kind && apiVersion && !item.apiVersion && !item.kind) {
-        return `apiVersion: ${apiVersion}\nkind: ${kind}\n${stringify(item)}`;
+        return `apiVersion: ${apiVersion}\nkind: ${kind}\n${doc.toString()}`;
       }
 
-      return stringifyK8sResource(item);
+      return doc.toString();
     })
     .join(YAML_DOCUMENT_DELIMITER_NEW_LINE);
+
+  return result;
 }
 
 /**
