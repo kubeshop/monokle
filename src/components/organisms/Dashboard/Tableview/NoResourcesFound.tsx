00709691ab4a9e5ed0a91214c958af701d8ecf04
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2023-05-04 12:57:15 +0300

    refactor: add margin top/bottom

--- src/components/organisms/Dashboard/Tableview/NoResourcesFound.tsx
+++ src/components/organisms/Dashboard/Tableview/NoResourcesFound.tsx
@@ -32,6 +32,7 @@ const NoResourcesFound: React.FC = () => {
 export default NoResourcesFound;
 
 const Container = styled.div`
+  margin: 50px 0px;
   display: flex;
   flex-direction: column;
   align-items: center;
