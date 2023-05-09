e52a394c5baa2ba70c51fd99de2255926d3097f2
Dev Catalin <20538711+devcatalin@users.noreply.github.com>
2023-05-09 15:57:10 +0300

    fix: getIncludedFilePaths

--- src/redux/selectors/fileMapGetters.ts
+++ src/redux/selectors/fileMapGetters.ts
@@ -2,6 +2,6 @@ import {FileMapType} from '@shared/models/appState';
 
 export const getIncludedFilePaths = (fileMap: FileMapType) => {
   return Object.values(fileMap)
-    .filter(file => !file.isExcluded)
+    .filter(file => !file.isExcluded && file.isSupported)
     .map(file => file.filePath);
 };
