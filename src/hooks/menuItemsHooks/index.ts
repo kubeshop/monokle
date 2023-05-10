eeedf3b582a9580eb4e85c44b61db5e1af4fa669
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2023-05-10 10:30:58 +0300

    chore: remove unused code

--- src/hooks/menuItemsHooks/index.ts
+++ src/hooks/menuItemsHooks/index.ts
@@ -2,5 +2,4 @@ export {useHelpMenuItems} from './useHelpMenuItems';
 export {useNewResourceMenuItems} from './useNewResourceMenuItems';
 export {useNewTerminalMenuItems} from './useNewTerminalMenuItems';
 export {useProblemPaneMenuItems} from './useProblemPaneMenuItems';
-export {useRefDropdownMenuItems} from './useRefDropdownMenuItems';
 export {useTerminalOptionsMenuItems} from './useTerminalOptionsMenuItems';
