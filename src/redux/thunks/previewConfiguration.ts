c249bc36be89810e450fe262bd29141ca5b21944
Topliceanu Razvan <47887589+topliceanurazvan@users.noreply.github.com>
2023-05-29 13:35:05 +0300

    fix: multiple fixes

--- src/redux/thunks/previewConfiguration.ts
+++ src/redux/thunks/previewConfiguration.ts
@@ -4,6 +4,7 @@ import {cloneDeep} from 'lodash';
 
 import {updateProjectConfig} from '@redux/appConfig';
 import {clearSelection, selectFile} from '@redux/reducers/main';
+import {stopPreview} from '@redux/services/preview';
 
 import {AppDispatch} from '@shared/models/appDispatch';
 import {RootState} from '@shared/models/rootState';
@@ -21,11 +22,21 @@ export const deletePreviewConfiguration = createAsyncThunk<void, string, {dispat
     if (state.main.preview?.type === 'helm-config' && state.main.preview.configId === previewConfigurationId) {
       const previewConfiguration = previewConfigurationMap[previewConfigurationId];
       const helmChartFilePath = previewConfiguration?.helmChartFilePath;
+
       if (helmChartFilePath) {
         thunkAPI.dispatch(selectFile({filePath: helmChartFilePath}));
       } else {
         thunkAPI.dispatch(clearSelection());
       }
+
+      stopPreview(thunkAPI.dispatch);
+    }
+
+    if (
+      state.main.selection?.type === 'preview.configuration' &&
+      state.main.selection.previewConfigurationId === previewConfigurationId
+    ) {
+      thunkAPI.dispatch(clearSelection());
     }
 
     const previewConfigurationMapCopy = cloneDeep(previewConfigurationMap);
