9f5151e4c7d9ae57585f35b64430ec2e575787c2
Hamid <33328203+emamihe@users.noreply.github.com>
2023-06-15 15:41:54 +0300

    Update docusaurus.config.js

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -18,8 +18,8 @@ const config = {
   url: `https://${organizationName}.github.io`,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: `/${projectName}/`,
-
+  //baseUrl: `/${projectName}/`,
+  baseUrl: `/`,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName, // Usually your GitHub org/user name.
