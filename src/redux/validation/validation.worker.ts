d7b90c3e97ed86a053d1d76b154ad58c922f9340
Dev Catalin <20538711+devcatalin@users.noreply.github.com>
2023-06-20 14:51:39 +0300

    fix: validation import

--- src/redux/validation/validation.worker.ts
+++ src/redux/validation/validation.worker.ts
@@ -1,8 +1,7 @@
 /// <reference lib="webworker" />
 import {WorkerMessage, matchWorkerEvent} from '@utils/worker';
 
-import {ResourceParser, SchemaLoader} from '@monokle/validation';
-import {createExtensibleMonokleValidator} from '@monokle/validation/lib/createExtensibleMonokleValidator.browser';
+import {ResourceParser, SchemaLoader, createExtensibleMonokleValidator} from '@monokle/validation';
 
 import {
   LoadValidationMessage,
