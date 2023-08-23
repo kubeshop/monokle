95d2702311777223dd656d01037ba30116e80b0f
Dev Catalin <20538711+devcatalin@users.noreply.github.com>
2023-08-23 16:35:31 +0300

    feat: cloud login

---
+++ electron/app/services/cloud/login.ts
@@ -0,0 +1,23 @@
+import {shell} from 'electron';
+
+import {CloudLoginResponse} from '@shared/models/cloud';
+
+import {getAuthenticator} from './authenticator';
+import {serializeUser} from './user';
+
+export const cloudLogin = async (): Promise<CloudLoginResponse> => {
+  const authenticator = await getAuthenticator();
+  if (!authenticator) {
+    throw new Error('Something went wrong with the authenticator');
+  }
+  const loginResponse = await authenticator.login('device code');
+  if (!loginResponse.handle) {
+    throw new Error('Something went wrong with the login response');
+  }
+  shell.openExternal(loginResponse.handle.verification_uri_complete);
+  const user = await loginResponse.onDone;
+  if (!user) {
+    throw new Error('Login to Cloud has failed');
+  }
+  return {user: serializeUser(user)};
+};
