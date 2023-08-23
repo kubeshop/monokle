428ec0d0f53451021e9bc9e9fefebf1b404bdf5e
Dev Catalin <20538711+devcatalin@users.noreply.github.com>
2023-08-23 16:34:18 +0300

    refactor: update types for cloud sync

---
+++ src/shared/models/cloud.ts
@@ -0,0 +1,10 @@
+/**
+ * This type is used by the renderer so it doesn't need to have all the properties of the User class
+ */
+export type CloudUser = {
+  readonly email: string;
+};
+
+export type CloudLoginResponse = {
+  user: CloudUser;
+};
